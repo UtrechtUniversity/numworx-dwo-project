f5e979e995dd564666ed0319d7e7b9b1fb207250
wim <wim@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2009-06-11 08:05:11 +0000

    copy WebContent
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO/trunk@2505 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ WebContent/script/frameResize.js
@@ -0,0 +1,28 @@
+	function NewWindow(mypage) 
+	{	
+		parent.bodyframe.location.href=mypage;
+		parent.top.document.body.rows = "100, *"
+	}
+
+	function CloseWindow() 
+	{
+			
+		parent.bodyframe.location.href='about:blank';
+		parent.top.document.body.rows = "600, *"
+	}
+	
+	var win;
+		
+	function NewPopUp(mypage, myname, w, h, scroll) 
+	{	var winl = (screen.width - w) / 2;
+		var wint = (screen.height - h) / 2;
+		winprops = 'height='+h+',width='+w+',top='+wint+',left='+winl+',scrollbars='+scroll+',resizable,status=no';
+		win = window.open(mypage, myname, winprops);
+		if (parseInt(navigator.appVersion) >= 4) 
+		{	win.window.focus(); 
+		}
+	}
+	
+	function ClosePopUp(mypage) 
+	{	win.window.close(mypage);
+	}
---
+++ WebContent/script/ieupdate.js
@@ -0,0 +1,12 @@
+theObjects = document.getElementsByTagName("object"); 
+for (var i = 0; i < theObjects.length; i++) 
+    { 
+    theObjects[i].outerHTML = theObjects[i].outerHTML; 
+    } 
+theObjects = document.getElementsByTagName("applet"); 
+for (var i = 0; i < theObjects.length; i++) 
+    { 
+    theObjects[i].outerHTML = theObjects[i].outerHTML; 
+    } 
+
+
