23fe8cf4594fc73f70862e5b66492289749f0ffb
peterb <peterb@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2009-11-24 15:39:45 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO/trunk@2737 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/client/gui/GuiConstants.java
+++ src/fi/dwo/client/gui/GuiConstants.java
@@ -252,6 +252,36 @@ public abstract class GuiConstants {
 				GUI_IMAGE_SCO = "resources/UU-dwo-sco.png";
 				GUI_IMAGE_COURSE = "resources/UU-dwo-course.png";
 			} else
+			if(profile==44)
+			{	MAIN_BACKGROUND = new Color(255,255,255);
+			   	CELL_BACKGROUND = new Color(255,255,255);
+				FI_LOGO_LOCATION = "resources/fi.gif";
+				HEADER_TEXT = new Font("SansSerif", Font.BOLD, 36);
+				GUI_IMAGE_BG = true;
+				GUI_IMAGE_WELCOME = "resources/UU-dwo-welkom.png";
+				GUI_IMAGE_SCO = "resources/UU-dwo-sco.png";
+				GUI_IMAGE_COURSE = "resources/UU-dwo-course.png";
+			} else
+			if(profile==45)
+			{	MAIN_BACKGROUND = new Color(255,255,255);
+			   	CELL_BACKGROUND = new Color(255,255,255);
+				FI_LOGO_LOCATION = "resources/fi.gif";
+				HEADER_TEXT = new Font("SansSerif", Font.BOLD, 36);
+				GUI_IMAGE_BG = true;
+				GUI_IMAGE_WELCOME = "resources/UU-dwo-welkom.png";
+				GUI_IMAGE_SCO = "resources/UU-dwo-sco.png";
+				GUI_IMAGE_COURSE = "resources/UU-dwo-course.png";
+			} else
+			if(profile==46)
+			{	MAIN_BACKGROUND = new Color(255,255,255);
+			   	CELL_BACKGROUND = new Color(255,255,255);
+				FI_LOGO_LOCATION = "resources/fi.gif";
+				HEADER_TEXT = new Font("SansSerif", Font.BOLD, 36);
+				GUI_IMAGE_BG = true;
+				GUI_IMAGE_WELCOME = "resources/UU-dwo-welkom.png";
+				GUI_IMAGE_SCO = "resources/UU-dwo-sco.png";
+				GUI_IMAGE_COURSE = "resources/UU-dwo-course.png";
+			} else
 //		if(profile==4)
 //		{	MAIN_BACKGROUND = new Color(221, 238, 255);
 //    		CELL_BACKGROUND = new Color(221, 238, 255);
