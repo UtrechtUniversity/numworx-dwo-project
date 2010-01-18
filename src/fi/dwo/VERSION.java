7de71ea88b24b5e7cbeb1c7cae3156b16c953b58
Velth101 <Velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2010-01-18 12:36:07 +0000

    Markeer DWO source als UTF-8
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO/trunk@2881 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/VERSION.java
+++ src/fi/dwo/VERSION.java
@@ -1,6 +1,6 @@
 
 package fi.dwo;
 public interface VERSION {
-	public String VERSION = new String("20100111");
-	public String REVISION= new String("2845:2862M");
+	public String VERSION = new String("20100112");
+	public String REVISION= new String("2845:2870");
 }
