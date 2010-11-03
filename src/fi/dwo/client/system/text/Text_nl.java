f0a69e6b270fbbf911b33c7b2db7db00a9b17041
peterb <peterb@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2010-11-03 16:51:43 +0000

    tekst aanmeldpanel aangepast
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO/trunk@3487 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/client/system/text/Text_nl.java
+++ src/fi/dwo/client/system/text/Text_nl.java
@@ -67,7 +67,7 @@ public class Text_nl extends ListResourceBundle {
 
          { TextMapper.GUIR_SCHOOLLOGIN, "Schoollogin"},
          { TextMapper.GUIR_SCHOOLGROUP, "Ik ben"}, 
-         { TextMapper.GUIR_SCHOOLPASSWORD, "Wachtwoord"},
+         { TextMapper.GUIR_SCHOOLPASSWORD, "Sleutelcode"},
          
          { TextMapper.GUIR_BTN_REGISTER, "Aanmelden"}, 
          { TextMapper.GUIR_BTN_RESET, "Alles Wissen"},
@@ -80,7 +80,7 @@ public class Text_nl extends ListResourceBundle {
          { TextMapper.GUIR_OPT_TEACHER, "Docent"},
          { TextMapper.GUIR_OPT_ADMIN, "Administrator"},
          { TextMapper.GUIR_OPT_SCHOOLCODE, "Sleutelcode"},
-         { TextMapper.GUIR_OPT_SCHOOLADMIN, "Schooladministratie" },
+         { TextMapper.GUIR_OPT_SCHOOLADMIN, "Schooladmin" },
          { TextMapper.GUIR_ERR_REGISTER, "Fout bij het Aanmelden" },
          
          { TextMapper.GUIR_MSG_REGISTERED, "U bent succesvol geregistreerd" },
