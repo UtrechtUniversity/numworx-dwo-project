fa670e5410c8d896539a8f234f35d6a0280dc4bc
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2011-06-01 05:05:25 +0000

    nieuwe profielen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO/trunk@4204 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/client/gui/GuiConstants.java
+++ src/fi/dwo/client/gui/GuiConstants.java
@@ -178,18 +178,8 @@ public abstract class GuiConstants {
 			GUI_IMAGE_SCO = "resources/UU-brx-sco.png";
 			GUI_IMAGE_COURSE = "resources/UU-brx-course.png";
 		} else /**/
-// done 1, 23
-		if(  profile==33 || profile==34 || profile==43 || profile==44 || profile==46 || profile==47 || profile==52 || profile==54)
-		{	MAIN_BACKGROUND = new Color(255,255,255);
-		   	CELL_BACKGROUND = new Color(255,255,255);
-			FI_LOGO_LOCATION = "resources/fi.gif";
-			HEADER_TEXT = new Font("SansSerif", Font.BOLD, 36);
-			HEADER_COLOR = new Color(0,0,0);
-			GUI_IMAGE_BG = true;
-			GUI_IMAGE_WELCOME = "resources/UU-dwo-welkom.png";
-			GUI_IMAGE_SCO = "resources/UU-dwo-sco.png";
-			GUI_IMAGE_COURSE = "resources/UU-dwo-course.png";
-		} else	
+// done 1, 23, 34, 33, 44, 43, 46, 47, 52, 54
+			
 		if(profile==59)
 		{	MAIN_BACKGROUND = new Color(255,255,255);
 		   	CELL_BACKGROUND = new Color(255,255,255);
