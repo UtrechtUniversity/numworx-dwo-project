3802b705cdbe81d7c806a9efb2be9a0fe5d6916b
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2013-03-15 21:58:26 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO/trunk@5656 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/export-jar.bat
+++ toolfiles/export-jar.bat
@@ -1,3 +1,6 @@
 copy ..\output\jar\dwo.jar N:\home\www-dev\InfoGroups\dwo\jars\dwo.jar
+copy ..\output\jar\dwo.jar N:\home\www-dev\InfoGroups\dwo\jars\dwo.jar.pack.gz
+copy ..\output\jar\dwo.jar R:\www-dev\dwo\jars\dwo.jar
+copy ..\output\jar\dwo.jar R:\www-dev\dwo\jars\dwo.jar.pack.gz
 
 
--- toolfiles/jshrink.bat
+++ toolfiles/jshrink.bat
@@ -1,3 +1,5 @@
 java -jar ..\..\..\tools\jshrink.exe -script jshrink_all.txt
 cd ..\output\jar
 call jarindex
+%JAVA_HOME%\bin\pack200 --repack dwo.jar
+%JAVA_HOME%\bin\pack200 dwo.jar.pack.gz dwo.jar
