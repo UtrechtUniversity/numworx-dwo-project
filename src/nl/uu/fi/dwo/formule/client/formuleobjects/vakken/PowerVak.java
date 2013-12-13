9ccc7e7d7db00a215d198005ac30abbbce895dee
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2013-12-13 11:55:02 +0000

    x² wordt getekend door PowerVak maar nog niet op de goede hoogte.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/DWOplayer@6479 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/formule/client/formuleobjects/vakken/PowerVak.java
+++ src/nl/uu/fi/dwo/formule/client/formuleobjects/vakken/PowerVak.java
@@ -2,6 +2,7 @@ package nl.uu.fi.dwo.formule.client.formuleobjects.vakken;
 
 import nl.uu.fi.dwo.formule.client.formuleobjects.FormuleElement;
 import nl.uu.fi.dwo.formule.client.formuleobjects.FormuleElementWithChildren;
+import nl.uu.fi.dwo.formule.client.formuleobjects.FormuleRegel;
 
 public class PowerVak extends FormuleElementWithChildren {
 
@@ -12,19 +13,32 @@ public class PowerVak extends FormuleElementWithChildren {
 	public String toString() {
 		return "$p" + getChild(0) + "$n" + getChild(1) + "@@";
 	}
-	public void paint() {
-		if(!isChanged()) 
-			return;
+	public void paintObject() {
 // at 0,0
-		getChild(0).paint();
-		int width =  getChild(0).width;
-		int height = getChild(0).height;
+		FormuleRegel b = getChild(0);
+		b.paint();
+		int width =  b.width;
+		int height = b.height;
 // at width,0
-		getChild(1).setPosition(width, 0);
-		getChild(1).paint();
+		FormuleRegel p = getChild(1);
+		p.setSmallText(true);
+		p.setPosition(width, 0);
+		p.paint();
 		width += getChild(1).width;
 		height = Math.max(height, getChild(1).height);		
 		setSize(width, height);
+		setAsHoogte(b.getAsHoogte());
+		
+		if (this.isSelected())
+		{
+			ctx.setFillStyle("#aaf");
+			ctx.fillRect(0, 0, this.width, this.height);
+		}
+
+		p.draw(ctx);
+		b.draw(ctx);
+		this.drawCursor();
+
 	}
 
 }
