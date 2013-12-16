8172278f695e369e9699acc41e743f4082c7c6da
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2013-12-16 15:18:17 +0000

    ashoogte voor OperatorVak
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/DWOplayer@6487 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/formule/client/formuleobjects/vakken/OperatorVak.java
+++ src/nl/uu/fi/dwo/formule/client/formuleobjects/vakken/OperatorVak.java
@@ -2,6 +2,7 @@ package nl.uu.fi.dwo.formule.client.formuleobjects.vakken;
 
 import nl.uu.fi.dwo.formule.client.formuleobjects.FormuleElement;
 import nl.uu.fi.dwo.formule.client.formuleobjects.FormuleElementWithChildren;
+import nl.uu.fi.dwo.formule.client.formuleobjects.FormuleRegel;
 import nl.uu.fi.dwo.formule.client.formuleobjects.FormuleTeken;
 
 public class OperatorVak extends FormuleElementWithChildren {
@@ -25,22 +26,23 @@ public class OperatorVak extends FormuleElementWithChildren {
 	
 	public void paintObject() {
 // at 0,0
-		getChild(0).paint();
-		int width =  getChild(0).width;
-		int height = getChild(0).height;
+		final FormuleRegel a = getChild(0);
+		a.paint();
+		int width =  a.width;
+		int as;
 // at width,0
 		teken.setPosition(width, 0);
 		teken.paint();
 		width += teken.width;
-		height  = Math.max(height, teken.height);
 // at width+width, 0
-		getChild(1).setPosition(width, 0);
-		getChild(1).paint();
-		width += getChild(1).width;
-		height = Math.max(height, getChild(1).height);
-		int ashoogte = getChild(0).getAsHoogte();
+		final FormuleRegel b = getChild(1);
+		b.setPosition(width, 0);
+		b.paint();
+		width += b.width;
+		as = Math.max(a.getAsHoogte(), Math.max(b.getAsHoogte(), teken.getAsHoogte()));
+		height = as + Math.max(a.height-a.getAsHoogte(), Math.max( teken.height-teken.getAsHoogte(), b.height-b.getAsHoogte()));
 		setSize(width, height);
-		setAsHoogte(ashoogte);
+		setAsHoogte(as);
 
 		if (this.isSelected())
 		{
@@ -52,13 +54,13 @@ public class OperatorVak extends FormuleElementWithChildren {
 		ctx.setFillStyle("#000");
 		//}
 
-		ctx.setLineWidth(fm.getStrokeWidth());
+		//ctx.setLineWidth(fm.getStrokeWidth());
 
-		this.drawline(ctx, fm.getAscent() / 3, getChild(0).height + fm.getAscent() / 8, this.width - (fm.getAscent() / 3), getChild(0).height + fm.getAscent() / 8);
+		//this.drawline(ctx, fm.getAscent() / 3, a.height + fm.getAscent() / 8, this.width - (fm.getAscent() / 3), a.height + fm.getAscent() / 8);
 
-		this.getChild(1).draw(ctx);
-		this.getChild(0).draw(ctx);
-		teken.draw(ctx);
+		b.draw(ctx, b.x, as-b.getAsHoogte());
+		a.draw(ctx, a.x, as-a.getAsHoogte());
+		teken.draw(ctx, teken.x, as-teken.getAsHoogte());
 		this.drawCursor();
 
 	}
