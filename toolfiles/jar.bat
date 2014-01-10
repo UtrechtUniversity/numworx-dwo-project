732d06dfe64b493ebd13f331301d326f65a9f191
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-01-10 08:04:11 +0000

    toolfiles
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO/trunk@6577 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/jar.bat
+++ toolfiles/jar.bat
@@ -1,7 +1,5 @@
-%JAVA_HOME%\bin\jar.exe cvfm ..\output\jar\dwo.jar ..\output\classes\meta-inf\Manifest.mf @jar_all.txt
-%JAVA_HOME%\bin\jar.exe cvfm ..\output\jar\dwo-test.jar ..\output\classes\meta-inf\Manifest.mf @jar_all.txt
+%JAVA_HOME%\bin\jar.exe cvfm ..\output\jar\dwo.jar ..\output\classes\meta-inf\Manifest.mf @jar.txt
 cd ..\output\jar
 call jarindex
-call jarindex-test
 cd ..\..\toolfiles
 
