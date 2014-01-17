20597fbedeb279cb50450985568a555656eeeef2
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-01-17 07:40:11 +0000

    testcase voor getLaunchData
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOmAccess/trunk@6610 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/servlet/dwomaccess/DWOmAccessTest.java
@@ -0,0 +1,30 @@
+package fi.servlet.dwomaccess;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import junit.framework.TestCase;
+
+public class DWOmAccessTest extends TestCase {
+	DWOmAccess access;
+
+	@Override
+	protected void setUp() throws Exception {
+		access = new DWOmAccess();
+		access.init();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		access.destroy();
+	}
+
+	public void testGetLaunchDataIntOutputStream() throws IOException {
+		int sco = 76537; // wim kladje test
+		OutputStream out = new FileOutputStream("test.xml");
+		access.getLaunchData(sco, out);
+		out.close();
+	}
+
+}
