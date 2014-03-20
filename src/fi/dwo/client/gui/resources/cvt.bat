377c3b4b29259d59f947587e36687c9884657289
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-03-20 11:36:16 +0000

    Griekse swing properties
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO/trunk@7292 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/client/gui/resources/cvt.bat
+++ src/fi/dwo/client/gui/resources/cvt.bat
@@ -1,1 +1,2 @@
-native2ascii -encoding utf-8 swing_fa.properties.utf8 > swing_fa.properties
+%JAVA_HOME%\bin\native2ascii -encoding utf-8 swing_fa.properties.utf8 > swing_fa.properties
+%JAVA_HOME%\bin\native2ascii -encoding utf-8 swing_gr.properties.utf8 > swing_gr.properties
