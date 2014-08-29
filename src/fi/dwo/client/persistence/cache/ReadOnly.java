46c047517a8152637bb7825199c7ab101f9db7ca
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-08-29 13:40:22 +0000

    Haal geen suspendData op in "readonly" profielen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO/trunk@8130 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/client/persistence/cache/ReadOnly.java
+++ src/fi/dwo/client/persistence/cache/ReadOnly.java
@@ -1,6 +1,7 @@
 package fi.dwo.client.persistence.cache;
 
 import fi.dwo.client.persistence.DbAccessIF;
+import fi.dwo.client.system.PersistenceException;
 
 public class ReadOnly extends NoCache {
 
@@ -11,6 +12,14 @@ public class ReadOnly extends NoCache {
 	public String setValue(int uid, int scoid, String key, String value)
 	{
 		return "true";
+	}
+
+	@Override
+	public String getValue(int uid, int scoid, String key) throws PersistenceException
+	{
+		if("suspendData".equals(key))
+			return "";
+		return super.getValue(uid, scoid, key);
 	}
 
 }
