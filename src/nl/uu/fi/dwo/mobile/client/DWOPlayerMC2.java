e1bb3eb13befc591d233ceda2f4c9845f06da502
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-10-16 18:33:01 +0000

    Scorm 2004 voor MC2 configuratie
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/DWOplayer@8404 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/mobile/client/DWOPlayerMC2.java
+++ src/nl/uu/fi/dwo/mobile/client/DWOPlayerMC2.java
@@ -1,10 +1,25 @@
 package nl.uu.fi.dwo.mobile.client;
 
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.user.client.Window;
+
 public class DWOPlayerMC2 extends DWOplayerDefaults implements DWOplayerParameters {
 
+	private String launchData;
+	
+	public DWOPlayerMC2() {
+		String host = Window.Location.getHost();
+		if(!GWT.isProdMode())
+			host = "9-dot-mc2dme.appspot.com";
+
+		launchData = "http://"
+				+ host
+				+ "/getJSONLaunchDataBytes?s=";
+	}
+		
 	@Override
 	public String getLaunchData() {
-		return "http://9-dot-mc2dme.appspot.com/getJSONLaunchDataBytes?s=";
+		return launchData;
 	}
 
 }
