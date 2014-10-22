140a8952b3d03b1ae4a198c6060aff2490afab97
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2014-10-22 07:51:07 +0000

    lti widget.jsp
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOmAccess/trunk@8422 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/servlet/lti/WidgetBean.java
+++ src/fi/servlet/lti/WidgetBean.java
@@ -1,5 +1,7 @@
 package fi.servlet.lti;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.Properties;
 
@@ -10,11 +12,23 @@ public class WidgetBean implements Serializable {
 	public WidgetBean() {
 		properties = new Properties();
 		properties.setProperty("widgetsample.SampleWidget", "widgetsample.jar");
+		try {
+			InputStream in = getClass().getResourceAsStream("index.properties");
+			properties.load(in);
+			in.close();
+		} catch (Exception _) {
+		}
 	}
 	
 	public String getArchive(String className) {
 		String archive = "cbookinstance.jar";
 		archive = properties.getProperty(className, archive);
 		return archive;
+	}
+	
+	public static void main(String[] args) { 
+		WidgetBean wb = new WidgetBean();
+		System.out.println(wb.getArchive("xxx"));
+		
 	}
 }
