e63a22feda2eb1001710f5b6719ab331a6f361e8
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-02-26 08:39:02 +0000

    dwo import
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/KeyboardGWT/trunk@9059 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/keyboard/client/DesktopKeyboardFactory.java
+++ src/nl/uu/fi/dwo/keyboard/client/DesktopKeyboardFactory.java
@@ -3,7 +3,6 @@ package nl.uu.fi.dwo.keyboard.client;
 public class DesktopKeyboardFactory implements KeyboardFactory {
 
 	public DesktopKeyboardFactory() {
-		// TODO Auto-generated constructor stub
 	}
 
 }
