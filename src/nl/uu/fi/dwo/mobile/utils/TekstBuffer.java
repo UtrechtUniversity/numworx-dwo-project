ac65b32ac7b4a6c7696f2d1952501ee0bad25618
tacom103 <tacom103@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-04-01 10:56:02 +0000

    Toevoeging kansbomen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/DWOplayer@9420 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/mobile/utils/TekstBuffer.java
+++ src/nl/uu/fi/dwo/mobile/utils/TekstBuffer.java
@@ -348,6 +348,9 @@ public class TekstBuffer
 		case 43:
 			return new StubView("CalculatorGWT.html", currentVakGegevens, randomVarNamen, randomVarWaarden);
 			//return new PopupFacade(currentVakGegevens, new fi.calculatorgwt.client.CalculatorGWT(currentVakGegevens, randomVarNamen, randomVarWaarden));
+		case 42:
+			return new StubView("KansbomenGWT.html", currentVakGegevens, randomVarNamen, randomVarWaarden);
+			//return new PopupFacade(currentVakGegevens, new fi.kansbomengwt.client.KansbomenGWT(currentVakGegevens, randomVarNamen, randomVarWaarden));
 		}
 		
 
