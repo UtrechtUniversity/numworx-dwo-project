78124f81fe33ab489961b53d73f30107940da62f
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-04-02 13:59:24 +0000

    DwoEmfFactory added
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/branches/working@9435 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/server/persistence/DwoEmfFactory.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.dwo.server.persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.sql.DataSource;
+import org.eclipse.persistence.config.PersistenceUnitProperties;
+
+/**
+ * Read the configured datasource from the web.xml and configures the
+ * {@Link EntityManagerFactory EntityManagerFactory} to use it.
+ *
+ * @author G.A.J. van der Plas
+ */
+public class DwoEmfFactory {
+
+    private static final Logger log = Logger.getLogger(DwoEmfFactory.class.getName());
+    
+
+    //
+    private volatile static EntityManagerFactory _instance = Persistence.createEntityManagerFactory("DWO_MySQLDB");;
+//uses config below, but does not load persistent classes. Needs to generate xml or so.
+//<persistence-unit name="DWO_MySQLDB" transaction-type="NON_JTA_DATASOURCE">
+//<!-- include all and any required persistent entity in here exclude-unlisted-classes is being ignored!-->
+//    <non-jta-data-source>jdbc/dwodb</non-jta-data-source>
+//    <provider>org.eclipse.persistence.jpa.PersistenceProvider</provider>
+//    <class>fi.dwo.commons.persistence.entities.PersistentHasRole</class>
+//    <class>fi.dwo.commons.persistence.entities.PersistentUser</class>
+//    <class>fi.dwo.commons.persistence.entities.DwoSystemParameters</class>
+//    <exclude-unlisted-classes>false</exclude-unlisted-classes>
+//    <shared-cache-mode>NONE</shared-cache-mode>
+//  </persistence-unit>
+    public static EntityManagerFactory instance()  {
+        if (_instance == null) {
+            synchronized (DwoEmfFactory.class) {
+                if (_instance == null) {
+                    try {
+                        Map properties = new HashMap();
+                        DataSource dataSource = getDataSource();
+                        properties.put(PersistenceUnitProperties.NON_JTA_DATASOURCE, dataSource);
+                        properties.put(PersistenceUnitProperties.LOGGING_LEVEL, "FINE");
+                        properties.put(PersistenceUnitProperties.LOGGING_TIMESTAMP, "false");
+                        properties.put(PersistenceUnitProperties.LOGGING_THREAD, "false");
+                        properties.put(PersistenceUnitProperties.LOGGING_SESSION, "false");
+                        _instance = Persistence.createEntityManagerFactory("DWO_MySQLDB", properties);
+                    } catch (NamingException ex) {
+                        log.log(Level.SEVERE, null, ex);
+                        return null;
+                    }
+                }
+            }
+
+        }
+        return _instance;
+    }
+
+    private static DataSource getDataSource() throws NamingException {
+        Context initContext = new InitialContext();
+        Context envContext = (Context) initContext.lookup("java:/comp/env");
+        DataSource dataSource = (DataSource) envContext.lookup("jdbc/dwodb");
+//        javax.naming.Context context = new javax.naming.InitialContext();
+//        javax.sql.DataSource dataSource = (javax.sql.DataSource) context.lookup("jdbc/dwodb");
+
+        log.log(Level.FINE, "Datasource jdbc/dwodb is: {0}.", new Object[]{dataSource.toString()});
+        return dataSource;
+    }
+
+}
