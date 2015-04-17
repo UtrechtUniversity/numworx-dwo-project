3f8b78e2532f3aba4bd095550175ef105e3a652a
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-04-17 16:54:14 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/KeyboardGWT/trunk@9559 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/keyboard/client/DWODesktopKeyboard.java
+++ src/nl/uu/fi/dwo/keyboard/client/DWODesktopKeyboard.java
@@ -61,35 +61,39 @@ public class DWODesktopKeyboard extends AbstractKeyboard {
 	FKey t4_1,t4_2,t4_3,t4_4, t4_5,t4_6,t4_7,t4_8, t4_9,t4_10,t4_11,t4_12, t4_13,t4_14;//,t4_15;
 
 
-	@UiHandler("t3_1") void onT3_1(ClickEvent e) {getEditor().insert('1');}
-	@UiHandler("t3_2") void onT3_2(ClickEvent e) {getEditor().insert('2');}
-	@UiHandler("t3_3") void onT3_3(ClickEvent e) {getEditor().insert('3');}
-	@UiHandler("t3_4") void onT3_4(ClickEvent e) {getEditor().insert('+');}
-	@UiHandler("t3_5") void onT3_5(ClickEvent e) {getEditor().insert('a');}
-	@UiHandler("t3_6") void onT3_6(ClickEvent e) {getEditor().insert('b');}
+	@UiHandler("t3_1") void onT3_1(ClickEvent e) {getEditor().wortel();}
+	@UiHandler("t3_2") void onT3_2(ClickEvent e) {getEditor().macht();}
+	@UiHandler("t3_3") void onT3_3(ClickEvent e) {getEditor().kwadraat();}
+	@UiHandler("t3_4") void onT3_4(ClickEvent e) {getEditor().breuk();}
+	@UiHandler("t3_5") void onT3_5(ClickEvent e) {getEditor().haakjes();}
+	@UiHandler("t3_6") void onT3_6(ClickEvent e) {getEditor().ndewortel();}
 	@UiHandler("t3_7") void onT3_7(ClickEvent e) {getEditor().integraal();}
 	@UiHandler("t3_8") void onT3_8(ClickEvent e) {getEditor().prv();}
-	@UiHandler("t3_9") void onT3_9(ClickEvent e) {getEditor().integraal();}
-	@UiHandler("t3_10") void onT3_10(ClickEvent e) {getEditor().diff();}
-	@UiHandler("t3_11") void onT3_11(ClickEvent e) {getEditor().diff();}
-	@UiHandler("t3_12") void onT3_12(ClickEvent e) {getEditor().insert('⟨');}
-	@UiHandler("t3_13") void onT3_13(ClickEvent e) {getEditor().insert('⟩');}
+	@UiHandler("t3_9") void onT3_9(ClickEvent e) {getEditor().ndelog();}
+	@UiHandler("t3_10") void onT3_10(ClickEvent e) {getEditor().abs();}
+	@UiHandler("t3_11") void onT3_11(ClickEvent e) {getEditor().subscript();}
+	@UiHandler("t3_12") void onT3_12(ClickEvent e) {getEditor().bin();}
+	@UiHandler("t3_13") void onT3_13(ClickEvent e) {getEditor().conjug();}
 	@UiHandler("t3_14") void onT3_14(ClickEvent e) {switchGreek();}
-//	@UiHandler("t3_15") void onT3_15(ClickEvent e) {getEditor().insert('\u2265');}
 
-	@UiHandler("t4_1") void onT4_1(ClickEvent e) {getEditor().insert('0');}
-	@UiHandler("t4_2") void onT4_2(ClickEvent e) {getEditor().insert(',');}
-	@UiHandler("t4_3") void onT4_3(ClickEvent e) {getEditor().haakjes();}
-	@UiHandler("t4_4") void onT4_4(ClickEvent e) {getEditor().insert('-');}
-	@UiHandler("t4_5") void onT4_5(ClickEvent e) {getEditor().breuk();}
-	@UiHandler("t4_6") void onT4_6(ClickEvent e) {getEditor().insert('\u03c0');}
-	@UiHandler("t4_7") void onT4_7(ClickEvent e) {getEditor().limiet0();}
-	@UiHandler("t4_8") void onT4_8(ClickEvent e) {getEditor().limiet1();}
-	@UiHandler("t4_9") void onT4_9(ClickEvent e) {getEditor().limiet2();}
-	@UiHandler("t4_10") void onT4_10(ClickEvent e) {getEditor().sigma();}
-	@UiHandler("t4_11") void onT4_11(ClickEvent e) {getEditor().conjug();}
-	@UiHandler("t4_12") void onT4_12(ClickEvent e) {getEditor().insert('←');}
-	@UiHandler("t4_13") void onT4_13(ClickEvent e) {getEditor().insert('→');};
+	@UiHandler("t4_1") void onT4_1(ClickEvent e) {getEditor().diff();}
+	@UiHandler("t4_2") void onT4_2(ClickEvent e) {getEditor().limiet0();}
+	@UiHandler("t4_3") void onT4_3(ClickEvent e) {getEditor().limiet1();}
+	@UiHandler("t4_4") void onT4_4(ClickEvent e) {getEditor().limiet2();}
+	@UiHandler("t4_6") void onT4_6(ClickEvent e) {getEditor().primitieve();}
+	@UiHandler("t4_7") void onT4_7(ClickEvent e) {getEditor().sigma();}
+
+	@UiHandler({"t4_5", "t4_8", "t4_9", "t4_10", "t4_11", "t4_12"} )
+	void insert(ClickEvent e) {
+		Object s = e.getSource(); // HTML mischien getElement en dan DOM methoden?
+		String string = s.toString();
+		int i = string.indexOf('>');
+		int j = string.indexOf('<', i);
+		string = string.substring(i+1, j);
+		getEditor().insert(string);
+	}
+
+	@UiHandler("t4_13") void onT4_13(ClickEvent e) {getEditor().diff();};
 	@UiHandler("t4_14") void onT4_14(ClickEvent e) {blur();}
 //	@UiHandler("t4_15") void onT4_15(ClickEvent e) {getEditor().insert('∞');}
 
