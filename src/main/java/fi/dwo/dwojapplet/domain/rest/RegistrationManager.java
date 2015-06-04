686c7efd01a4a1a4bc94584f492801814cafa91c
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-06-04 13:57:11 +0000

    eod
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOJAppletClient/branches/working@9913 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/dwojapplet/domain/rest/RegistrationManager.java
+++ src/main/java/fi/dwo/dwojapplet/domain/rest/RegistrationManager.java
@@ -1,9 +1,19 @@
 package fi.dwo.dwojapplet.domain.rest;
 
 import fi.dwo.commons.exceptions.Dwo2RestException;
+import fi.dwo.commons.persistence.entities.PersistentUser;
 import fi.dwo.commons.rest.entities.*;
 import fi.dwo.dwojapplet.REST.StoredRestManager;
+import fi.dwo.dwojapplet.domain.DwoHelper;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 
 /**
  * Manages the user profile.
@@ -19,11 +29,26 @@ public class RegistrationManager {
         r = StoredRestManager.getInstance().put("/rest/public/registration/newUser/json", Boolean.class, newUserReg);
         return r;
     }
-
-    public static boolean RegisterExistingUser(NewUserRegistration newUserReg) throws RestException {
+    
+    public static boolean RegisterExistingUser(NewUserRegistration existingUserReg) throws RestException {
         boolean r;
-
-        r = StoredRestManager.getInstance().put("/rest/secure/registration/existingUser/json", Boolean.class, newUserReg);
+        //login to rest service, note there is usually not yet be a fully configured StoredRestManager.
+        HttpAuthenticationFeature feature = HttpAuthenticationFeature.universalBuilder().credentialsForDigest(existingUserReg.getUsername(), existingUserReg.getPassword()).build();
+        Client client = ClientBuilder.newClient().register(feature);
+        
+        Response response = client.target(DwoHelper.getBaseServletUrlString())
+                .path("/rest/secure/registration/existingUser/json")
+                .request().put(Entity.entity(existingUserReg, MediaType.APPLICATION_JSON));
+        if (response.getStatus() != 200) {
+            // failed login
+            System.out.println("Code: " + response.getStatus() + ". Reason: " + response.getStatusInfo().getReasonPhrase());
+            return false;
+        } else {
+            //Set return value
+           r = response.readEntity(Boolean.class);
+            // succeeded login
+            LOG.log(Level.INFO, "Logged in with username {0}.", new Object[]{existingUserReg.getUsername()});
+        }
         return r;
     }
 
