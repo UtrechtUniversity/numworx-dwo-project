261d8f9fe117c6a936fbd7a7b1b8ed8d3d0db769
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-07-06 11:25:53 +0000

    RestException handling fixes.Locale resource needs testing.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-commons-lib/branches/working@10093 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/commons/exceptions/Dwo2ExceptionCode.java
+++ src/main/java/fi/dwo/commons/exceptions/Dwo2ExceptionCode.java
@@ -1,11 +1,14 @@
 /*Copyrighted 2015. */
 package fi.dwo.commons.exceptions;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Statically typed Dwo2 exception codes for which a localized description can be found.
  * 
  * @author Gert van der Plas
  */
+@XmlRootElement
 public enum Dwo2ExceptionCode {
     Rest_InternalError, //Internal software error, a stack trace should be acquired.
     User_AuthenticationError, // Illegal account details.
--- src/main/java/fi/dwo/commons/exceptions/Dwo2RestException.java
+++ src/main/java/fi/dwo/commons/exceptions/Dwo2RestException.java
@@ -1,10 +1,16 @@
 package fi.dwo.commons.exceptions;
 
 import com.owlike.genson.Genson;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,24 +18,26 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Dwo2 Rest exception. This exception contains an error code and message. The
- * error code can be used to lookup general description that is localized for 
- * translation. Primary use is for the client-side. The message part is not 
- * localized as it is assumed to contain stack traces and other internal code 
- * info which by default is English. For GUI messaging it is recommended to use 
+ * error code can be used to lookup general description that is localized for
+ * translation. Primary use is for the client-side. The message part is not
+ * localized as it is assumed to contain stack traces and other internal code
+ * info which by default is English. For GUI messaging it is recommended to use
  * the method {@Link getLocalizedTypeMessage getLocalizedTypeMessage}.
  *
  * @author G.A.J. van der Plas
  */
 @XmlRootElement
 public class Dwo2RestException extends WebApplicationException implements Dwo2ExceptionInterface {
-
+    Dwo2ExceptionCode code;
+    String message;
+    
     /**
-     * 
+     *
      * Returns the Dwo2 exception code. The function extracts the component from
      * the JSON string stored in the default exception message.
-     * 
+     *
      * @return A Dwo2 exception code.
-     * 
+     *
      * @see Dwo2ExceptionCode
      */
     @Override
@@ -38,58 +46,82 @@ public class Dwo2RestException extends WebApplicationException implements Dwo2Ex
     }
 
     /**
-     * 
-     * Returns the Dwo2 exception message. The function extracts the component from
-     * the JSON string stored in the default exception message.
-     * 
+     *
+     * Returns the Dwo2 exception message. The function extracts the component
+     * from the JSON string stored in the default exception message.
+     *
      * @return A Dwo2 message string.
-     * 
+     *
      * @see Dwo2ExceptionCode
-     * @return 
+     * @return
      */
     @Override
-    public String getDwo2Message(){
+    public String getDwo2Message() {
         return decodeMessageInJSON(super.getMessage());
     }
 
     /**
-     * Encodes the parameters as a JSON string into the Exception message. The default 
-     * status code BAD_REQUEST is set via the Response for the WebApplicationException.
-     * 
+     * Encodes the parameters as a JSON string into the Exception message. The
+     * default status code BAD_REQUEST is set via the Response for the
+     * WebApplicationException.
+     *
      * @param code A Dwo2 exception code.
      * @param message A Dwo2 message string.
-     * 
+     *
      * @see Response
      * @see WebApplicationException
      */
     public Dwo2RestException(Dwo2ExceptionCode code, String message) {
         super(Response.status(400)
-             .entity(encodeJSON(code,message)).type(MediaType.TEXT_HTML).build()
+                .entity(encodeJSON(code, message)).type(MediaType.TEXT_HTML).build()
         );
-        
+        this.code = code;
+        this.message = message;
     }
 
     /**
-     * Encodes the code and message parameters as a JSON string into the Exception message.
-     * The status code is set via  Response for the WebApplicationException.
-     * 
+     * Encodes the code and message parameters as a JSON string into the
+     * Exception message. The status code is set via Response for the
+     * WebApplicationException.
+     *
      * @param code A Dwo2 exception code.
      * @param message A Dwo2 message string.
      * @param status A response status code.
-     * 
+     *
      * @see Response
      * @see WebApplicationException
      */
-        public Dwo2RestException(Dwo2ExceptionCode code, String message, Response.Status status) {
+    public Dwo2RestException(Dwo2ExceptionCode code, String message, Response.Status status) {
         super(Response.status(status)
-             .entity(encodeJSON(code,message)).type(MediaType.TEXT_PLAIN).build()
+                .entity(encodeJSON(code, message)).type(MediaType.TEXT_PLAIN).build()
         );
     }
 
     @Override
     public String getLocalizedCodeExplanation(Locale locale) {
-        ResourceBundle localeLookup = ResourceBundle.getBundle("Dwo2Exceptions", locale);
-        String msg = localeLookup.getString(Dwo2ExceptionCode.class.getSimpleName() + "." + getDwo2Code().name());
+        String msg;
+        try {
+            //TODO test locale resource finding.
+            ResourceBundle localeLookup = ResourceBundle.getBundle("Dwo2Exceptions", locale);
+            msg = localeLookup.getString(Dwo2ExceptionCode.class.getSimpleName() + "." + code.name());
+        }
+        catch (Exception e) {
+            Logger.getLogger(Dwo2RestException.class.getName()).log(Level.SEVERE, null, e);
+            msg = message;
+        }
+//        try {
+//            localeLookup = new PropertyResourceBundle(new FileInputStream("./Dwo2Exceptions_nl_NL.properties"));
+//            msg = localeLookup.getString(Dwo2ExceptionCode.class.getSimpleName() + "." + getDwo2Code().name());
+//        }
+//        catch (FileNotFoundException ex) {
+//            Logger.getLogger(Dwo2RestException.class.getName()).log(Level.SEVERE, null, ex);
+//            msg = this.getDwo2Message();
+//        }
+//        catch (IOException ex) {
+//            Logger.getLogger(Dwo2RestException.class.getName()).log(Level.SEVERE, null, ex);
+//            msg = this.getDwo2Message();
+//        }
+
         return msg;
     }
 
@@ -98,10 +130,10 @@ public class Dwo2RestException extends WebApplicationException implements Dwo2Ex
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("Dwo2ExceptionCode", code.name());
         map.put("msg", message);
-        String json = genson.serialize(map); 
+        String json = genson.serialize(map);
         return json;
     }
-    
+
     public static String decodeMessageInJSON(String json) {
         Genson genson = new Genson();
         Map<String, Object> map = (Map<String, Object>) genson.deserialize(json, Map.class);
@@ -111,7 +143,7 @@ public class Dwo2RestException extends WebApplicationException implements Dwo2Ex
     public static Dwo2ExceptionCode decodeCodeInJSON(String json) {
         Genson genson = new Genson();
         Map<String, Object> map = (Map<String, Object>) genson.deserialize(json, Map.class);
-        cast error
-        return (Dwo2ExceptionCode) map.get("Dwo2ExceptionCode");
-    }    
+        String code = (String) map.get("Dwo2ExceptionCode");
+        return Dwo2ExceptionCode.valueOf(code);
+    }
 }
