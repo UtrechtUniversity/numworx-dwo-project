7d3a9a2885ae771ff7bd04cbdc04fe6e7a869ff6
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-07-20 10:09:23 +0000

    added AppletConfig Manager.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/branches/working@10226 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/server/PersistentEntityManagers/AppletConfigManager.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.dwo.server.PersistentEntityManagers;
+
+import fi.dwo.commons.persistence.entities.PersistentAppletConfig;
+import fi.dwo.server.persistence.DwoEmfFactory;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ * Manages applet configs in the persistent storage. 
+ *
+ * @author G.A.J. van der Plas
+ */
+public class AppletConfigManager {
+
+    private static final Logger LOG = Logger.getLogger(AppletConfigManager.class.getName());
+
+    private static EntityManager getEntityManager() {
+        EntityManager em = DwoEmfFactory.createEntityManager();
+        return em;
+    }
+
+    /**
+     * Create.
+     *
+     * @param persistentAppletConfig
+     */
+    public static void create(PersistentAppletConfig persistentAppletConfig) throws PersistenceException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(persistentAppletConfig);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Can't create the PersistentAppletConfig.", e);
+            throw new PersistenceException(e);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    /**
+     * Update
+     *
+     * @param persistentAppletConfig
+     * @throws Exception
+     */
+    public static void edit(PersistentAppletConfig persistentAppletConfig) throws PersistenceException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            persistentAppletConfig = em.merge(persistentAppletConfig);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            String msg = e.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = persistentAppletConfig.getAppletConfigID();
+                if (findEntity(id) == null) {
+                    LOG.log(Level.FINE, "The PersistentAppletConfig with " + id + " no longer exists.", e);
+                    throw new PersistenceException(e);
+                }
+            }
+            throw new PersistenceException(e);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    /**
+     * Removes a user from the persistent store.
+     *
+     * @param id
+     */
+    public static void destroy(Integer id) throws PersistenceException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            PersistentAppletConfig persistentAppletConfig = null;
+            try {
+                persistentAppletConfig = em.getReference(PersistentAppletConfig.class, id);
+                persistentAppletConfig.getAppletConfigID();
+            } catch (EntityNotFoundException e) {
+                LOG.log(Level.FINE, "The PersistentAppletConfig with " + id + " no longer exists.", e);
+                throw new PersistenceException(e);
+            }
+            em.remove(persistentAppletConfig);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public static List<PersistentAppletConfig> findEntities() {
+        return findEntities(true, -1, -1);
+    }
+
+    public static List<PersistentAppletConfig> findEntities(int maxResults, int firstResult) {
+        return findEntities(false, maxResults, firstResult);
+    }
+
+    private static List<PersistentAppletConfig> findEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(PersistentAppletConfig.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static PersistentAppletConfig findEntity(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(PersistentAppletConfig.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public static int getEntityCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<PersistentAppletConfig> rt = cq.from(PersistentAppletConfig.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+
+
+}
