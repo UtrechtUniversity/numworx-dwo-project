dfff7f1ad006fea04ad4eb640540025acb587315
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-07-20 09:39:43 +0000

    added SchoolClassManager.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/branches/working@10218 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/server/PersistentEntityManagers/SchoolClassManager.java
@@ -0,0 +1,154 @@
+package fi.dwo.server.PersistentEntityManagers;
+
+import fi.dwo.commons.persistence.entities.PersistentSchoolClass;
+import fi.dwo.server.persistence.DwoEmfFactory;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ * Manages school classes  in the persistent storage.
+ *
+ * @author G.A.J. van der Plas
+ */
+public class SchoolClassManager {
+
+    private static final Logger LOG = Logger.getLogger(SchoolClassManager.class.getName());
+
+    private static EntityManager getEntityManager() {
+        EntityManager em = DwoEmfFactory.createEntityManager();
+        return em;
+    }
+
+    /**
+     * Create.
+     *
+     * @param entity
+     */
+    public static void create(PersistentSchoolClass entity) throws PersistenceException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Can't create the PersistentSchoolClass.", e);
+            throw new PersistenceException(e);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    /**
+     * Update
+     *
+     * @param entity
+     * @throws Exception
+     */
+    public static void edit(PersistentSchoolClass entity) throws PersistenceException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            entity = em.merge(entity);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            String msg = e.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = entity.getClassID();
+                if (findEntity(id) == null) {
+                    LOG.log(Level.FINE, "The PersistentSchoolClass with " + id + " no longer exists.", e);
+                    throw new PersistenceException(e);
+                }
+            }
+            throw new PersistenceException(e);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    /**
+     * Removes a user from the persistent store.
+     *
+     * @param id
+     */
+    public static void destroy(Integer id) throws PersistenceException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+                PersistentSchoolClass entity = null;
+            try {
+                entity = em.getReference(PersistentSchoolClass.class, id);
+                entity.getClassID();
+            } catch (EntityNotFoundException e) {
+                LOG.log(Level.FINE, "The PersistentSchoolClass with " + id + " no longer exists.", e);
+                throw new PersistenceException(e);
+            }
+            em.remove(entity);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public static List<PersistentSchoolClass> findEntities() {
+        return findEntities(true, -1, -1);
+    }
+
+    public static List<PersistentSchoolClass> findEntities(int maxResults, int firstResult) {
+        return findEntities(false, maxResults, firstResult);
+    }
+
+    private static List<PersistentSchoolClass> findEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(PersistentSchoolClass.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static PersistentSchoolClass findEntity(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(PersistentSchoolClass.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public static int getEntityCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<PersistentSchoolClass> rt = cq.from(PersistentSchoolClass.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+
+}
