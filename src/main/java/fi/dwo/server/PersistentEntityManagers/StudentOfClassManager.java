97e57e466aac57c75f91fa85e063ef161d1bd4f0
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-07-20 09:14:29 +0000

    Added persistent student of class manager.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/branches/working@10203 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/server/PersistentEntityManagers/StudentOfClassManager.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.dwo.server.PersistentEntityManagers;
+
+import fi.dwo.commons.persistence.entities.PersistentStudentOfClass;
+import fi.dwo.commons.persistence.entities.PersistentStudentOfClassPK;
+import fi.dwo.server.persistence.DwoEmfFactory;
+import fi.dwo.server.persistence.exceptions.NonexistentEntityException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ * Manages a student's class membership in the persistent storage. 
+ *
+ * @author G.A.J. van der Plas
+ */
+public class StudentOfClassManager {
+
+    private static final Logger LOG = Logger.getLogger(StudentOfClassManager.class.getName());
+
+    private static EntityManager getEntityManager() {
+        EntityManager em = DwoEmfFactory.createEntityManager();
+        return em;
+    }
+
+    /**
+     * Create.
+     *
+     * @param role
+     */
+    public static void create(PersistentStudentOfClass studentOf) throws PersistenceException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(studentOf);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Can't create the hasRole.", e);
+            throw new PersistenceException(e);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    /**
+     * Update
+     *
+     * @param role
+     * @throws Exception
+     */
+    public static void edit(PersistentStudentOfClass studentOf) throws PersistenceException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            studentOf = em.merge(studentOf);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            String msg = e.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                PersistentStudentOfClassPK id = studentOf.getPersistentStudentOfClassPK();
+                if (findEntity(id) == null) {
+                    LOG.log(Level.FINE, "The persistentSchool with " + id + " no longer exists.", e);
+                    throw new PersistenceException(e);
+                }
+            }
+            throw new PersistenceException(e);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    /**
+     * Removes a user from the persistent store.
+     *
+     * @param id
+     * @throws NonexistentEntityException
+     */
+    public static void destroy(PersistentStudentOfClassPK id) throws PersistenceException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+                PersistentStudentOfClass student = null;
+            try {
+                student = em.getReference(PersistentStudentOfClass.class, id);
+                student.getPersistentStudentOfClassPK();
+            } catch (EntityNotFoundException e) {
+                LOG.log(Level.FINE, "The PersistentHasRole with " + id + " no longer exists.", e);
+                throw new PersistenceException(e);
+            }
+            em.remove(student);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public static List<PersistentStudentOfClass> findEntities() {
+        return findEntities(true, -1, -1);
+    }
+
+    public static List<PersistentStudentOfClass> findEntities(int maxResults, int firstResult) {
+        return findEntities(false, maxResults, firstResult);
+    }
+
+    private static List<PersistentStudentOfClass> findEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(PersistentStudentOfClass.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static PersistentStudentOfClass findEntity(PersistentStudentOfClassPK id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(PersistentStudentOfClass.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public static int getEntityCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<PersistentStudentOfClass> rt = cq.from(PersistentStudentOfClass.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+}
