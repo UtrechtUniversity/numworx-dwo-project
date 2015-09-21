908a33be71f333bd241c86a7b79a6d34a19547cd
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-09-21 12:46:22 +0000

    RESTfull secure/user/account/logins context path.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/branches/working@10470 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/rest/SecuredUserAccountLoginsManager.java
+++ src/main/java/fi/dwo/server/rest/SecuredUserAccountLoginsManager.java
@@ -10,8 +10,11 @@ import fi.dwo.commons.exceptions.Dwo2RestException;
 import fi.dwo.commons.persistence.*;
 import fi.dwo.commons.persistence.entities.*;
 import fi.dwo.commons.rest.entities.*;
+import fi.dwo.server.PersistentEntityManagers.HasRoleManager;
+import fi.dwo.server.PersistentEntityManagers.UserManager;
 import fi.dwo.server.persistence.DwoEmfFactory;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,6 +24,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 /**
@@ -30,10 +34,10 @@ import javax.ws.rs.core.SecurityContext;
  *
  * @author G.A.J. van der Plas
  */
-@Path("/secure/user/schoolsrolesandclasses")
-public class SecuredSchoolsRolesAndClassesManager {
+@Path("/secure/user/account/logins")
+public class SecuredUserAccountLoginsManager {
 
-    private static final Logger LOG = Logger.getLogger(SecuredSchoolsRolesAndClassesManager.class.getName());
+    private static final Logger LOG = Logger.getLogger(SecuredUserAccountLoginsManager.class.getName());
 //    @Context  //injected response proxy supporting multiple threads
 //    private HttpServletResponse response;
 
@@ -83,8 +87,8 @@ public class SecuredSchoolsRolesAndClassesManager {
      */
     @GET
     @Produces({"application/json"})
-    @Path("/get/json")
-    public SchoolsRolesAndClasses getSchoolsAndClasses(@Context SecurityContext sc) {
+    @Path("/get")
+    public SchoolsRolesAndClasses getSchoolLogins(@Context SecurityContext sc) {
         EntityManager em = DwoEmfFactory.getEntityManager();
 
         SchoolsRolesAndClasses sacs = new SchoolsRolesAndClasses();
@@ -156,8 +160,8 @@ public class SecuredSchoolsRolesAndClassesManager {
      */
     @PUT
     @Produces({"application/json"})
-    @Path("/update/json")
-    public SchoolRoleAndClass setActiveSchoolRoleAndClass(@Context SecurityContext sc, SchoolRoleAndClass sarc) {
+    @Path("/switch")
+    public SchoolRoleAndClass switchToSchoolLogin(@Context SecurityContext sc, SchoolRoleAndClass sarc) {
         EntityManager em = DwoEmfFactory.getEntityManager();
 
         PersistentUser user;
@@ -199,10 +203,93 @@ public class SecuredSchoolsRolesAndClassesManager {
         return curSac;
     }        // Create all the tuples.
 
-    @GET
+    
+    /**
+     * Registers an existing user into a new <school,hasRole> tuple.
+     *
+     * @param sc
+     * @param existingUserReg
+     * @return
+     */
+    @PUT
     @Produces({"application/json"})
-    @Path("/classinfo/json")
-    public String info() {
-        return this.getClass().getName();
+    @Path("/submit")
+    public Response addASchoolLogin(@Context SecurityContext sc, KnownUserRegistration existingUserReg) {
+        EntityManager em = DwoEmfFactory.getEntityManager();
+
+        //Check for userid, should exist.
+        PersistentUser user = UserManager.findByUserName(sc.getUserPrincipal().getName());
+        if (user == null) {
+            LOG.log(Level.WARNING, "Username {0}: Authentication for schoollogin {1} and role {2} for usercode {3} failed.", new Object[]{sc.getUserPrincipal().getName(), existingUserReg.getSchoolLogin(), existingUserReg.getRole().getGroupname(), sc.getUserPrincipal().getName()});
+            throw new Dwo2RestException(Dwo2ExceptionCode.User_AuthenticationError, "Authentication for " + sc.getUserPrincipal().getName() + " failed.");
+        }
+        //invariant: have user data
+
+        //fetch schoolgroup id.     
+        PersistentSchoolGroup sg;
+        PersistentSchool school = null;
+        if (existingUserReg.getSchoolLogin() == null && existingUserReg.getSchoolCode() == null) {
+            existingUserReg.setSchoolLogin("null"); //TODO retrieve the null school login and code from the DwoSystemParameters.
+            existingUserReg.setSchoolCode("null");
+        }
+
+        try {
+            javax.persistence.Query q = em.createQuery(" select sg from PersistentSchoolGroup sg join PersistentSchool s where s.schoollogin = :schoollogin and sg.role.groupname = :role and sg.passwd = :schoolcode");
+            q.setParameter("schoollogin", existingUserReg.getSchoolLogin());
+            q.setParameter("schoolcode", existingUserReg.getSchoolCode());
+            q.setParameter("role", (existingUserReg.getRole().getGroupname()));
+            sg = (PersistentSchoolGroup) q.getSingleResult();
+            school = sg.getSchool(); // Sadly, another query.
+            if (school == null) {
+                String msg = String.format("Username {0}: Registration authentication failed for school {1} with school login {2} and school code {3} for usercode {4}.", new Object[]{sc.getUserPrincipal().getName(), school.getSchoolName(), existingUserReg.getSchoolLogin(), existingUserReg.getSchoolCode(), user.getUsername()});
+                throw new Dwo2RestException(Dwo2ExceptionCode.Rest_Registration_Invalid_school_role_credentials, msg);
+            }
+            //invariant: usercode does not exists and a school exists for schoollogin and schoolcode
+            LOG.log(Level.FINER, "Username {0}: School-manager retrieved school {1} from school login and school code for usercode {2}.", new Object[]{sc.getUserPrincipal().getName(), school.getSchoolName(), user.getUsername()});
+        }
+        catch (Exception ex) {
+                String msg = String.format("Username {0}: Registration authentication failed for school login {1} and school code {2} for usercode {4}.", new Object[]{sc.getUserPrincipal().getName(), existingUserReg.getSchoolLogin(), existingUserReg.getSchoolCode(), user.getUsername()});
+                LOG.log(Level.WARNING, msg, ex);
+                throw new Dwo2RestException(Dwo2ExceptionCode.Rest_Registration_Invalid_school_role_credentials, msg);
+        }
+        finally {
+            em.close();
+        }
+
+        //invariant: have school data and user data
+        if (!school.licenseIsValid()) {
+            LOG.log(Level.INFO, "Username {0}: Registration failde for school {1}, school id {2}, the license expired on {3}.", new Object[]{sc.getUserPrincipal().getName(), school.getSchoolName(), school.getSchoolID(), school.getExpire()});
+            throw new Dwo2RestException(Dwo2ExceptionCode.Rest_Registration_School_license_expired, "The license expired on " + school.getExpire());
+        }
+
+        Date now = new Date();
+
+        //invariant: have school data and user data and school has a valid licence.
+        //check for hasRole
+        PersistentHasRolePK pk = new PersistentHasRolePK();
+        pk.setSchoolGroupID(sg.getSchoolGroupID());
+        pk.setUserID(user.getUserID());
+        PersistentHasRole hasRole = HasRoleManager.findEntity(pk);
+        if (hasRole != null) {
+            LOG.log(Level.FINE, "Username {0}: Registration failde for school {1}, schoolgroup id {2}, userid {3} already exists.", new Object[]{sc.getUserPrincipal().getName(), school.getSchoolName(), hasRole.getPersistentHasRolePK().getSchoolGroupID(), hasRole.getPersistentHasRolePK().getUserID()});
+            throw new Dwo2RestException(Dwo2ExceptionCode.Rest_Registration_hasRole_exists, "The user has already been registered.");
+        }
+
+        //invariant: usercode does exist and school exists for schoollogin and schoolcode and has a valid licence and the hasRole does not yet exist.
+        //building hasRole
+        //buiding compound key hasRole
+        hasRole = new PersistentHasRole();
+        hasRole.setPersistentHasRolePK(pk);
+
+        hasRole.setClassID(null);
+        hasRole.setLastLogin(now); //considering an account creation a first login as there is a password
+        hasRole.setRegisterDate(now);
+        hasRole.setRights("_");  //TODO make a rightsManager
+
+        HasRoleManager.create(hasRole);
+        LOG.log(Level.INFO, "Username {0}: Created a new HasRole for user index {1}, schoolgroup index {2} and role {3} was added to the database.", new Object[]{sc.getUserPrincipal().getName(), hasRole.getPersistentHasRolePK().getUserID(), hasRole.getPersistentHasRolePK().getSchoolGroupID(), hasRole.getSchoolGroup().getRole()});
+        //success
+        return Response.status(200).entity(true).build();
     }
+    
 }
