a2909109b5485ba80b7a32a91fe358dee0a8baed
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-10-06 09:12:52 +0000

    Added rest interface class.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-commons-lib/branches/working@10625 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/commons/rest/entities/RestSchoolClass4Teacher.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.dwo.commons.rest.entities;
+
+import fi.dwo.commons.persistence.PersistenceId;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Schools and classes transported over the REST interface.
+ * 
+ * @author G.A.J. van der Plas
+ */
+@XmlRootElement
+public class RestSchoolClass4Teacher extends RestSchoolClass {
+    private String registrationKey;
+    private int iconizer;
+
+    /**
+     * @return the iconizer
+     */
+    public int getIconizer() {
+        return iconizer;
+    }
+
+    /**
+     * @param iconizer the iconizer to set
+     */
+    public void setIconizer(int iconizer) {
+        this.iconizer = iconizer;
+    }
+
+    /**
+     * @return the registrationKey
+     */
+    public String getRegistrationKey() {
+        return registrationKey;
+    }
+
+    /**
+     * @param registrationKey the registrationKey to set
+     */
+    public void setRegistrationKey(String registrationKey) {
+        this.registrationKey = registrationKey;
+    }
+    
+    
+}
