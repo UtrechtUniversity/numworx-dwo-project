a2cab33cc3906f8fb015554590514071a83c6475
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-10-15 14:39:28 +0000

    More junit tests and code updates.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/branches/working@10717 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/test/java/fi/dwo/server/rest/SecuredUserAccountLoginsManagerTest.java
@@ -0,0 +1,131 @@
+/**
+ * Copyrighted Oct 15, 2015
+ */
+package fi.dwo.server.rest;
+
+import fi.dwo.commons.persistence.MySQLPersistenceId;
+import fi.dwo.commons.persistence.RoleType;
+import fi.dwo.commons.persistence.entities.PersistentUser;
+import fi.dwo.commons.rest.entities.RestNewSchoolLogin;
+import fi.dwo.commons.rest.entities.RestSchoolRoleAndClass;
+import fi.dwo.commons.rest.entities.RestSchoolsRolesAndClasses;
+import fi.dwo.server.PersistentDataManagers.core.UserManager;
+import fi.dwo.server.mysql.DatabaseManager;
+import fi.dwo.server.persistence.DwoEmfFactory;
+import java.util.logging.Logger;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gert van der Plas
+ */
+public class SecuredUserAccountLoginsManagerTest {
+    private static final Logger LOG = Logger.getLogger(SecuredUserAccountLoginsManagerTest.class.getName());
+    
+    static DatabaseManager instance = null;
+    
+    public SecuredUserAccountLoginsManagerTest() {
+    }
+    
+
+    @BeforeClass
+    public static void setUpClass() {
+        DwoEmfFactory.setEntityManagerFactory("DWO_TestDB");
+        instance = new DatabaseManager();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        instance = new DatabaseManager();
+        DwoEmfFactory.setDefaultEntityManagerFactory();
+    }
+
+    @Before
+    public void setUp() {
+        instance.IntializeTestDatabase();
+    }
+
+    @After
+    public void tearDown() {
+        instance.ClearDatabase();
+    }
+
+
+    /**
+     * Test of getSchoolLogins method, of class SecuredUserAccountLoginsManager.
+     */
+    @Test
+    public void testGetSchoolLogins() {
+        System.out.println("getSchoolLogins");
+        SecuredUserAccountLoginsManager instance = new SecuredUserAccountLoginsManager();
+
+        SecurityContext sc = new TestSecurityContext("user01", RoleType.STUDENT);
+        RestSchoolsRolesAndClasses result = instance.getSchoolLogins(sc);
+        if(result.getSchoolsRolesAndClassesList().size()!=5){
+        fail("The number of schoollogins is wrong.");
+        }
+        //test default user
+        if((int) MySQLPersistenceId.getId(result.getActiveSchoolRoleAndClass().getSchoolGroupId())!=5
+                ||  (int) MySQLPersistenceId.getId(result.getActiveSchoolRoleAndClass().getUserId())!=10
+                || result.getActiveSchoolRoleAndClass().getSchoolClassId()!= null
+                ){
+        fail("The retreived selected user, group and class is wrong for the selected login.");
+        }
+    }
+
+//    /**
+//     * Test of switchToSchoolLogin method, of class SecuredUserAccountLoginsManager.
+//     */
+//    @Test
+//    public void testSwitchToSchoolLogin() {
+//        System.out.println("switchToSchoolLogin");
+//        SecurityContext sc = new TestSecurityContext("user01", RoleType.STUDENT);
+//        RestSchoolRoleAndClass sarc = new RestSchoolRoleAndClass();
+//        SecuredUserAccountLoginsManager instance = new SecuredUserAccountLoginsManager();
+//        RestSchoolRoleAndClass expResult = null;
+//        RestSchoolRoleAndClass result = instance.switchToSchoolLogin(sc, sarc);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of submitASchoolLogin method, of class SecuredUserAccountLoginsManager.
+//     */
+//    @Test
+//    public void testSubmitASchoolLogin() {
+//        System.out.println("submitASchoolLogin");
+//        SecurityContext sc = null;
+//        RestNewSchoolLogin existingUserReg = null;
+//        SecuredUserAccountLoginsManager instance = new SecuredUserAccountLoginsManager();
+//        Response expResult = null;
+//        Response result = instance.submitASchoolLogin(sc, existingUserReg);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of removeASchoolLogin method, of class SecuredUserAccountLoginsManager.
+//     */
+//    @Test
+//    public void testRemoveASchoolLogin() {
+//        System.out.println("removeASchoolLogin");
+//        SecurityContext sc = null;
+//        RestSchoolRoleAndClass sarc = null;
+//        SecuredUserAccountLoginsManager instance = new SecuredUserAccountLoginsManager();
+//        Boolean expResult = null;
+//        Boolean result = instance.removeASchoolLogin(sc, sarc);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//    
+}
