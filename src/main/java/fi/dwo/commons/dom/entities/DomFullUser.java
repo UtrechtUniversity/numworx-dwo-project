03fcfaaeec3d44c5854186e00c6dbb73ab9cde4d
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-12-01 14:39:54 +0000

    Improved user login
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-commons-lib/branches/working@11126 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/commons/dom/entities/DomFullUser.java
+++ src/main/java/fi/dwo/commons/dom/entities/DomFullUser.java
@@ -5,8 +5,6 @@
  */
 package fi.dwo.commons.dom.entities;
 
-import fi.dwo.commons.persistence.MySQLPersistenceId;
-import fi.dwo.commons.persistence.PersistenceId;
 import fi.dwo.commons.persistence.entities.PersistentUser;
 import java.io.Serializable;
 
@@ -15,69 +13,17 @@ import java.io.Serializable;
  *
  * @author G.A.J. van der Plas
  */
-public class DomFullUser implements Serializable {
-    private PersistenceId id;
-    private String username;
+public class DomFullUser extends DomUser implements Serializable {
     private String password;
-    private String GivenName;
-    private String insertion;
-    private String FamilyName;
     private String email;
-    
 
     public DomFullUser() {
     }
 
     public DomFullUser(PersistentUser user){
-        this.id = MySQLPersistenceId.createPersistentId(user);
-        this.username = user.getUsername();
+        super(user);
         this.password = user.getPasswd();
-        this.GivenName = user.getFirstname();
-        this.FamilyName = user.getLastname();
-        this.insertion = user.getMiddlename();
         this.email = user.getEmail();
-    }
-
-    /**
-     * @return the id
-     */
-    public PersistenceId getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(PersistenceId id) {
-        this.id = id;
-    }
-    
-    /**
-     * @return the GivenName
-     */
-    public String getGivenName() {
-        return GivenName;
-    }
-
-    /**
-     * @param GivenName the GivenName to set
-     */
-    public void setGivenName(String GivenName) {
-        this.GivenName = GivenName;
-    }
-
-    /**
-     * @return the username
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * @param username the username to set
-     */
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     /**
@@ -92,34 +38,6 @@ public class DomFullUser implements Serializable {
      */
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    /**
-     * @return the insertion
-     */
-    public String getInsertion() {
-        return insertion;
-    }
-
-    /**
-     * @param insertion the insertion to set
-     */
-    public void setInsertion(String insertion) {
-        this.insertion = insertion;
-    }
-
-    /**
-     * @return the FamilyName
-     */
-    public String getFamilyName() {
-        return FamilyName;
-    }
-
-    /**
-     * @param FamilyName the FamilyName to set
-     */
-    public void setFamilyName(String FamilyName) {
-        this.FamilyName = FamilyName;
     }
 
     /**
---
+++ src/main/java/fi/dwo/commons/rest/entities/RestFullUser.java
@@ -0,0 +1,48 @@
+/**
+ * Copyrighted Sep 24, 2015
+ */
+package fi.dwo.commons.rest.entities;
+
+import fi.dwo.commons.dom.entities.DomFullUser;
+import fi.dwo.commons.dom.entities.DomUser;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * A class for transferring need-to-know User data over the REST-interface.
+ * 
+ * @author G.A.J. van der Plas
+ */
+
+@XmlRootElement
+public class RestFullUser {
+    private RestContext restContext;
+    private DomFullUser domFullUser;
+
+    /**
+     * @return the restContext
+     */
+    public RestContext getRestContext() {
+        return restContext;
+    }
+
+    /**
+     * @param restContext the restContext to set
+     */
+    public void setRestContext(RestContext restContext) {
+        this.restContext = restContext;
+    }
+
+    /**
+     * @return the domUser
+     */
+    public DomFullUser getDomFullUser() {
+        return domFullUser;
+    }
+
+    /**
+     * @param domUser the domUser to set
+     */
+    public void setDomFullUser(DomFullUser domUser) {
+        this.domFullUser = domUser;
+    }
+}
