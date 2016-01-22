4a54b008c977bd97063325096e00abe6e5824981
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-01-22 13:46:27 +0000

    added SchoolsClassList entities for new Rest function.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-commons-lib/branches/working@11505 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/commons/dom/entities/DomNewSchoolClass4Student.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.dwo.commons.dom.entities;
+
+import fi.dwo.commons.persistence.entities.PersistentSchoolClass;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Schools and classes transported over the REST interface.
+ *
+ * @author G.A.J. van der Plas
+ */
+@XmlRootElement
+public class DomNewSchoolClass4Student extends DomSchoolClass {
+
+    private String registrationKey = null;
+
+    public DomNewSchoolClass4Student(){
+        
+    }
+
+    
+    public DomNewSchoolClass4Student(PersistentSchoolClass sc) {
+        super(sc);
+        registrationKey = sc.getRegistrationKey();
+    }
+
+
+    /**
+     * @return the registrationKey
+     */
+    public String getRegistrationKey() {
+        return registrationKey;
+    }
+
+    /**
+     * @param registrationKey the registrationKey to set
+     */
+    public void setRegistrationKey(String registrationKey) {
+        this.registrationKey = registrationKey;
+    }
+
+}
