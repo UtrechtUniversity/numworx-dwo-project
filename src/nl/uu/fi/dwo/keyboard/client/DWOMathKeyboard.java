58c928bfc8163e54a0318cf9dca2d4cda5be9352
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-01-25 19:00:35 +0000

    enter
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/KeyboardGWT/trunk@11516 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/keyboard/client/DWOMathKeyboard.java
+++ src/nl/uu/fi/dwo/keyboard/client/DWOMathKeyboard.java
@@ -7,6 +7,7 @@ import nl.uu.fi.dwo.interaction.client.FormuleEditorIF;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -107,4 +108,10 @@ public class DWOMathKeyboard extends AbstractKeyboard {
 	int getKeyboardHeight() {
 		return HEIGHT;
 	}
+
+	@Override
+	void setEnterImage(ImageResource resource) {
+		pad.setEnterImage(resource);
+	}
+	
 }
