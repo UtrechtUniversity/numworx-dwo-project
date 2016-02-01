1fb0f5f6a71aea63543a186efc5ced2a9064b65e
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-02-01 14:43:59 +0000

    working on remove schoolClass
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-commons-lib/branches/working@11581 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/commons/rest/entities/RestRemoveTeacherFromSchoolClass.java
+++ src/main/java/fi/dwo/commons/rest/entities/RestRemoveTeacherFromSchoolClass.java
@@ -4,6 +4,7 @@
 package fi.dwo.commons.rest.entities;
 
 import fi.dwo.commons.dom.entities.DomContext;
+import fi.dwo.commons.dom.entities.DomRemoveTeacherFromSchoolClass;
 import fi.dwo.commons.dom.entities.DomSchoolClass;
 import fi.dwo.commons.dom.entities.DomTeacher;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -14,37 +15,8 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 public class RestRemoveTeacherFromSchoolClass {
-    private DomTeacher teacher;
-    private DomSchoolClass schoolClass;
+    private DomRemoveTeacherFromSchoolClass domRemoveTeacherFromSchoolClass;
     private DomContext restContext;
-
-    /**
-     * @return the teacher
-     */
-    public DomTeacher getTeacher() {
-        return teacher;
-    }
-
-    /**
-     * @param teacher the teacher to set
-     */
-    public void setTeacher(DomTeacher teacher) {
-        this.teacher = teacher;
-    }
-
-    /**
-     * @return the schoolClass
-     */
-    public DomSchoolClass getSchoolClass() {
-        return schoolClass;
-    }
-
-    /**
-     * @param schoolClass the schoolClass to set
-     */
-    public void setSchoolClass(DomSchoolClass schoolClass) {
-        this.schoolClass = schoolClass;
-    }
 
     /**
      * @return the restContext
@@ -58,5 +30,19 @@ public class RestRemoveTeacherFromSchoolClass {
      */
     public void setRestContext(DomContext restContext) {
         this.restContext = restContext;
+    }
+
+    /**
+     * @return the domRemoveTeacherFromSchoolClass
+     */
+    public DomRemoveTeacherFromSchoolClass getDomRemoveTeacherFromSchoolClass() {
+        return domRemoveTeacherFromSchoolClass;
+    }
+
+    /**
+     * @param domRemoveTeacherFromSchoolClass the domRemoveTeacherFromSchoolClass to set
+     */
+    public void setDomRemoveTeacherFromSchoolClass(DomRemoveTeacherFromSchoolClass domRemoveTeacherFromSchoolClass) {
+        this.domRemoveTeacherFromSchoolClass = domRemoveTeacherFromSchoolClass;
     }
 }
