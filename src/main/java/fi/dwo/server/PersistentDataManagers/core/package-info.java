2cf8e424e7809b0aefcdab26e19d42166faad87a
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-03-04 08:22:37 +0000

    updated package info with explanatory constraints.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/branches/working@11919 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/PersistentDataManagers/core/package-info.java
+++ src/main/java/fi/dwo/server/PersistentDataManagers/core/package-info.java
@@ -6,8 +6,14 @@
  * 
  * <p>These data-managers currently do CRUD updates on individual operations. 
  * The entity manages in this package are structured in a specific way and should 
- * not be refactored lightly. They are designed to be refactored when generic (static)
- * methods can be properly used by the persistence framework in use.</p>
+ * not be refactored lightly. They can be refactored when generic (static)
+ * methods can be properly used by the persistence framework in use. However when
+ * opting to use multiple persistence units one should maintain the basic design.
+ * One feature for which one might use multiple persistence units is to store large
+ * data blobs,jars and media on separate storage servers.
+ * 
+ * 
+ * </p>
  */
 package fi.dwo.server.PersistentDataManagers.core;
 
