a71f4ef350a1234929068ee5ad3fc494f59b2e58
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-03-17 12:28:04 +0000

    added manage schoolclasses panel for schooladmin
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOJAppletClient/branches/working@12123 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/dwojapplet/gui/SchoolClassesSchoolAdminPanelTableModel.java
@@ -0,0 +1,154 @@
+/*Copyrighted 2015. */
+package fi.dwo.dwojapplet.gui;
+
+import fi.dwo.commons.dom.entities.DomSchoolClass;
+import fi.dwo.commons.exceptions.Dwo2Exception;
+import fi.dwo.commons.system.TextMapper;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Gert van der Plas <gertvdplas@gmail.com>
+ */
+class SchoolClassesSchoolAdminPanelTableModel extends AbstractTableModel {
+
+    private String[] columnNames = {TextMapper.getText(TextMapper.GUIC_TBL_CLASSNAME),
+        TextMapper.getText(TextMapper.TBL_EDITCLASS),
+        TextMapper.getText(TextMapper.TBL_EDITSTUDENTS),
+        TextMapper.getText(TextMapper.TBL_EDITTEACHERS),
+        TextMapper.getText(TextMapper.TBL_DELETE)};
+
+    static boolean DEBUG = false;
+    private SchoolClassesSchoolAdminPanelProperties prop;
+
+    private int selectedRow, selectedColumn;
+
+    private Object[][] data;
+
+    public void init(SchoolClassesSchoolAdminPanelProperties props, Image editClassImage, Image editStudentsImage, Image editTeachersImage, Image removeImage) throws Dwo2Exception {
+
+        prop = props;
+        List<DomSchoolClass> scList = prop.getSchoolClassList();
+        int rows = 0;
+        if (scList == null) {
+            scList = new ArrayList<DomSchoolClass>();
+        }
+
+        for (DomSchoolClass sc : scList) {
+            rows++; // one for each item in List
+        }
+
+        data = new Object[rows][6];
+        int j = 0;
+        for (DomSchoolClass sc : scList) {
+            data[j][0] = sc.getSchoolClassName();
+            data[j][1] = editClassImage;
+            data[j][2] = editStudentsImage;
+            data[j][3] = editTeachersImage;
+            data[j][4] = removeImage;
+            data[j][5] = sc;
+            j++;
+        }
+        fireTableDataChanged();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public int getRowCount() {
+        return data.length;
+    }
+
+    @Override
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        return data[row][col];
+    }
+
+    /*
+     * JTable uses this method to determine the default renderer/ editor for
+     * each cell. If we didn't implement this method, then the last column
+     * would contain text ("true"/"false"), rather than a check box.
+     */
+    @Override
+    public Class getColumnClass(int c) {
+        return getValueAt(0, c).getClass();
+    }
+
+    /*
+     * Don't need to implement this method unless your table's editable.
+     */
+    @Override
+    public boolean isCellEditable(int row, int col) {
+        //Note that the data/cell address is constant,
+        //no matter where the cell appears onscreen.
+        if (col < 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    /*
+     * Don't need to implement this method unless your table's data can
+     * change.
+     */
+    @Override
+    public void setValueAt(Object value, int row, int col) {
+        //don't change any setting, but update selected values.
+        setSelectedRow(row);
+        setSelectedColumn(col);
+    }
+
+//    private void printDebugData() {
+//      int numRows = getRowCount();
+//      int numCols = getColumnCount();
+//
+//      for (int i = 0; i < numRows; i++) {
+//        System.out.print("    row " + i + ":");
+//        for (int j = 0; j < numCols; j++) {
+//          System.out.print("  " + data[i][j]);
+//        }
+//        System.out.println();
+//      }
+//      System.out.println("--------------------------");
+//    }
+    /**
+     * @return the selectedRow
+     */
+    public int getSelectedRow() {
+        return selectedRow;
+    }
+
+    /**
+     * @param selectedRow the selectedRow to set
+     */
+    public void setSelectedRow(int selectedRow) {
+        this.selectedRow = selectedRow;
+    }
+
+    /**
+     * @return the selectedColumn
+     */
+    public int getSelectedColumn() {
+        return selectedColumn;
+    }
+
+    /**
+     * @param selectedColumn the selectedColumn to set
+     */
+    public void setSelectedColumn(int selectedColumn) {
+        this.selectedColumn = selectedColumn;
+    }
+}
