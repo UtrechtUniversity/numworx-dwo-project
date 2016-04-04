d08223f7e4de58bc3b6544c0b38e69dc256ada68
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-04-04 10:43:44 +0000

    #209	 part one fixed.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/branches/working@12261 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/server/rest/PublicScoDataManager.java
@@ -0,0 +1,69 @@
+package fi.dwo.server.rest;
+
+import fi.dwo.commons.exceptions.Dwo2ExceptionCode;
+import fi.dwo.commons.exceptions.Dwo2RestException;
+import fi.dwo.commons.persistence.entities.PersistentScoData;
+import fi.dwo.server.PersistentDataManagers.core.ScoDataManager;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.DataFormatException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.Inflater;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+/**
+ * Handles the public registration of new users.
+ *
+ * @author G.A.J. van der Plas
+ */
+@Path("/public/scoData")
+public class PublicScoDataManager {
+
+    private static final Logger LOG = Logger.getLogger(PublicScoDataManager.class.getName());
+
+    /**
+     * Verifies that a user, password combination. Waits a configured amount of
+     * time before giving a response.
+     *
+     * @param loginCheck
+     * @return
+     */
+    @GET
+    @Produces({"text/plain"})
+    @Path("/getJSONLaunchDataBytes")
+    @Deprecated
+    public String getJSONLaunchDataBytes(@QueryParam("scoId") int scoId) {
+
+            PersistentScoData scoData = ScoDataManager.findEntity(Integer.valueOf(scoId).longValue());
+            byte[] launchData = scoData.getLaunchdatabytes();
+
+            byte[] buffer = new byte[1024];
+
+            try {
+                ByteArrayInputStream inStream = new ByteArrayInputStream(launchData);
+                ByteArrayOutputStream outStream = new ByteArrayOutputStream(launchData.length);
+                GZIPInputStream gzIn = new GZIPInputStream(inStream);
+
+                int len;
+                while ((len = gzIn.read(buffer)) > 0) {
+                    outStream.write(buffer, 0, len);
+                }
+
+                gzIn.close();
+                outStream.close();
+
+                return outStream.toString();
+            }
+            catch (IOException ex) {
+                LOG.log(Level.SEVERE, "Error while unzipping launchdata with scoid " + scoId + ".", ex);
+            }
+            throw new Dwo2RestException(Dwo2ExceptionCode.Rest_InternalError, "Error while unzipping launchdata with scoid " + scoId + ".");
+        }
+    }
