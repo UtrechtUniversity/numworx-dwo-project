4aa1b69fb5cd6fd70fd4acbd7d0bff2844b1b348
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-04-05 11:20:34 +0000

    trunk working
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-commons-lib/trunk@12294 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/commons/util/RandomPasswordGenerator.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fi.dwo.commons.util;
+import java.security.SecureRandom;
+
+/**
+ * RandomPasswordGenerator for DWO allows to set and select characters to be used
+ * for generation.
+ * 
+ * @author G.A.J. van der Plas
+ */
+public class RandomPasswordGenerator {
+
+    private static RandomPasswordGenerator _instance;
+    private final char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+    StringBuilder sb;
+//
+//    RandomPasswordGenerator() {
+//        //sb = new StringBuilder();
+//    }
+
+    public String Generate(int size) {
+
+        SecureRandom  random = new SecureRandom();
+        sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+    
+    
+     /**
+     * Returns an instance of PersistenceFacade.
+     * @return fi.dwo.client.persistence.PersistenceFacade
+     *  
+     */
+    public static RandomPasswordGenerator instance() {
+        if (_instance == null) {
+            _instance = new RandomPasswordGenerator();
+        }
+        return _instance;
+    }
+}
