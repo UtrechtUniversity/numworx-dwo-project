bac62dc3e6fd43c5a9a1868b0855a70e8e9f422e
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-04-07 09:15:15 +0000

    capturing mouse event
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/DWOplayer@12356 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/formule/client/formuleholder/FormuleEditorTouchHandler.java
+++ src/nl/uu/fi/dwo/formule/client/formuleholder/FormuleEditorTouchHandler.java
@@ -4,8 +4,12 @@ import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.EventTarget;
+import com.google.gwt.user.client.Event;
 import com.googlecode.mgwt.dom.client.event.touch.TouchCancelEvent;
 import com.googlecode.mgwt.dom.client.event.touch.TouchEndEvent;
+import com.googlecode.mgwt.dom.client.event.touch.TouchEvent;
 import com.googlecode.mgwt.dom.client.event.touch.TouchHandler;
 import com.googlecode.mgwt.dom.client.event.touch.TouchMoveEvent;
 //import com.googlecode.mgwt.dom.client.event.touch.TouchStartEvent;
@@ -26,6 +30,7 @@ public class FormuleEditorTouchHandler implements TouchHandler
 	final HashMap<String, Double> dif = new HashMap<String, Double>();
 	int x,y;
 	boolean soft;
+	Element capture;
 	
 	public FormuleEditorTouchHandler(FormuleHolder editor)
 	{
@@ -42,6 +47,10 @@ public class FormuleEditorTouchHandler implements TouchHandler
 		{
 			event.preventDefault();
 			event.stopPropagation();
+		} else {
+			// Mouse simulatie: capture mouse
+			capture = getElement(event);
+			Event.setCapture(capture);
 		}
 		
 		try
@@ -107,7 +116,8 @@ public class FormuleEditorTouchHandler implements TouchHandler
 		{
 			event.preventDefault();
 			event.stopPropagation();
-		}	
+		}
+		release();
 		try
 		{
 			int x = event.getChangedTouches().get(0).getPageX() - editor.getCanvas().getAbsoluteLeft();
@@ -122,10 +132,22 @@ public class FormuleEditorTouchHandler implements TouchHandler
 		
 	}
 
+	private Element getElement(TouchEvent event) {
+		EventTarget target = event.getNativeEvent().getCurrentEventTarget();
+		return Element.as(target);
+	}
+
 	@Override
 	public void onTouchCanceled(TouchCancelEvent event)
 	{
-		
+		release();
+	}
+
+	private void release() {
+		if(capture != null)
+		{	Event.releaseCapture(capture);
+			capture = null;
+		}
 	}
 
 }
