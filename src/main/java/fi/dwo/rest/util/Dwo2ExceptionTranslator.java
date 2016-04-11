03c0b17a0430d38a3a7340bb7e53990e4e854c5b
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-04-11 06:53:50 +0000

    strange
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-rest-lib/branches/working@12430 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/rest/util/Dwo2ExceptionTranslator.java
+++ src/main/java/fi/dwo/rest/util/Dwo2ExceptionTranslator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fi.dwo.rest.util;
+package fi.dwo.rest.util; 
 
 import fi.dwo.rest.exceptions.Dwo2ExceptionCode;
 import java.util.logging.Logger;
