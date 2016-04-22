39c2b4cfc19fbb5dd96821e1dde7477d50f775f7
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-04-22 10:56:56 +0000

    Epsilon Chat
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/DWOplayer@12719 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- war/scripts/mc2.js
+++ war/scripts/mc2.js
@@ -8,6 +8,6 @@ var mc2 = {
 		"ewcbook.EpsiAplusixWidget": "http://www.epsilonwriter.com/GWT/EpsiAplusix.php",
 		"ewcbook.EpsiDrawWidget": "http://www.epsilonwriter.com/GWT/EpsiDraw.php",
 		"flwidget.FLWidget": "http://hansen.dcs.bbk.ac.uk/fl_DME/public_html/host.php",
-		"ewcbook.EpsilonChatWidget", "http://www.epsilonwriter.com/GWT/EpsilonChat.php",
+		"ewcbook.EpsilonChatWidget": "http://www.epsilonwriter.com/GWT/EpsilonChat.php"
 
 }
