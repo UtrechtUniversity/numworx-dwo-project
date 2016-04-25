a6962f1d59ac8d6cda2961e2156f5b0ecaf75572
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-04-25 14:45:27 +0000

    stash
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-gwt-lib/trunk@12760 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/dwo/gwt/lib/rest/client/SecuredUserAccountRestCaller.java
@@ -0,0 +1,34 @@
+package fi.dwo.gwt.lib.rest.client;
+
+import fi.dwo.gwt.lib.rest.CallManagers.Callback;
+import fi.dwo.rest.dom.entities.DomRole;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+
+import org.fusesource.restygwt.client.MethodCallback;
+import org.fusesource.restygwt.client.RestService;
+
+import fi.dwo.rest.dom.entities.DomSchoolsRolesAndClasses;
+import fi.dwo.rest.dom.entities.DomUserFull;
+import fi.dwo.rest.entities.RestLoginCheck;
+import java.util.List;
+
+public interface SecuredUserAccountRestCaller extends RestService {
+	@PUT
+	@Path("/public/user/loginCheck")
+	public void loginCheck(RestLoginCheck arg, MethodCallback<Boolean> callback);
+	
+	@GET
+	@Path("/secure/user/account/get")
+	public void getCurrentUser(MethodCallback<DomUserFull> callback);
+
+	@GET
+	@Path("/secure/user/account/logins/getList")
+	public void getSchoolLogins(MethodCallback<DomSchoolsRolesAndClasses> callback);
+
+	@GET
+	@Path("/public/roles/getList")
+	public void getRoles(Callback<List<DomRole>> callback);
+	
+}
