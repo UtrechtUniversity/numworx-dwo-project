e1a73bc81bc3d8ff28b4c46ead4c16c7eb2ede84
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-04-28 13:13:02 +0000

    stash
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-gwt-lib/trunk@12806 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/dwo/gwt/lib/rest/client/SecuredUserAccountManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.dwo.gwt.lib.rest.client;
+
+import fi.dwo.rest.dom.SecuredUserAccountManagerInterface;
+
+/**
+ * SecuredUserAccountManager uses Call
+ * @author G.A.J. van der Plas
+ */
+public class SecuredUserAccountManager implements SecuredUserAccountManagerInterface {
+    //manager...
+    @Override
+    public void Test(){
+        
+    }
+}
