d723a6c7bed7d31150c051f77095de68a26eac6a
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-05-10 12:59:21 +0000

    working profile changes, should test password test.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/branches/Account@12849 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/account/client/DwoViewer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.uu.fi.dwo.account.client;
+
+import com.google.gwt.user.client.Window;
+import fi.dwo.gwt.lib.rest.DwoGlobalVars;
+import fi.dwo.rest.exceptions.Dwo2ExceptionCode;
+import fi.dwo.rest.util.Dwo2ExceptionTranslator;
+
+/**
+ * Creates or shows dynamic GUI components.
+ * @author G.A.J. van der Plas
+ */
+public class DwoViewer {
+    public static void showMessage(Dwo2ExceptionCode code){
+        Window.alert(Dwo2ExceptionTranslator.getLocalizedCodeExplanation(DwoGlobalVars.instance().getDwoLocale(), code));
+    }
+}
