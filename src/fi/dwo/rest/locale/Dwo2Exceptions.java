a67a674b3c291bd99491b926bbace65f93251255
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-05-19 14:23:47 +0000

    added locales to replace TextMapper in the future.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-gwt-lib/trunk@12915 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/rest/locale/Dwo2Exceptions.java
+++ src/fi/dwo/rest/locale/Dwo2Exceptions.java
@@ -2,7 +2,7 @@ package fi.dwo.rest.locale;
 
 /**
  * Interface to represent the messages contained in resource bundle:
- * 	D:/GertvanderPlas/netbeans-checkout/project.fisme.java/DWO-gwt-lib/trunk/src/fi/dwo/rest/locale/Dwo2Exceptions.properties'.
+ * 	/Users/gert/NetBeansProjects/UU/DWO-gwt-lib/trunk/src/fi/dwo/rest/locale/Dwo2Exceptions.properties'.
  */
 public interface Dwo2Exceptions extends com.google.gwt.i18n.client.Messages {
   
---
+++ src/fi/dwo/rest/locale/Dwo2Locale.java
@@ -0,0 +1,8 @@
+package fi.dwo.rest.locale;
+
+/**
+ * Interface to represent the messages contained in resource bundle:
+ * 	/Users/gert/NetBeansProjects/UU/DWO-gwt-lib/trunk/src/fi/dwo/rest/locale/Dwo2Exceptions.properties'.
+ */
+public interface Dwo2Locale extends com.google.gwt.i18n.client.Messages {
+  }
