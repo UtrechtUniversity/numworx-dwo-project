c1b459e50d5b8d8d02a106bf1f5fa106dddeea39
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-06-20 08:15:54 +0000

    Added RestMessage for double schoolclass name error.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-rest-lib/trunk@13096 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/rest/exceptions/Dwo2ExceptionCode.java
+++ src/main/java/fi/dwo/rest/exceptions/Dwo2ExceptionCode.java
@@ -4,14 +4,15 @@ package fi.dwo.rest.exceptions;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- * Statically typed Dwo2 exception codes for which a localized description can be found.
- * 
+ * Statically typed Dwo2 exception codes for which a localized description can
+ * be found.
+ *
  * @author Gert van der Plas
  */
 @XmlRootElement
 public enum Dwo2ExceptionCode {
     User_AuthenticationError, //Illegal account details.
-     User_IllegalAction, //Illegal action logged.
+    User_IllegalAction, //Illegal action logged.
 
     //REST interface errors
     Rest_InternalError, //Internal software error, a stack trace should be acquired.
@@ -19,11 +20,12 @@ public enum Dwo2ExceptionCode {
     Rest_ConnectionTimeout, //Connection time-out to REST-interface.
 
     // REST Registration errors
-    Rest_Registration_UserName_exists,  //User exists already, can't register.
+    Rest_Registration_UserName_exists, //User exists already, can't register.
     Rest_Registration_Invalid_school_role_credentials, //Illegal combination of school login and passcode.
-    Rest_Registration_School_authentication_failed, 
-    Rest_Registration_School_license_expired,  
+    Rest_Registration_School_authentication_failed,
+    Rest_Registration_School_license_expired,
     Rest_Registration_hasRole_exists, //User is already registered for this role.
+    Rest_Submitted_SchoolClass_exists,//A school
     
     //GUI
     GUI_NoUserIsSignedIn,
