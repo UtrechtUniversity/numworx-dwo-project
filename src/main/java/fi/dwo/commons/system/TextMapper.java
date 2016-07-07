055a3ca6ea2e471e5e309f8d14191ba623d83b3a
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-07-07 08:36:25 +0000

    bug fix.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-commons-lib/trunk@13237 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/commons/system/TextMapper.java
+++ src/main/java/fi/dwo/commons/system/TextMapper.java
@@ -95,6 +95,7 @@ public abstract class TextMapper {
 	public static final String DLG_CREATESTUDENTERROR = "DLG_CREATESTUDENTERROR";
 	public static final String DLG_CREATETEACHERERROR = "DLG_CREATETEACHERERROR";
         public static final String DLG_LOSE_NEW_STUDENT_ACCOUNTS = "DLG_LOSE_NEW_STUDENT_ACCOUNTS";
+        public static final String DLG_LOSE_NEW_TEACHER_ACCOUNTS = "DLG_LOSE_NEW_TEACHER_ACCOUNTS";
 
 	public static final String DLG_ENTER_INPUT = "DLG_ENTER_INPUT";
 	/* Exception Messages */
