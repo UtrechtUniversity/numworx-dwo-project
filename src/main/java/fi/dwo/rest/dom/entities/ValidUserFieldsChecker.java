cde64d79b6ce8a02e6bfecb8204b9d287f3c3bc0
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-07-18 14:38:23 +0000

    added UserfieldsChecker to test for valid userfields.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-rest-lib/trunk@13344 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/rest/dom/entities/ValidUserFieldsChecker.java
@@ -0,0 +1,71 @@
+package fi.dwo.rest.dom.entities;
+
+/**
+ * ValidUserFieldsChecker checks the class for valid user fields. Currently it
+ * checks the username characters and size, and the email characters.
+ * 
+ * @author Gert van der Plas
+ */
+public final class ValidUserFieldsChecker {
+    public static boolean isValidPassword(String password){
+        if(password==null) return false;
+        if(password.length()==128) return false;
+        return true;
+    }
+    
+    /**
+     *  Tests for RFC 5322 addresses, but not RFC 6530
+     * @param email
+     * @return 
+     */
+    public static boolean isValidEmail(String email) {
+// JavaMail package approach
+//        boolean result = true;
+//        try {
+//            InternetAddress emailAddr = new InternetAddress(email);
+//            emailAddr.validate();
+//        }
+//        catch (AddressException ex) {
+//            result = false;
+//        }
+//        return result;
+//from http://stackoverflow.com/questions/624581/what-is-the-best-java-email-address-validation-method
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+    public static boolean isValidUserName(String username) {
+
+        if (!username.trim().equals(username)) {
+            return false;
+        }
+        char[] chars = username.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c < 0x20 || c >= 0x7F // ascii, no space?, no delete?
+                    || c == '(' // aselect verbiedt =*?
+                    || c == ')' // maar ook , \ ( en ) mogen niet
+                    || c == '*' || c == '?' || c == '=' || c == '\\' || c == ',' || c == ';' // beter
+                    // van
+                    // niet
+                    // in
+                    // LDAP
+                    || c == '+' || c == '#' // nieuw, werkt niet in PHP
+                    ) {
+                return false;
+            }
+        }
+//    private static final String[] realms = {"@kennisnet.org", "@fi.uu.nl", "@w2k3.fi.uu.nl", "@soliscom.uu.nl"};
+//
+//        for (String realm : realms) {
+//            if (username.endsWith(realm)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+        return true;
+    }    
+}
