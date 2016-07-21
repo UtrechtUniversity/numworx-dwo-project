01053cc2c7e4fe16ac1141afa3927639d147cce4
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-07-21 09:10:07 +0000

    code cleanup
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-gwt-lib/trunk@13393 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/gwt/lib/rest/CallManagers/SecuredUserSchoolLoginManager.java
+++ src/fi/dwo/gwt/lib/rest/CallManagers/SecuredUserSchoolLoginManager.java
@@ -5,7 +5,6 @@
  */
 package fi.dwo.gwt.lib.rest.CallManagers;
 
-import fi.dwo.gwt.lib.rest.util.RestAuthenticator;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
