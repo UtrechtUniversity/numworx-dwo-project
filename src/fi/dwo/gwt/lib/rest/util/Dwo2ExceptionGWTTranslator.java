04b034909e639338118981f914af54ab95bbf70d
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-07-21 07:08:19 +0000

    catch missing resource
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-gwt-lib/trunk@13389 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/gwt/lib/rest/util/Dwo2ExceptionGWTTranslator.java
+++ src/fi/dwo/gwt/lib/rest/util/Dwo2ExceptionGWTTranslator.java
@@ -15,6 +15,7 @@ import fi.dwo.rest.exceptions.Dwo2ExceptionCode;
 import fi.dwo.rest.locale.Dwo2ExceptionsForGWT;
 import fi.dwo.rest.util.DWO2ExceptionTranslatorInterface;
 
+import java.util.MissingResourceException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -52,18 +53,20 @@ public class Dwo2ExceptionGWTTranslator implements DWO2ExceptionTranslatorInterf
     @Override
     public String getLocalizedCodeExplanation(DwoLocale locale, Dwo2ExceptionCode code) {
         String msg;
-        LOG.log(Level.INFO,"Curent locale is : "+locale.toString());
+        msg = code.name();
         try {
             //Current resources are in /java/resources, however if in java/resources/fi/dwo then
             //replace getBundle("Dwo2Exceptions", locale); with getBundle("fi.dwo.Dwo2Exceptions", locale);
             //ResourceBundle localeLookup = ResourceBundle.getBundle("Dwo2Exceptions", locale);
             //msg = localeLookup.getString(Dwo2ExceptionCode.class.getSimpleName() + "." + code.name());
             
-            msg = code.name();
             msg = Dwo2ExceptionsForGWT.instance.getString("Dwo2ExceptionCode_"+msg);
         }
+        catch (MissingResourceException m) {
+            LOG.log(Level.SEVERE, "Can't find Dwo2ExceptionCode_" + code.name(), m);
+        }
         catch (Exception e) {
-            LOG.log(Level.SEVERE, "Can't find the resource Dwo2Exceptions.properties, returning English log message.", e);
+            LOG.log(Level.SEVERE, "Can't find the resource Dwo2Exceptions.properties, returning English log message. " + code.name(), e);
             msg = "Internal error reading Dwo2Exception locale properties.";
         }
         return msg;
