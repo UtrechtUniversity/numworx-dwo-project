5938656902867d9dc623b57ef5254b6df98f48bc
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-07-21 06:38:41 +0000

    added 401 error for logging out in basicauthentication from gwt.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/trunk@13388 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/rest/SecuredRestTestManager.java
+++ src/main/java/fi/dwo/server/rest/SecuredRestTestManager.java
@@ -1,7 +1,14 @@
 package fi.dwo.server.rest;
 
+import fi.dwo.rest.exceptions.Dwo2ExceptionCode;
+import fi.dwo.rest.exceptions.Dwo2RestException;
 import java.util.logging.Logger;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  * REST functions that allows one to test the proper catching of HTML-errors.
@@ -22,15 +29,16 @@ public class SecuredRestTestManager {
 //        return "{NoError!}";
 //    }
 //
-//    @GET
-//    @Produces({"application/json"})
-//    @Path("/test400Error/json")
-//    public Response test400Error(@Context SecurityContext sc) {
-//        String userName = sc.getUserPrincipal().getName();
-//        //TODO REST update lastLogin and such.
-//        Dwo2RestException e = new Dwo2RestException(Dwo2ExceptionCode.User_AuthenticationError, "DwoRestException 400 thrown on request of user: " + userName + ".");
-//        throw e;
-//    }
+    @GET
+    @Produces({"application/json"})
+    @Path("/test401Error/json")
+    public Response test401Error(@Context SecurityContext sc) {
+        String userName = sc.getUserPrincipal().getName();
+        //TODO REST update lastLogin and such.
+        Dwo2RestException e = new Dwo2RestException(Dwo2ExceptionCode.User_AuthenticationError, "DwoRestException 400 thrown on request of user: " + userName + ".");
+        Response r = Response.status(401).entity(e.getMessage()).build();
+        return r;
+    }
 //
 //    @GET
 //    @Produces({"application/json"})
