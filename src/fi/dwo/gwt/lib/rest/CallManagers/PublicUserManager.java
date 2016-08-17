a720391ed973b605695b7159ce9d4adf64e45923
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-08-17 14:02:43 +0000

    eod
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-gwt-lib/trunk@13746 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/dwo/gwt/lib/rest/CallManagers/PublicUserManager.java
@@ -0,0 +1,34 @@
+package fi.dwo.gwt.lib.rest.CallManagers;
+
+import org.fusesource.restygwt.client.Defaults;
+import org.fusesource.restygwt.client.dispatcher.DefaultFilterawareDispatcher;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import fi.dwo.gwt.lib.rest.GwtRestVars;
+import fi.dwo.gwt.lib.rest.client.PublicUserRestCaller;
+import fi.dwo.rest.dom.entities.DomContext;
+import fi.dwo.rest.dom.entities.DomNewUser;
+import fi.dwo.rest.entities.RestNewUser;
+
+public class PublicUserManager {
+	
+	PublicUserRestCaller caller = GWT.create(PublicUserRestCaller.class);
+
+	public void RegisterNewUser(DomNewUser domNewUser,
+			AsyncCallback<Boolean> asyncCallback) {
+		RestNewUser user = new RestNewUser();
+		user.setDomNewUser(domNewUser);
+		user.setRestContext(new DomContext());
+		instance.setCurrentUser(null);
+		caller.submitNewUser(user, new Callback<Boolean>(asyncCallback));
+		
+	}
+	
+	GwtRestVars instance;
+	
+	public PublicUserManager() {	
+		instance = GwtRestVars.instance();
+	}
+}
---
+++ src/fi/dwo/gwt/lib/rest/client/PublicUserRestCaller.java
@@ -0,0 +1,17 @@
+package fi.dwo.gwt.lib.rest.client;
+
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+
+import org.fusesource.restygwt.client.MethodCallback;
+import org.fusesource.restygwt.client.RestService;
+
+import fi.dwo.rest.entities.RestNewUser;
+
+public interface PublicUserRestCaller extends RestService {
+
+	@PUT
+    @Path("/public/user/submit")
+    public void submitNewUser(RestNewUser restNewUser, MethodCallback<Boolean> callback);
+
+}
