7de541d136dd57d9aea236388bdf19675f480ef4
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-09-08 05:35:20 +0000

    beta function
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/trunk@13899 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/PersistentDataManagers/util/UserUtilManager.java
+++ src/main/java/fi/dwo/server/PersistentDataManagers/util/UserUtilManager.java
@@ -3,19 +3,27 @@
  */
 package fi.dwo.server.PersistentDataManagers.util;
 
+import fi.dwo.commons.persistence.entities.PersistentSchool;
 import fi.dwo.rest.exceptions.Dwo2Exception;
 import fi.dwo.rest.exceptions.Dwo2ExceptionCode;
 import fi.dwo.commons.persistence.entities.PersistentSchoolClass;
+import fi.dwo.commons.persistence.entities.PersistentSchoolGroup;
 import fi.dwo.commons.persistence.entities.PersistentStudentOfClass;
 import fi.dwo.commons.persistence.entities.PersistentTeacherOfClass;
 import fi.dwo.commons.persistence.entities.PersistentUser;
+import fi.dwo.rest.dom.entities.RoleType;
+import fi.dwo.rest.exceptions.Dwo2RestException;
+import fi.dwo.server.PersistentDataManagers.core.SchoolGroupManager;
 import fi.dwo.server.PersistentDataManagers.core.StudentOfClassManager;
 import fi.dwo.server.PersistentDataManagers.core.TeacherOfClassManager;
 import fi.dwo.server.PersistentDataManagers.core.UserManager;
+import fi.dwo.server.persistence.DwoEmfFactory;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.persistence.EntityManager;
 
 /**
  * Checks if a user is in a certain role context.
@@ -62,6 +70,40 @@ public class UserUtilManager {
             stdList.add(u);
         }
         return  stdList;
+    }
+    
+    /**
+     * untested function
+     * 
+     * 
+     * @param school
+     * @param role
+     * @return
+     * @throws Dwo2Exception 
+     */
+        public static List<PersistentUser> getUsersInSchoolwithRole(PersistentSchool school, RoleType role) throws Dwo2Exception {
+        EntityManager em = DwoEmfFactory.getEntityManager();
+        if (school == null || role==null) {
+            LOG.log(Level.SEVERE, "user or school is invalid.");
+            throw new Dwo2Exception(Dwo2ExceptionCode.Rest_InternalError, "Illegal parameters.");
+        }
+        PersistentSchoolGroup sg = SchoolGroupManager.findBySchoolAndRole(school, role);
+        try {
+            //           school = SchoolManager.findBySchoolLogin(newUserReg.getSchoolLogin());
+            javax.persistence.Query q = em.createQuery(" select u from PersistentUser u join PersistentHasRole h on h.persistentHasRolePK.userID=u.userID where hr.schoolGroupID = :schoolGroupId");
+            q.setParameter("schoolGroupId", sg.getSchoolGroupID());
+            List<PersistentUser> userList =  q.getResultList();
+            LOG.log(Level.FINE, "School-manager retrieved {0} users in school {1} with role {2}.", new Object[]{userList.size(), school.getSchoolID(), role.name()});
+            return userList;
+        }
+        catch (Exception ex) {
+            String msg = MessageFormat.format("Failed retrieving users in school {0} with role {1}.", new Object[]{school.getSchoolID(), role.name()});
+            LOG.log(Level.WARNING, msg, ex);
+            throw new Dwo2RestException(Dwo2ExceptionCode.Rest_Registration_School_authentication_failed, msg);
+        }
+        finally {
+            em.close();
+        }
     }
 
 }
