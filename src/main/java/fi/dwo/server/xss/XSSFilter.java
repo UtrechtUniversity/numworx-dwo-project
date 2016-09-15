8b8da736d4c6d633c82d5a6dba90c721b5b5a76e
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-09-15 11:47:39 +0000

    remove logging
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOServer/trunk@13963 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/xss/XSSFilter.java
+++ src/main/java/fi/dwo/server/xss/XSSFilter.java
@@ -25,7 +25,7 @@ public class XSSFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain) throws IOException, ServletException {
         String method;
-        logger.fine("doFilter called");
+        //logger.fine("doFilter called");
         if (request instanceof HttpServletRequest) {
             method = ((HttpServletRequest) request).getMethod();
             if ("OPTIONS".equals(method) && response instanceof HttpServletResponse) {
