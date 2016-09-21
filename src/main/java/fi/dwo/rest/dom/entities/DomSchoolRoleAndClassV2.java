ca4ac4bb55f2862e18bc66c5d400f8b2327867d7
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-09-21 13:30:02 +0000

    APIVersioning test and modelfix on DomSchoolRoleAndClass
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO-rest-lib/trunk@14003 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/rest/dom/entities/DomSchoolRoleAndClassV2.java
+++ src/main/java/fi/dwo/rest/dom/entities/DomSchoolRoleAndClassV2.java
@@ -41,184 +41,184 @@ public class DomSchoolRoleAndClassV2 {
 //    private String schoolClassName;
 //    private Boolean iconizer;
 
-    /**
-     * @return the schoolId
-     */
-    @Deprecated
-    @JsonIgnore
-    public PersistenceId getSchoolId() {
-        return getSchool().getId();
-    }
-
-    /**
-     * @param schoolId the schoolId to set
-     */
-    @Deprecated
-    @JsonIgnore
-    public void setSchoolId(PersistenceId schoolId) {
-        getSchool().setId(schoolId);
-    }
-
-    /**
-     * @return the schoolName
-     */
-    @Deprecated
-    @JsonIgnore
-    public String getSchoolName() {
-        return getSchool().getSchoolName();
-    }
-
-    /**
-     * @param schoolName the schoolName to set
-     */
-    @Deprecated
-    @JsonIgnore
-    public void setSchoolName(String schoolName) {
-        getSchool().setSchoolName(schoolName);
-    }
-
-    /**
-     * @return the roleId
-     */
-    @Deprecated
-    @JsonIgnore
-    public PersistenceId getRoleId() {
-        return role.getId();
-    }
-
-    /**
-     * @param roleId the roleId to set
-     */
-    @Deprecated
-    @JsonIgnore
-    public void setRoleId(PersistenceId roleId) {
-        role.setId(roleId);
-    }
-
-    /**
-     * @return the roleName
-     */
-    @Deprecated
-    @JsonIgnore
-    public String getRoleName() {
-        return role.getRoleName();
-    }
-
-    /**
-     * @param roleName the roleName to set
-     */
-    @Deprecated
-    @JsonIgnore
-    public void setRoleName(String roleName) {
-        role.setRoleName(roleName);
-    }
-
-    /**
-     * @return the schoolClassId
-     */
-    @Deprecated
-    @JsonIgnore
-    public PersistenceId getSchoolClassId() {
-        return (schoolClass != null) ? schoolClass.getId() : null;
-    }
-
-    /**
-     * @param schoolClassId the schoolClassId to set
-     */
-    @Deprecated
-    @JsonIgnore
-    public void setSchoolClassId(PersistenceId schoolClassId) {
-        schoolClass.setId(schoolClassId);
-    }
-
-    /**
-     * @return the schoolClassName
-     */
-    @Deprecated
-    @JsonIgnore
-    public String getSchoolClassName() {
-        return (schoolClass != null) ? schoolClass.getSchoolClassName() : null;
-    }
-
-    /**
-     * @param schoolClassName the schoolClassName to set
-     */
-    @Deprecated
-    @JsonIgnore
-    public void setSchoolClassName(String schoolClassName) {
-        schoolClass.setSchoolClassName(schoolClassName);
-    }
-
-    /**
-     * @return the userId
-     */
-    @Deprecated
-    @JsonIgnore
-    public PersistenceId getUserId() {
-        return hasRole.getId();
-    }
-
-    /**
-     * @param userId the userId to set
-     */
-    @Deprecated
-    @JsonIgnore
-    public void setUserId(PersistenceId userId) {
-        hasRole.setId(userId);
-    }
-
-    /**
-     * @return the groupId
-     */
-    @Deprecated
-    @JsonIgnore
-    public PersistenceId getSchoolGroupId() {
-        return (hasRole != null) ? hasRole.getSchoolGroupId() : null;
-    }
-
-    /**
-     * @param schoolGroupId
-     */
-    @Deprecated
-    @JsonIgnore
-    public void setSchoolGroupId(PersistenceId schoolGroupId) {
-        hasRole.setSchoolGroupId(schoolGroupId);
-    }
-
-    @Deprecated
-    @JsonIgnore
-    public Boolean getIconizer() {
-        return (schoolClass != null) ? schoolClass.getIconizer() : null;
-    }
-
-    @Deprecated
-    @JsonIgnore
-    public void setIconizer(Boolean iconizer) {
-        schoolClass.setIconizer(iconizer);
-    }
-
-    @Deprecated
-    @JsonIgnore
-    public String getRoleRights() {
-        return (hasRole != null) ? hasRole.getRights() : null;
-    }
-
-    @Deprecated
-    @JsonIgnore
-    public void setRoleRights(String roleRights) {
-        hasRole.setRights(roleRights);
-    }
-
-    @Deprecated
-    public String getSchoolRights() {
-        return (school != null) ? school.getSchoolRights(): null;
-    }
-
-    @Deprecated
-    @JsonIgnore
-    public void setSchoolRights(String schoolRights) {
-        getSchool().setSchoolRights(schoolRights);
-    }
+//    /**
+//     * @return the schoolId
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public PersistenceId getSchoolId() {
+//        return getSchool().getId();
+//    }
+//
+//    /**
+//     * @param schoolId the schoolId to set
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public void setSchoolId(PersistenceId schoolId) {
+//        getSchool().setId(schoolId);
+//    }
+//
+//    /**
+//     * @return the schoolName
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public String getSchoolName() {
+//        return getSchool().getSchoolName();
+//    }
+//
+//    /**
+//     * @param schoolName the schoolName to set
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public void setSchoolName(String schoolName) {
+//        getSchool().setSchoolName(schoolName);
+//    }
+//
+//    /**
+//     * @return the roleId
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public PersistenceId getRoleId() {
+//        return role.getId();
+//    }
+//
+//    /**
+//     * @param roleId the roleId to set
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public void setRoleId(PersistenceId roleId) {
+//        role.setId(roleId);
+//    }
+//
+//    /**
+//     * @return the roleName
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public String getRoleName() {
+//        return role.getRoleName();
+//    }
+//
+//    /**
+//     * @param roleName the roleName to set
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public void setRoleName(String roleName) {
+//        role.setRoleName(roleName);
+//    }
+//
+//    /**
+//     * @return the schoolClassId
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public PersistenceId getSchoolClassId() {
+//        return (schoolClass != null) ? schoolClass.getId() : null;
+//    }
+//
+//    /**
+//     * @param schoolClassId the schoolClassId to set
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public void setSchoolClassId(PersistenceId schoolClassId) {
+//        schoolClass.setId(schoolClassId);
+//    }
+//
+//    /**
+//     * @return the schoolClassName
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public String getSchoolClassName() {
+//        return (schoolClass != null) ? schoolClass.getSchoolClassName() : null;
+//    }
+//
+//    /**
+//     * @param schoolClassName the schoolClassName to set
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public void setSchoolClassName(String schoolClassName) {
+//        schoolClass.setSchoolClassName(schoolClassName);
+//    }
+//
+//    /**
+//     * @return the userId
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public PersistenceId getUserId() {
+//        return hasRole.getId();
+//    }
+//
+//    /**
+//     * @param userId the userId to set
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public void setUserId(PersistenceId userId) {
+//        hasRole.setId(userId);
+//    }
+//
+//    /**
+//     * @return the groupId
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public PersistenceId getSchoolGroupId() {
+//        return (hasRole != null) ? hasRole.getSchoolGroupId() : null;
+//    }
+//
+//    /**
+//     * @param schoolGroupId
+//     */
+//    @Deprecated
+//    @JsonIgnore
+//    public void setSchoolGroupId(PersistenceId schoolGroupId) {
+//        hasRole.setSchoolGroupId(schoolGroupId);
+//    }
+//
+//    @Deprecated
+//    @JsonIgnore
+//    public Boolean getIconizer() {
+//        return (schoolClass != null) ? schoolClass.getIconizer() : null;
+//    }
+//
+//    @Deprecated
+//    @JsonIgnore
+//    public void setIconizer(Boolean iconizer) {
+//        schoolClass.setIconizer(iconizer);
+//    }
+//
+//    @Deprecated
+//    @JsonIgnore
+//    public String getRoleRights() {
+//        return (hasRole != null) ? hasRole.getRights() : null;
+//    }
+//
+//    @Deprecated
+//    @JsonIgnore
+//    public void setRoleRights(String roleRights) {
+//        hasRole.setRights(roleRights);
+//    }
+//
+//    @Deprecated
+//    public String getSchoolRights() {
+//        return (school != null) ? school.getSchoolRights(): null;
+//    }
+//
+//    @Deprecated
+//    @JsonIgnore
+//    public void setSchoolRights(String schoolRights) {
+//        getSchool().setSchoolRights(schoolRights);
+//    }
 
     /**
      * @return the school
