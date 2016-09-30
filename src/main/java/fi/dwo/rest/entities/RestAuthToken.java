d675d9b30410e84030a5897cdf92dd2405f51b3c
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-09-30 06:49:27 +0000

    new class: RestAuthToken
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-rest-lib/trunk@14192 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/rest/entities/RestAuthToken.java
@@ -0,0 +1,37 @@
+package fi.dwo.rest.entities;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import fi.dwo.rest.dom.entities.DomContext;
+
+@XmlRootElement
+public class RestAuthToken {
+	private DomContext restContext;
+	private String authToken;
+	/**
+	 * @return the restContext
+	 */
+	public DomContext getRestContext() {
+		return restContext;
+	}
+	/**
+	 * @param restContext the restContext to set
+	 */
+	public void setRestContext(DomContext restContext) {
+		this.restContext = restContext;
+	}
+	/**
+	 * @return the authToken
+	 */
+	public String getAuthToken() {
+		return authToken;
+	}
+	/**
+	 * @param authToken the authToken to set
+	 */
+	public void setAuthToken(String authToken) {
+		this.authToken = authToken;
+	}
+	
+	
+}
