2cdff5e42008cc73b05fbe19f54c3caae320989a
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-10-12 12:31:17 +0000

    LMS-14
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWOServer/trunk@14380 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/server/PersistentDataManagers/util/SparseResultsUtilManager.java
@@ -0,0 +1,17 @@
+package fi.dwo.server.PersistentDataManagers.util;
+
+import java.util.logging.Logger;
+
+
+/**
+ * The SparseResultsUtilManager returns SpareResultData obtained from complex queries
+ * on the MySQLDatabase.  
+ * 
+ * @author Gert van der Plas
+ */
+public class SparseResultsUtilManager {
+
+    private static final Logger LOG = Logger.getLogger(SparseResultsUtilManager.class.getName());
+    
+    
+}
