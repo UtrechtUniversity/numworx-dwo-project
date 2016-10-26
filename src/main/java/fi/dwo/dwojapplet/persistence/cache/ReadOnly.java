444d52b66151d43036581505177284f41aa5d0cf
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-10-26 13:43:49 +0000

    introductie methode allowWrite(scormvariable)
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWOJClient/trunk@14669 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/dwojapplet/persistence/cache/ReadOnly.java
+++ src/main/java/fi/dwo/dwojapplet/persistence/cache/ReadOnly.java
@@ -13,10 +13,15 @@ public class ReadOnly extends NoCache {
 
     @Override
     public String setValue(int uid, int scoid, int sgid, String key, String value) throws PersistenceException {
- 		if(hasSuspendData && "cmi.completion_status".equals(key)) // verzegelen.
+ 		if(hasSuspendData && allowWrite(key)) // verzegelen.
 			return super.setValue(uid, scoid, sgid, key, value);
 		return "true";
 	}
+
+	private boolean allowWrite(String key) {
+		return "cmi.completion_status".equals(key)||"cmi.comments_from_lms.0.comment".equals(key) || "score".equals(key);
+	}
+	
     @Override
     public String getValue(int uid, int scoid, int sgid, String key) throws PersistenceException {
         if ("suspendData".equals(key) && !hasSuspendData) {
