7d825342452d81792d6030a7affc87067db1b61f
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-11-01 15:50:44 +0000

    eod.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWOServer/trunk@14772 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/PersistentDataManagers/util/CourseUtilManager.java
+++ src/main/java/fi/dwo/server/PersistentDataManagers/util/CourseUtilManager.java
@@ -11,7 +11,5 @@ public class CourseUtilManager {
     public static List<PersistentCourse> getChildCourses(PersistentCourse course) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    
     
 }
