40c1487d0ab77798c75fc9f917abc05819832140
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-11-03 09:03:36 +0000

    #LMS-31 building cycle-safe query.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWOServer/trunk@14794 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/PersistentDataManagers/util/CourseUtilManager.java
+++ src/main/java/fi/dwo/server/PersistentDataManagers/util/CourseUtilManager.java
@@ -1,15 +1,27 @@
 package fi.dwo.server.PersistentDataManagers.util;
 
-import fi.dwo.commons.persistence.entities.PersistentCourse;
-import java.util.List;
 import java.util.logging.Logger;
 
 public class CourseUtilManager {
 
     private static final Logger LOG = Logger.getLogger(CourseUtilManager.class.getName());
 
-    public static List<PersistentCourse> getChildCourses(PersistentCourse course) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+//    public static List<PersistentCourse> getChildCoursesAndSetTreeIndex(Queue<PersistentCourse> courseQueue) {
+//            List<PersistentCourse> courses;
+//            
+//            while (!courseQueue.isEmpty()) {
+//                PersistentCourse course = courseQueue.remove();
+//                if (course.getWithChildren()) {
+//                    List<PersistentCourse> childrenCourses = CourseManager.findChildrenOf(course);
+////                    CourseManager.findEntities(school);
+//                    courseQueue.addAll(childrenCourses);
+//                } else {//leave
+//                    leaves.add(course);
+//                }
+//                DomCourse c = course.createDomCourse();
+//                domCourses.put(c.getId(), c);
+//            }
+//        
+//    }
     
 }
