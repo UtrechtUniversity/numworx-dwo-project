31304fb38eeab654489c3dea086f5ef04000d5e8
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-11-17 10:21:03 +0000

    Rest classes for DwoProfile
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-rest-lib/trunk@14958 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/entities/RestDwoProfile.java
+++ src/main/java/nl/uu/fi/dwo/rest/entities/RestDwoProfile.java
@@ -1,29 +1,9 @@
 package nl.uu.fi.dwo.rest.entities;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
-import nl.uu.fi.dwo.rest.dom.entities.DomContext;
-import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfile;
-
-@XmlRootElement
+/**
+ *
+ * @author G.A.J. van der Plas <G.A.J.vanderPlas@uu.nl>
+ */
 public class RestDwoProfile {
-	private DomContext restContext;
-	private DomDwoProfile domDwoProfile;
-	   
-   public RestDwoProfile() {
-   }
-   
-   public DomContext getRestContext() {
-		return restContext;
-   }
-   public void setRestContext(DomContext restContext) {
-	   this.restContext = restContext;
-   }
-   public DomDwoProfile getDomDwoProfile() {
-	   return domDwoProfile;
-   }
-   public void setDomDwoProfile(DomDwoProfile domDwoProfile) {
-	   this.domDwoProfile = domDwoProfile;
-   }
-	   
+    
 }
