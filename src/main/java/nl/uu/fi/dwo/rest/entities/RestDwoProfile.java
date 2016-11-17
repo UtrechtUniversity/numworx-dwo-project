cea5f439bca8acbdf7642c96ed748bbc9d0bf339
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-11-17 10:28:05 +0000

    Rest classes for DwoProfile
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-rest-lib/trunk@14959 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/entities/RestDwoProfile.java
+++ src/main/java/nl/uu/fi/dwo/rest/entities/RestDwoProfile.java
@@ -1,9 +1,43 @@
 package nl.uu.fi.dwo.rest.entities;
 
+import nl.uu.fi.dwo.rest.dom.entities.DomContext;
+import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfile;
+
 /**
- *
+ * RestDwoProfile contains a minimal DwoProfile info.
+ * 
  * @author G.A.J. van der Plas <G.A.J.vanderPlas@uu.nl>
  */
 public class RestDwoProfile {
+	private DomContext restContext;
+	private DomDwoProfile domDwoProfile;
+
+    /**
+     * @return the restContext
+     */
+    public DomContext getRestContext() {
+        return restContext;
+    }
+
+    /**
+     * @param restContext the restContext to set
+     */
+    public void setRestContext(DomContext restContext) {
+        this.restContext = restContext;
+    }
+
+    /**
+     * @return the domDwoProfile
+     */
+    public DomDwoProfile getDomDwoProfile() {
+        return domDwoProfile;
+    }
+
+    /**
+     * @param domDwoProfile the domDwoProfile to set
+     */
+    public void setDomDwoProfile(DomDwoProfile domDwoProfile) {
+        this.domDwoProfile = domDwoProfile;
+    }
     
 }
