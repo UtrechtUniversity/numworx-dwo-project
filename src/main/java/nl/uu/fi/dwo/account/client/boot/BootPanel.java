07a074e869a249a71658d321b78575eebb3c7fd2
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-11-29 14:06:48 +0000

    GWT TeacherResult
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/Account/trunk@15123 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/account/client/boot/BootPanel.java
@@ -0,0 +1,30 @@
+package nl.uu.fi.dwo.account.client.boot;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ *
+ * @author G.A.J. van der Plas <G.A.J.vanderPlas@uu.nl>
+ */
+public class BootPanel extends Composite implements EntryPoint {
+
+    interface MyUiBinder extends UiBinder<Widget, BootPanel> {
+    }
+    private static final MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+
+    public BootPanel() {
+        initWidget(uiBinder.createAndBindUi(this));
+    }
+
+    @Override
+    public void onModuleLoad() {
+        RootLayoutPanel.get().add(this);
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
