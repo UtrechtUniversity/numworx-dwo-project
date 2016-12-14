467865d79a1ce6e3f31f926708abf794743ee57b
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-12-14 08:13:16 +0000

    refined text in package-info file.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWOServer/trunk@15253 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/rest/package-info.java
+++ src/main/java/fi/dwo/server/rest/package-info.java
@@ -1,18 +1,17 @@
 package fi.dwo.server.rest;
 
 /**
- * Provides the managers that process gui-operations requests a restful interface.
+ * Provides the model controllers that process gui-operations a restful interface.
  * 
  * Class names of classes which are on the /secure context path are pre with 
  * 'Secured'. Other classes are on the context path /public and their class names
  * are prefixed with 'public'.
  * 
- * The REST-methods contain the security checks and often the code for the REST-operation
- * in order to reduce IO/SQL-queries. Common code between methods that should be placed
- * in the fi.dwo.server.PersistentDataManagers.util class if there is no significant
- * performance loss by it.
+ * The server-side REST-methods contain the security checks. They often also contain
+ * the code for the REST-operation in order to reduce IO/SQL-queries. Common code 
+ * between methods that should be placed in the fi.dwo.server.PersistentDataManagers.util 
+ * class if there is no significant performance loss by it.<p>
  * 
- * <p>
  * @author G.A.J. van der Plas
  * 
  */
