e477ec4a4b29c7381b985b91d2e3e93fdc8c78e8
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-01-12 09:16:47 +0000

    Eerste versie
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-rest-lib/trunk@15470 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/dom/entities/DomResultsPerStudentCourse.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/entities/DomResultsPerStudentCourse.java
@@ -1,9 +1,25 @@
 package nl.uu.fi.dwo.rest.dom.entities;
 
+import java.util.Map;
+
+import nl.uu.fi.dwo.rest.persistence.PersistenceId;
+
 public class DomResultsPerStudentCourse {
 
 	public DomResultsPerStudentCourse() {
-		// TODO Auto-generated constructor stub
 	}
+
+	// TODO more to come...
+	private Map<PersistenceId,DomStudentScoContext> studentScoContexts;
+
+    
+    
+    
+    public Map<PersistenceId, DomStudentScoContext> getStudentScoContexts() {
+		return studentScoContexts;
+	}
+	public void setStudentScoContexts(Map<PersistenceId, DomStudentScoContext> studentScoContexts) {
+		this.studentScoContexts = studentScoContexts;
+	}    
 
 }
