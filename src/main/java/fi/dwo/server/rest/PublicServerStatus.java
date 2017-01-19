1186f79e286676e792a2023aaeafd5a5ca28bfaf
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-01-19 12:55:39 +0000

    bugfix for getServletRevision
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWOServer/trunk@15559 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/rest/PublicServerStatus.java
+++ src/main/java/fi/dwo/server/rest/PublicServerStatus.java
@@ -4,7 +4,12 @@ import nl.uu.fi.dwo.rest.exceptions.Dwo2ExceptionCode;
 import nl.uu.fi.dwo.rest.exceptions.Dwo2RestException;
 import fi.dwo.commons.persistence.entities.PersistentDwoSystemParameters;
 import fi.dwo.server.PersistentDataManagers.core.DwoSystemParametersManager;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletContext;
@@ -12,6 +17,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 
 /**
  * Public server status. Showing health of the service. Under development.
@@ -26,13 +32,33 @@ public class PublicServerStatus {
 
     private static final Logger LOG = Logger.getLogger(PublicServerStatus.class.getName());
 
+    Attributes getManifestAttributes() throws FileNotFoundException, IOException {
+        InputStream resourceAsStream = context.getResourceAsStream("/META-INF/MANIFEST.MF");
+        Manifest mf = new Manifest();
+        mf.read(resourceAsStream);
+        Attributes atts = mf.getMainAttributes();
+
+        return atts;
+    }
+    
+//  tests above getManifestAttributes()
+//    @GET
+//    @Produces({"application/json"})
+//    @Path("/getManifest")    
+//    public Response getData() throws FileNotFoundException, IOException {
+//        Attributes manifestAttributes = getManifestAttributes();
+//
+//        return Response.status(Response.Status.OK)
+//                .entity(manifestAttributes)
+//                .build();
+//    }
+
     public List<PersistentDwoSystemParameters> getDwoSystemParamStatus() {
         List<PersistentDwoSystemParameters> result;
         try {
             result = DwoSystemParametersManager.findEntities();
             LOG.log(Level.FINER, "Fetched DwoSystemParameters {0}", new Object[]{result.size()});
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             LOG.log(Level.SEVERE, "Can't query the DwoSystemParameters", e);
             throw new Dwo2RestException(Dwo2ExceptionCode.Rest_InternalError, "An exception occured while fetching the DwoSystemParameters.");
         }
@@ -74,10 +100,11 @@ public class PublicServerStatus {
     @GET
     @Produces({"text/plain"})
     @Path("/getServletRevision/html")
-    public String getServletRevision() {
-        String buildNumber = context.getInitParameter("buildnumber");
-        String softwareVersion = context.getInitParameter("projectVersion");
-        String timeStamp = context.getInitParameter("timestamp");
+    public String getServletRevision() throws FileNotFoundException, IOException {
+        Attributes manifestAttributes = getManifestAttributes();
+        String buildNumber = manifestAttributes.getValue("Implementation-Build");
+        String softwareVersion = manifestAttributes.getValue("Implementation-Version");
+        String timeStamp = manifestAttributes.getValue("Implementation-Timestamp");
 
         String out = "Software version, buildnumber: " + softwareVersion + ", " + buildNumber + ", timestamp " + timeStamp + "\n";
         LOG.log(Level.INFO, "Software version {0}, buildnumber {1}, timestamp {2}", new Object[]{softwareVersion, buildNumber, timeStamp});
@@ -88,12 +115,17 @@ public class PublicServerStatus {
     @GET
     @Produces({"application/json"})
     @Path("/get")
-    public String getStatusJSON() {
+    public String getStatusJSON() throws IOException {
+        Attributes manifestAttributes = getManifestAttributes();
+        String buildNumber = manifestAttributes.getValue("Implementation-Build");
+        String softwareVersion = manifestAttributes.getValue("Implementation-Version");
+        String timeStamp = manifestAttributes.getValue("Implementation-Timestamp");
+        
         StringBuilder result = new StringBuilder();
         result.append("[")
-                .append("{\"name\":\"projectVersion\", \"value\":\"").append(context.getInitParameter("projectVersion")).append("\"},")
-                .append("{\"name\":\"buildNumber\", \"value\":\"").append(context.getInitParameter("buildnumber")).append("\"},")
-                .append("{\"name\":\"timestamp\", \"value\":\"").append(context.getInitParameter("timestamp"));
+                .append("{\"name\":\"projectVersion\", \"value\":\"").append(softwareVersion).append("\"},")
+                .append("{\"name\":\"buildNumber\", \"value\":\"").append(buildNumber).append("\"},")
+                .append("{\"name\":\"timestamp\", \"value\":\"").append(timeStamp);
         for (PersistentDwoSystemParameters p : getDwoSystemParamStatus()) {
             result.append("\"},{\"name\":\"").append(p.getName()).append("\", \"value\":\"").append(p.getValue());
         }
@@ -104,8 +136,7 @@ public class PublicServerStatus {
     @GET
     @Produces({"text/plain"})
     @Path("/get/html")
-    public String getStatus() {
-
+    public String getStatus() throws IOException {
         return getServletRevision()
                 + "\n" + getDwoSystemStatusText();
     }
