272a6cbf654fe679530a9fcba0f3458d7888762a
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-04-19 13:15:57 +0000

    simple validuserfields checker
    DomResultPlotMatrix zonder toString()
    
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@16240 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/dom/entities/SimpleValidUserFieldsChecker.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/entities/SimpleValidUserFieldsChecker.java
@@ -35,19 +35,71 @@ public class SimpleValidUserFieldsChecker {
 	}
 
 	SimpleValidUserFieldsChecker() {
-		// TODO Auto-generated constructor stub
 	}
 	
 // GWT compatible, zonder java.util.regex 
 	
-	public static boolean isValidEmail(String newEmail) {
-		// TODO Auto-generated method stub
+	public static boolean isValidEmail(String email) {
+/*
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+ */
+		if (email == null || email.length() < 3) return false; // minumum a@b
+		int alpha = email.indexOf('@');
+		if (alpha <= 0 || alpha >= email.length()-1) return false;
+		String local = email.substring(0, alpha);
+		String domain = email.substring(alpha+1);
+		for(char c : domain.toCharArray()) {
+			boolean ok = inrange(c, 'a','z') || inrange(c, 'A', 'Z') || inrange(c, '0', '9') || c == '-' || c == '.';
+			if(!ok) return false;
+		}
+		for(char c: local.toCharArray()) {
+			boolean ok = 
+					c == '!' ||
+					inrange(c, '#' , '\'') ||
+					c == '*' ||
+					c == '+' ||
+					inrange(c, '-', '9') ||
+					c == '=' ||					
+					inrange(c, '?', 'Z') ||
+					inrange(c, '^', '~');
+			if (!ok)
+				return false;
+		}
 		return true;
 	}
 
-// GWT compatible, zonder java.util.regex 
+	/**
+	 * In range b-e inclusief
+	 * @param c char
+	 * @param b start
+	 * @param e end
+	 * @return boolean
+	 */
+	private static boolean inrange(char c, char b, char e ) {
+		return b <= c && c <= e;
+	}
+/** GWT compatible, zonder java.util.regex 
+ *  alleen alfanumeriek, - _ en .
+ * @param username
+ * @return
+ */
 	public static boolean isValidUserName(String username) {
-		return true;
+//       return username.matches("[A-Za-z0-9_.-]+");
+      if (username == null || username.length() < 2) { // minimum lengte 2
+          return false;
+      }
+      char[] chars = username.toCharArray();
+      for (char c: chars) {
+// valid character?
+          boolean ok = inrange(c, 'a','z') || inrange(c, 'A', 'Z') || inrange(c, '0', '9') || c == '-' || c == '.' || c == '_' ;
+          if (!ok) {
+              return false;
+          }
+      }
+      return true;
 	}
 
 }
