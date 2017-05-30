82c2cc0c8c16838df2ca920450a666a02d601711
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-05-30 13:25:06 +0000

    eod
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@16549 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/rest/entities/RestSchoolClassAndProfile.java
@@ -0,0 +1,63 @@
+/**
+ * Copyrighted Sep 24, 2015
+ */
+package nl.uu.fi.dwo.rest.entities;
+
+import nl.uu.fi.dwo.rest.dom.entities.DomContext;
+import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfile;
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClass;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author G.A.J. van der Plas
+ */
+
+@XmlRootElement
+public class RestSchoolClassAndProfile {
+    private DomContext restContext;
+    private DomSchoolClass domSchoolClass;
+    private DomDwoProfile domDwoProfile;
+    
+    public DomDwoProfile getDomDwoProfile() {
+		return domDwoProfile;
+	}
+
+	public void setDomDwoProfile(DomDwoProfile domDwoProfile) {
+		this.domDwoProfile = domDwoProfile;
+	}
+
+	public RestSchoolClassAndProfile(){
+        
+    }
+
+    /**
+     * @return the restContext
+     */
+    public DomContext getRestContext() {
+        return restContext;
+    }
+
+    /**
+     * @param restContext the restContext to set
+     */
+    public void setRestContext(DomContext restContext) {
+        this.restContext = restContext;
+    }
+
+    /**
+     * @return the domSchoolClass
+     */
+    public DomSchoolClass getDomSchoolClass() {
+        return domSchoolClass;
+    }
+
+    /**
+     * @param domSchoolClass the domSchoolClass to set
+     */
+    public void setDomSchoolClass(DomSchoolClass domSchoolClass) {
+        this.domSchoolClass = domSchoolClass;
+    }
+
+    
+}
