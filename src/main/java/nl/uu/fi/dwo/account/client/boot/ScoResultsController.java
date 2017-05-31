f04a6e5d76f3585301b12e06d8eaaae40f9f085b
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-05-31 11:43:17 +0000

    #57 wip scoresults
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOResults/trunk@16560 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/account/client/boot/ScoResultsController.java
@@ -0,0 +1,36 @@
+package nl.uu.fi.dwo.account.client.boot;
+
+import fi.dwo.gwt.lib.rest.CallManagers.SecuredUserAccountManager;
+import java.util.logging.Logger;
+import nl.uu.fi.dwo.rest.dom.entities.DomUserFullwLoginContext;
+import org.osgi.util.promise.Promise;
+
+/**
+ * Controller for Login.
+ *
+ * @author Gert van der Plas
+ */
+class ScoResultsController {
+
+    private static final Logger LOG = Logger.getLogger(ScoResultsController.class.getName());
+
+    private SecuredUserAccountManager manager = new SecuredUserAccountManager();
+
+    ScoResultsController() {
+        init();
+    }
+
+    private void init() {
+
+    }
+
+//    public Promise<DomSchoolRoleAndClass> switchToSchoolLogin(DomSchoolRoleAndClass sc) {
+
+//    public void login(....){
+        //}
+ //   }
+    
+    public Promise<DomUserFullwLoginContext> login(String user, String password){
+        return manager.login(user, password);
+    }
+}
---
+++ src/main/java/nl/uu/fi/dwo/account/client/boot/ScoResultsPresenter.java
@@ -0,0 +1,102 @@
+package nl.uu.fi.dwo.account.client.boot;
+
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.ui.Widget;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import nl.uu.fi.dwo.account.client.DwoGlobalVars;
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolRoleAndClassV2;
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolsRolesAndClassesV2;
+
+/**
+ * Handler for for Login actions.
+ *
+ * @author Gert van der Plas
+ */
+public class ScoResultsPresenter {
+
+    private static final Logger LOG = Logger.getLogger(ScoResultsPresenter.class.getName());
+    private DwoGlobalVars dwoGlobalVars;
+    private EventBus eventBus;
+    private int selectedIndex = 0;
+    private List<DomSchoolRoleAndClassV2> sracData;
+
+    private Display view;
+
+    /**
+     * @param view the view to set
+     */
+    public void setView(Display view) {
+        this.view = view;
+    }
+
+    public interface Display {
+        Widget asWidget();
+        void clear();
+        void init();
+        void updateView(int height, int width, String[][] data);
+    }
+
+    ScoResultsPresenter(EventBus anEventBus, DwoGlobalVars aDwoGlobalVars) {
+        eventBus = anEventBus;
+        dwoGlobalVars = aDwoGlobalVars;
+    }
+
+    private List<DomSchoolRoleAndClassV2> getTeacherRoles() {
+        List<DomSchoolRoleAndClassV2> result = new ArrayList<DomSchoolRoleAndClassV2>();
+        DomSchoolsRolesAndClassesV2 sl = dwoGlobalVars.getSchoolLogins();
+        List<DomSchoolRoleAndClassV2> fullList = sl.getSchoolsRolesAndClassesList();
+        for (DomSchoolRoleAndClassV2 hasRole : fullList) {
+            if (hasRole.getRole().getRoleName().equals("TEACHER")) {
+                result.add(hasRole);
+            }
+        }
+        return result;
+    }
+
+    public void init() {
+        sracData = getTeacherRoles();
+        int i = 0;
+        selectedIndex = i;
+        for (DomSchoolRoleAndClassV2 srac : sracData) {
+            if (srac.getHasRole().getId().equals(srac.getHasRole().getId())) {
+                selectedIndex = i;
+            }
+            i++;
+        }
+        String[][] data = buildPlotData();
+        view.init();
+        view.updateView(data.length,1,data);
+    }
+
+    /**
+     * @param row the course to set
+     */
+    public void selectRow(int row) {
+        if (row != -1) {
+            selectedIndex = row;
+            return;
+        }
+    }
+
+    public void switchSchool() {
+        dwoGlobalVars.setActiveSchoolRoleAndClass(sracData.get(selectedIndex));
+        eventBus.fireEvent(new SwitchViewEvent(SwitchViewEvent.SelectedView.RESULTS));
+    }
+
+    private String[][] buildPlotData() {
+        String[][] data = new String[sracData.size()+1][1];
+        data[0][0] = "School";//<div style=\"text-align: left; background-color: #aaaaaa; padding: 2px; overflow auto;\">School</div>";
+        int i = 1;
+        selectedIndex=0;
+        for (DomSchoolRoleAndClassV2 srac : sracData) {
+            data[i][0] = srac.getSchool().getSchoolName();
+            if (srac.getHasRole().getId().equals(srac.getHasRole().getId())) {
+                selectedIndex = i;
+            }
+            i++;
+        }
+        return data;
+    }
+}
