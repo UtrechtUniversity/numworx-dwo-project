31eb6bee52c011b2d39fc4c26eec18415c9bb656
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-09-08 10:58:51 +0000

    #LMS-57 added i18n generated localisation code
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-gwt-lib/trunk@17379 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/nl/uu/fi/dwo/rest/locale/Dwo2LocaleMessages.java
@@ -0,0 +1,566 @@
+package nl.uu.fi.dwo.rest.locale;
+
+/**
+ * Interface to represent the constants contained in resource bundle:
+ * 	'/Users/gert/NetBeansProjects/UU/trunk/DWO-gwt-lib/src/nl/uu/fi/dwo/rest/locale/Dwo2LocaleMessages.properties'.
+ */
+public interface Dwo2LocaleMessages extends com.google.gwt.i18n.client.ConstantsWithLookup {
+  
+  /**
+   * Translated "Administrator".
+   * 
+   * @return translated "Administrator"
+   */
+  @DefaultStringValue("Administrator")
+  @Key("ADMIN")
+  String ADMIN();
+
+  /**
+   * Translated "Data provided by the school".
+   * 
+   * @return translated "Data provided by the school"
+   */
+  @DefaultStringValue("Data provided by the school")
+  @Key("GUIR_MSG_PROVIDED_SCHOOL")
+  String GUIR_MSG_PROVIDED_SCHOOL();
+
+  /**
+   * Translated "An incorrect password was given_".
+   * 
+   * @return translated "An incorrect password was given_"
+   */
+  @DefaultStringValue("An incorrect password was given_")
+  @Key("GUI_AnIncorrectPasswordWasGiven")
+  String GUI_AnIncorrectPasswordWasGiven();
+
+  /**
+   * Translated "back".
+   * 
+   * @return translated "back"
+   */
+  @DefaultStringValue("back")
+  @Key("GUI_Button_Back")
+  String GUI_Button_Back();
+
+  /**
+   * Translated "cancel".
+   * 
+   * @return translated "cancel"
+   */
+  @DefaultStringValue("cancel")
+  @Key("GUI_Button_Cancel")
+  String GUI_Button_Cancel();
+
+  /**
+   * Translated "delete all student results".
+   * 
+   * @return translated "delete all student results"
+   */
+  @DefaultStringValue("delete all student results")
+  @Key("GUI_Button_ClearAllStudentResults")
+  String GUI_Button_ClearAllStudentResults();
+
+  /**
+   * Translated "export".
+   * 
+   * @return translated "export"
+   */
+  @DefaultStringValue("export")
+  @Key("GUI_Button_Export")
+  String GUI_Button_Export();
+
+  /**
+   * Translated "login".
+   * 
+   * @return translated "login"
+   */
+  @DefaultStringValue("login")
+  @Key("GUI_Button_Login")
+  String GUI_Button_Login();
+
+  /**
+   * Translated "reset".
+   * 
+   * @return translated "reset"
+   */
+  @DefaultStringValue("reset")
+  @Key("GUI_Button_Reset")
+  String GUI_Button_Reset();
+
+  /**
+   * Translated "seal".
+   * 
+   * @return translated "seal"
+   */
+  @DefaultStringValue("seal")
+  @Key("GUI_Button_Seal")
+  String GUI_Button_Seal();
+
+  /**
+   * Translated "update".
+   * 
+   * @return translated "update"
+   */
+  @DefaultStringValue("update")
+  @Key("GUI_Button_Update")
+  String GUI_Button_Update();
+
+  /**
+   * Translated "delete".
+   * 
+   * @return translated "delete"
+   */
+  @DefaultStringValue("delete")
+  @Key("GUI_Delete")
+  String GUI_Delete();
+
+  /**
+   * Translated "email address".
+   * 
+   * @return translated "email address"
+   */
+  @DefaultStringValue("email address")
+  @Key("GUI_Email")
+  String GUI_Email();
+
+  /**
+   * Translated "family name".
+   * 
+   * @return translated "family name"
+   */
+  @DefaultStringValue("family name")
+  @Key("GUI_FamilyName")
+  String GUI_FamilyName();
+
+  /**
+   * Translated "given name".
+   * 
+   * @return translated "given name"
+   */
+  @DefaultStringValue("given name")
+  @Key("GUI_GivenName")
+  String GUI_GivenName();
+
+  /**
+   * Translated "insertion".
+   * 
+   * @return translated "insertion"
+   */
+  @DefaultStringValue("insertion")
+  @Key("GUI_Insertion")
+  String GUI_Insertion();
+
+  /**
+   * Translated "email address".
+   * 
+   * @return translated "email address"
+   */
+  @DefaultStringValue("email address")
+  @Key("GUI_Label_Email")
+  String GUI_Label_Email();
+
+  /**
+   * Translated "family name".
+   * 
+   * @return translated "family name"
+   */
+  @DefaultStringValue("family name")
+  @Key("GUI_Label_FamilyName")
+  String GUI_Label_FamilyName();
+
+  /**
+   * Translated "given name".
+   * 
+   * @return translated "given name"
+   */
+  @DefaultStringValue("given name")
+  @Key("GUI_Label_GivenName")
+  String GUI_Label_GivenName();
+
+  /**
+   * Translated "insertion".
+   * 
+   * @return translated "insertion"
+   */
+  @DefaultStringValue("insertion")
+  @Key("GUI_Label_Insertion")
+  String GUI_Label_Insertion();
+
+  /**
+   * Translated "new password".
+   * 
+   * @return translated "new password"
+   */
+  @DefaultStringValue("new password")
+  @Key("GUI_Label_NewPassword")
+  String GUI_Label_NewPassword();
+
+  /**
+   * Translated "new password again".
+   * 
+   * @return translated "new password again"
+   */
+  @DefaultStringValue("new password again")
+  @Key("GUI_Label_NewPasswordAgain")
+  String GUI_Label_NewPasswordAgain();
+
+  /**
+   * Translated "new login".
+   * 
+   * @return translated "new login"
+   */
+  @DefaultStringValue("new login")
+  @Key("GUI_Label_NewSchoolLogin")
+  String GUI_Label_NewSchoolLogin();
+
+  /**
+   * Translated "password".
+   * 
+   * @return translated "password"
+   */
+  @DefaultStringValue("password")
+  @Key("GUI_Label_Password")
+  String GUI_Label_Password();
+
+  /**
+   * Translated "schoolname".
+   * 
+   * @return translated "schoolname"
+   */
+  @DefaultStringValue("schoolname")
+  @Key("GUI_Label_SchoolName")
+  String GUI_Label_SchoolName();
+
+  /**
+   * Translated "select a file".
+   * 
+   * @return translated "select a file"
+   */
+  @DefaultStringValue("select a file")
+  @Key("GUI_Label_SelectAFile")
+  String GUI_Label_SelectAFile();
+
+  /**
+   * Translated "username".
+   * 
+   * @return translated "username"
+   */
+  @DefaultStringValue("username")
+  @Key("GUI_Label_UserName")
+  String GUI_Label_UserName();
+
+  /**
+   * Translated "login".
+   * 
+   * @return translated "login"
+   */
+  @DefaultStringValue("login")
+  @Key("GUI_Login")
+  String GUI_Login();
+
+  /**
+   * Translated "My Profile".
+   * 
+   * @return translated "My Profile"
+   */
+  @DefaultStringValue("My Profile")
+  @Key("GUI_MyProfile")
+  String GUI_MyProfile();
+
+  /**
+   * Translated "My schoolclasses".
+   * 
+   * @return translated "My schoolclasses"
+   */
+  @DefaultStringValue("My schoolclasses")
+  @Key("GUI_MySchoolClasses")
+  String GUI_MySchoolClasses();
+
+  /**
+   * Translated "My school logins".
+   * 
+   * @return translated "My school logins"
+   */
+  @DefaultStringValue("My school logins")
+  @Key("GUI_MySchoolLogins")
+  String GUI_MySchoolLogins();
+
+  /**
+   * Translated "new password".
+   * 
+   * @return translated "new password"
+   */
+  @DefaultStringValue("new password")
+  @Key("GUI_NewPassword")
+  String GUI_NewPassword();
+
+  /**
+   * Translated "new password again".
+   * 
+   * @return translated "new password again"
+   */
+  @DefaultStringValue("new password again")
+  @Key("GUI_NewPasswordAgain")
+  String GUI_NewPasswordAgain();
+
+  /**
+   * Translated "new login".
+   * 
+   * @return translated "new login"
+   */
+  @DefaultStringValue("new login")
+  @Key("GUI_NewSchoolLogin")
+  String GUI_NewSchoolLogin();
+
+  /**
+   * Translated "No user is signed in_".
+   * 
+   * @return translated "No user is signed in_"
+   */
+  @DefaultStringValue("No user is signed in_")
+  @Key("GUI_NoUserIsSignedIn")
+  String GUI_NoUserIsSignedIn();
+
+  /**
+   * Translated "password".
+   * 
+   * @return translated "password"
+   */
+  @DefaultStringValue("password")
+  @Key("GUI_Password")
+  String GUI_Password();
+
+  /**
+   * Translated "role name".
+   * 
+   * @return translated "role name"
+   */
+  @DefaultStringValue("role name")
+  @Key("GUI_RoleName")
+  String GUI_RoleName();
+
+  /**
+   * Translated "schoolclass key".
+   * 
+   * @return translated "schoolclass key"
+   */
+  @DefaultStringValue("schoolclass key")
+  @Key("GUI_SchoolClassRegistrationKey")
+  String GUI_SchoolClassRegistrationKey();
+
+  /**
+   * Translated "schoollogin".
+   * 
+   * @return translated "schoollogin"
+   */
+  @DefaultStringValue("schoollogin")
+  @Key("GUI_SchoolLogin")
+  String GUI_SchoolLogin();
+
+  /**
+   * Translated "school name".
+   * 
+   * @return translated "school name"
+   */
+  @DefaultStringValue("school name")
+  @Key("GUI_SchoolName")
+  String GUI_SchoolName();
+
+  /**
+   * Translated "klasnaam".
+   * 
+   * @return translated "klasnaam"
+   */
+  @DefaultStringValue("klasnaam")
+  @Key("GUI_SchoolclassName")
+  String GUI_SchoolclassName();
+
+  /**
+   * Translated "Switch to another school".
+   * 
+   * @return translated "Switch to another school"
+   */
+  @DefaultStringValue("Switch to another school")
+  @Key("GUI_SwitchSchool")
+  String GUI_SwitchSchool();
+
+  /**
+   * Translated "Userregistration failed.".
+   * 
+   * @return translated "Userregistration failed."
+   */
+  @DefaultStringValue("Userregistration failed.")
+  @Key("GUI_UserRegistrationFailed")
+  String GUI_UserRegistrationFailed();
+
+  /**
+   * Translated "The user is successfully registered.".
+   * 
+   * @return translated "The user is successfully registered."
+   */
+  @DefaultStringValue("The user is successfully registered.")
+  @Key("GUI_UserRegistrationSucceeded")
+  String GUI_UserRegistrationSucceeded();
+
+  /**
+   * Translated "username".
+   * 
+   * @return translated "username"
+   */
+  @DefaultStringValue("username")
+  @Key("GUI_Username")
+  String GUI_Username();
+
+  /**
+   * Translated "Account settings".
+   * 
+   * @return translated "Account settings"
+   */
+  @DefaultStringValue("Account settings")
+  @Key("GUI_View_ACCOUNT")
+  String GUI_View_ACCOUNT();
+
+  /**
+   * Translated "Results browser".
+   * 
+   * @return translated "Results browser"
+   */
+  @DefaultStringValue("Results browser")
+  @Key("GUI_View_ACTIVERESULTS")
+  String GUI_View_ACTIVERESULTS();
+
+  /**
+   * Translated "Add student ".
+   * 
+   * @return translated "Add student "
+   */
+  @DefaultStringValue("Add student ")
+  @Key("GUI_View_ADDSTUDENTS")
+  String GUI_View_ADDSTUDENTS();
+
+  /**
+   * Translated "Course-management".
+   * 
+   * @return translated "Course-management"
+   */
+  @DefaultStringValue("Course-management")
+  @Key("GUI_View_COURSESOFSCHOOLCLASS")
+  String GUI_View_COURSESOFSCHOOLCLASS();
+
+  /**
+   * Translated "Login".
+   * 
+   * @return translated "Login"
+   */
+  @DefaultStringValue("Login")
+  @Key("GUI_View_LOGIN")
+  String GUI_View_LOGIN();
+
+  /**
+   * Translated "Logout".
+   * 
+   * @return translated "Logout"
+   */
+  @DefaultStringValue("Logout")
+  @Key("GUI_View_LOGOUT")
+  String GUI_View_LOGOUT();
+
+  /**
+   * Translated "Results browser".
+   * 
+   * @return translated "Results browser"
+   */
+  @DefaultStringValue("Results browser")
+  @Key("GUI_View_RESULTS")
+  String GUI_View_RESULTS();
+
+  /**
+   * Translated "My schoolclasses".
+   * 
+   * @return translated "My schoolclasses"
+   */
+  @DefaultStringValue("My schoolclasses")
+  @Key("GUI_View_SCHOOLCLASSES")
+  String GUI_View_SCHOOLCLASSES();
+
+  /**
+   * Translated "Result viewer".
+   * 
+   * @return translated "Result viewer"
+   */
+  @DefaultStringValue("Result viewer")
+  @Key("GUI_View_SCORESULTS")
+  String GUI_View_SCORESULTS();
+
+  /**
+   * Translated "Students-of-class management".
+   * 
+   * @return translated "Students-of-class management"
+   */
+  @DefaultStringValue("Students-of-class management")
+  @Key("GUI_View_STUDENTSINSCHOOLCLASS")
+  String GUI_View_STUDENTSINSCHOOLCLASS();
+
+  /**
+   * Translated "Switch role".
+   * 
+   * @return translated "Switch role"
+   */
+  @DefaultStringValue("Switch role")
+  @Key("GUI_View_SWITCHSCHOOL")
+  String GUI_View_SWITCHSCHOOL();
+
+  /**
+   * Translated "Teachers-of-class management".
+   * 
+   * @return translated "Teachers-of-class management"
+   */
+  @DefaultStringValue("Teachers-of-class management")
+  @Key("GUI_View_TEACHERSINSCHOOLCLASS")
+  String GUI_View_TEACHERSINSCHOOLCLASS();
+
+  /**
+   * Translated "No school".
+   * 
+   * @return translated "No school"
+   */
+  @DefaultStringValue("No school")
+  @Key("NULLSCHOOL")
+  String NULLSCHOOL();
+
+  /**
+   * Translated "School admin".
+   * 
+   * @return translated "School admin"
+   */
+  @DefaultStringValue("School admin")
+  @Key("SCHOOLADMIN")
+  String SCHOOLADMIN();
+
+  /**
+   * Translated "Key code".
+   * 
+   * @return translated "Key code"
+   */
+  @DefaultStringValue("Key code")
+  @Key("SCHOOLCODE")
+  String SCHOOLCODE();
+
+  /**
+   * Translated "Student".
+   * 
+   * @return translated "Student"
+   */
+  @DefaultStringValue("Student")
+  @Key("STUDENT")
+  String STUDENT();
+
+  /**
+   * Translated "Teacher".
+   * 
+   * @return translated "Teacher"
+   */
+  @DefaultStringValue("Teacher")
+  @Key("TEACHER")
+  String TEACHER();
+}
