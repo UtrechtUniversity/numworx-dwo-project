abe54dc20a01fe1e37e089fd66aed4f6850c3ada
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-09-29 10:37:09 +0000

    missing.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOGwtClient/trunk@17613 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/resources/DwoResources.java
@@ -0,0 +1,53 @@
+package nl.uu.fi.dwo.lms.gwtclient.gwt.resources;
+
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+/**
+ *
+ * @author G.A.J. van der Plas
+ */
+public interface DwoResources extends ClientBundle{
+   @Source("/css/Style.css")
+   Style style();
+
+   @Source("DWO_logo_full.png")
+   public ImageResource logoIcon();
+   
+   @Source("docent.png")
+   public ImageResource teacherIcon();
+
+   @Source("student.png")
+   public ImageResource studentIcon();
+   
+   @Source("empty.gif")
+   public ImageResource emptyIcon();
+
+   @Source("delete.gif")
+   public ImageResource deleteIcon();
+   
+   @Source("edit.gif")
+   public ImageResource editIcon();
+
+   @Source("modules.gif")
+   public ImageResource modulesIcon();
+
+   @Source("userlist.gif")
+   public ImageResource usersIcon();
+
+   @Source("studentlist.gif")
+   public ImageResource studentsIcon();
+
+   @Source("teacherlist.gif")
+   public ImageResource teachersIcon();
+
+   @Source("loading.gif")
+   public ImageResource loadingIcon();
+   
+   @Source("drillUp.gif")
+   public ImageResource drillUpIcon();
+   
+   @Source("drillDown.gif")
+   public ImageResource drillDownIcon();
+}
---
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/resources/DwoStyle.java
@@ -0,0 +1,13 @@
+package nl.uu.fi.dwo.lms.gwtclient.gwt.resources;
+
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.resources.client.ClientBundle;
+
+/**
+ *
+ * @author G.A.J. van der Plas
+ */
+public interface DwoStyle extends ClientBundle{
+   @Source("/css/Style.css")
+   Style Style();
+}
