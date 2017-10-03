08d72e702566bea03ec07d85ace76c92d17d0935
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-10-03 08:24:10 +0000

    state updates
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@17647 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/dom/entities/util/PublishState.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/entities/util/PublishState.java
@@ -1,7 +1,11 @@
 package nl.uu.fi.dwo.rest.dom.entities.util;
 
 /**
- *
+ * Publish state for Sco launchData. Edit means being changed by a teacher, review
+ * is currently not being edited. Published is available for students and therefor
+ * no longer editable for a teacher. While this feature is unused the default value is
+ * enum ordinal 2, 'published'.
+ * 
  * @author Gert van der Plas
  */
 public enum PublishState {
--- src/main/java/nl/uu/fi/dwo/rest/dom/entities/util/ViewState.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/entities/util/ViewState.java
@@ -1,7 +1,8 @@
 package nl.uu.fi.dwo.rest.dom.entities.util;
 
 /**
- * Visibility of course for students, teachers and results. 
+ * Visibility of course for students, teachers and results. Used in 
+ * {@link fi.dwo.commons.persistence.entities.PersistentClassCourse PersistentClassCourse}.
  * 
  * @author Gert van der Plas
  */
