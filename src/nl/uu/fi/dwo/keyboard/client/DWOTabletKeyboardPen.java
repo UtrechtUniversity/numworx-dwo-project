6de9365c3615cf980b0d57e5ed220109525edd24
kuipe036 <kuipe036@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-10-03 09:00:40 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/KeyboardGWT/branches/nextVersion@17649 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/keyboard/client/DWOTabletKeyboardPen.java
+++ src/nl/uu/fi/dwo/keyboard/client/DWOTabletKeyboardPen.java
@@ -1,5 +1,7 @@
 package nl.uu.fi.dwo.keyboard.client;
 
+import java.util.logging.Logger;
+
 import nl.uu.fi.dwo.interaction.client.FormuleEditorIF;
 
 import com.google.gwt.core.client.GWT;
@@ -14,6 +16,8 @@ import fi.writemathgwt.client.WritePanel;
 import fi.writemathgwt.client.WritePanelHolder;
 
 public class DWOTabletKeyboardPen extends AbstractKeyboard implements WritePanelHolder {
+	private static Logger logger = Logger.getLogger("DWOTabletKeyboardPen");
+
 
 	private static int HEIGHT = 166;
 	int getKeyboardHeight() {
@@ -71,8 +75,23 @@ public class DWOTabletKeyboardPen extends AbstractKeyboard implements WritePanel
 	@Override
 	public void writePanelChanged() {
 		if(recurse) return;
-		getEditor().clearAll();
-		getEditor().insert(panel.parseFormule());
+		
+		String s = panel.parseFormule();
+		if (s.isEmpty()) {
+			// Temporary local fix :: clear all doet geen paint dus als s was bleef de vorige inhoud zichtbaar
+			// Huidige fix :: eerst clear all, (voor het geval er een grote formule verwijderd is)
+			//                vervolgens wordt een korte string toegevoegd (omdat ischanged geactiveerd dient te worden)
+			//                tenslotte wordt de backspace-functionaliteit (removeCurrentElement) aangeroepen
+			getEditor().clearAll();
+			s="1";
+			getEditor().insert(s);
+			getEditor().removeCurrentElement();
+			
+		} else {
+			getEditor().clearAll();
+			getEditor().insert(s);
+		}
+
 	}
 
 	public void focus() {
