facbe4ab61f4749e665df45e8218cf87c1195cbc
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-10-06 11:16:47 +0000

    Simple Course and ClassCourse join example with junitTest.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOServer/trunk@17715 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/test/java/fi/dwo/server/rest/ResultMapManagerIT.java
@@ -0,0 +1,76 @@
+/**
+ * Copyrighted Sep 18, 2015
+ */
+package fi.dwo.server.rest;
+
+import fi.dwo.commons.persistence.entities.PersistentClassCourse;
+import fi.dwo.commons.persistence.entities.PersistentCourse;
+import fi.dwo.server.mysql.DatabaseManager;
+import fi.dwo.server.persistence.DwoEmfFactory;
+import static fi.dwo.server.rest.PublicUserManagerIT.instance;
+import java.util.List;
+import javax.persistence.EntityManager;
+import static nl.uu.fi.dwo.rest.persistence.PersistenceClassType.PersistentClassCourse;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Tests bi-implication of roles in RoleType and the database.
+ * 
+ * 
+ * @author Gert van der Plas
+ */
+public class ResultMapManagerIT {
+
+    public ResultMapManagerIT() {
+      //  Dwo2ExceptionTranslator.setTranslator(new Dwo2ExceptionJavaTranslator());
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        DwoEmfFactory.setEntityManagerFactory("DWO_TestDB");
+        instance = new DatabaseManager();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        DwoEmfFactory.setDefaultEntityManagerFactory();
+        instance = null;
+    }
+
+    @Before
+    public void setUp() {
+        instance.IntializeTestDatabase();
+    }
+
+    @After
+    public void tearDown() {
+        instance.ClearDatabase();
+    }
+
+    /**
+     * Test of getRoles method, of class PublicRoleManager. Tests for one-to-one
+     * of RoleTypes mapping between persistent store and enum class.
+     */
+    @Test
+    public void testResultMap() {
+        System.out.println("testResultMap");
+        EntityManager em = DwoEmfFactory.getEntityManager();
+//        List<PersistentCourseInClass> results = em.createNativeQuery("SELECT a.* ,b.* FROM tblclasscourse a join tblcourse b using (courseid)", "CourseInClassMapping").getResultList();
+        List<Object[]> results = em.createNativeQuery("SELECT a.ClassCourseID, a.CourseID, a.ClassID, a.type, a.viewState, "
+                + "a.notBefore, a.notAfter, a.CourseID, b.schoolID, b.name, b.description, b.image, b.dwoProfileID, b.imageData, b.export, "
+                + "b.withChildren, b.parentID FROM tblclasscourse a join tblcourse b using (courseid)", "CourseInClassMapping").getResultList();
+        if(!(results.get(0)[0] instanceof PersistentCourse)){
+        fail("wrong class type");
+    }
+        if(!(results.get(0)[1] instanceof PersistentClassCourse)){
+        fail("wrong class type");
+    }
+
+
+    }
+}
