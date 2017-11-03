4683b7693ebcf85cf048220d5615e50f76d0ec9c
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-11-03 13:50:01 +0000

    domScoDataFull
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@17982 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/rest/dom/entities/DomScoDataFull.java
@@ -0,0 +1,9 @@
+package nl.uu.fi.dwo.rest.dom.entities;
+
+public class DomScoDataFull extends DomId {
+
+	public DomScoDataFull() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/nl/uu/fi/dwo/rest/entities/RestScoFull.java
@@ -0,0 +1,33 @@
+package nl.uu.fi.dwo.rest.entities;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import nl.uu.fi.dwo.rest.dom.entities.DomContext;
+import nl.uu.fi.dwo.rest.dom.entities.DomCourseFull;
+import nl.uu.fi.dwo.rest.dom.entities.DomScoContextFull;
+import nl.uu.fi.dwo.rest.dom.entities.DomScoDataFull;
+
+/**
+ * RestCourse contains a minimal DwoCourse info.
+ * 
+ * @author W.P.G. van Velthoven
+ */
+@XmlRootElement
+public class RestScoFull {
+
+	private DomContext restContext;
+	private DomScoContextFull domScoContext;
+	private DomScoDataFull domScoData;
+	/**
+	 * @return the restContext
+	 */
+	public DomContext getRestContext() {
+		return restContext;
+	}
+	/**
+	 * @param restContext the restContext to set
+	 */
+	public void setRestContext(DomContext restContext) {
+		this.restContext = restContext;
+	}
+}
