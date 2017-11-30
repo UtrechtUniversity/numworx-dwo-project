56207c136394c643064e4f8e75485dacbe7f4c98
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-11-30 09:32:11 +0000

    added verifyBrowserExamKey in /rest/public/test
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOServer/trunk@18357 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/rest/PublicRestTestManager.java
+++ src/main/java/fi/dwo/server/rest/PublicRestTestManager.java
@@ -1,14 +1,23 @@
 package fi.dwo.server.rest;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+import java.util.logging.Level;
 import nl.uu.fi.dwo.rest.exceptions.Dwo2ExceptionCode;
 import nl.uu.fi.dwo.rest.exceptions.Dwo2RestException;
 import java.util.logging.Logger;
+import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * REST functions that allows one to test the proper catching of HTML-errors.
@@ -21,6 +30,8 @@ import javax.ws.rs.core.SecurityContext;
 public class PublicRestTestManager {
 
     private static final Logger LOG = Logger.getLogger(PublicRestTestManager.class.getName());
+    @Context
+    private ServletContext context;
 
     @GET
     @Produces({"application/json"})
@@ -50,7 +61,7 @@ public class PublicRestTestManager {
         Response r = Response.status(401).entity(e.getMessage()).build();
         return r;
     }
-    
+
     @GET
     @Produces({"application/json"})
     @Path("/test500Error/json")
@@ -60,6 +71,41 @@ public class PublicRestTestManager {
         Dwo2RestException e = new Dwo2RestException(Dwo2ExceptionCode.User_AuthenticationError, "DwoRestException 500 thrown on request of user: " + userName + ".");
         Response r = Response.status(500).entity(e.getMessage()).build();
         return r;
-    }   
-   
-}
+    }
+
+    @GET
+    @Produces({"application/json"})
+    @Path("/VerifyBrowserExamKey/json")
+    public Response verifyBrowserExamKey(@Context UriInfo uriInfo, @HeaderParam("X-SafeExamBrowser-RequestHash") String headerHash, @QueryParam("key") String rawKey, @QueryParam("salt") String salt) {
+        Boolean result = false;
+        rawKey = "test";
+        salt = "bla";
+        String serverHash = rawKey + uriInfo.getAbsolutePath().toString() + salt;
+        //hash serverHash with SHA-1
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+            md.digest(serverHash.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            Logger.getLogger(PublicRestTestManager.class.getName()).log(Level.SEVERE, null, e);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(PublicRestTestManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        md.toString();
+        // Convert the byte to hex format
+        Formatter formatter = new Formatter();
+            for (final byte b : md.digest()) {
+                formatter.format("%02x", b);
+            }
+
+            LOG.log(Level.INFO, "serverHash = {0}", new Object[]{formatter.toString()});
+            LOG.log(Level.INFO, "headerHash = {0}", new Object[]{headerHash});
+            if (serverHash.equals(headerHash)) {
+                result = true;
+            }
+            Response r = Response.status(200).entity(result).build();
+            return r;
+        }
+
+    }
