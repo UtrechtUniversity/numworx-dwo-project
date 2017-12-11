a29b41c061e38bcb2d628aed62f06918896e418c
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-12-11 15:14:23 +0000

    LMS-124 jsinteropt translator access for HTML5/javascript.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOGwtClient/trunk@18534 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/jsutil/DwoTranslator.java
@@ -0,0 +1,50 @@
+package nl.uu.fi.dwo.lms.gwtclient.gwt.jsutil;
+
+import com.google.gwt.core.client.GWT;
+import java.util.ArrayList;
+import java.util.List;
+import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+import nl.uu.fi.dwo.rest.locale.Dwo2LocaleMessageCode;
+import nl.uu.fi.dwo.rest.locale.DwoLocalesForGWT;
+
+/**
+ * Translator access for javascript via jsinteropt.
+ * 
+ * @author Gert van der Plas
+ */
+
+@JsType(name="DwoTranslator", namespace = JsPackage.GLOBAL)
+public class DwoTranslator {
+    private static final DwoLocalesForGWT rb = GWT.create(DwoLocalesForGWT.class);
+    
+    /**
+     * A comfort function to yield an overview of all available translations for
+     * the HTML5 developer.
+     * 
+     * @return 
+     */
+    @JsMethod
+    public List<String> getTranslationList(){
+        // For Dwo2ExceptionTranslator.setTranslator(new Dwo2ExceptionGWTTranslator());
+        List<String> result= new ArrayList<>();
+        Dwo2LocaleMessageCode codeList[] = Dwo2LocaleMessageCode.values();
+        for(Dwo2LocaleMessageCode code : codeList){
+            result.add(code.name());
+        }
+        return result;
+    }
+
+    /**
+     * translates a string to the local encoding set in the application. 
+     * 
+     * @param text
+     * @return 
+     */
+    @JsMethod
+    public String  translate(String text){
+        return rb.getString(text);
+    }
+    
+}
