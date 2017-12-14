1c9e8a8ade53ce6a179317c3b60a49dcb1e6db17
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-12-14 12:47:45 +0000

    Session Registry wip
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@18566 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/dom/Registry.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/Registry.java
@@ -6,58 +6,53 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 /**
+ * Object Registry. Primarily for service factory as an alternative for
+ * singleton instances. In such a case the name is the full package name of the
+ * object.
  *
  * @author Gert van der Plas
  */
 public class Registry {
 
     private static final Logger LOG = Logger.getLogger(SessionedRegistry.class.getName());
- 
-  private static Registry instance;
- 
-  private final Map<String,Reference> registry = new HashMap<String,Reference>();
- 
-  public static synchronized Registry getInstance() {
-    if (instance == null) {
-      instance = new Registry();
+
+    private static Registry instance;
+
+    private final Map<String, Object> registry = new HashMap<String, Object>();
+
+    public static synchronized Registry getInstance() {
+        if (instance == null) {
+            instance = new Registry();
+        }
+        return instance;
     }
-    return instance;
-  }
- 
-  public synchronized Reference getReference(
-      final String name) {
-    final Reference result;
-    if (isRegistered(name)) {
-      result = registry.get(name);
-    } else {
-        return null;
+
+    public synchronized Object getObject(
+            final String name) {
+        final Object result;
+        if (isRegistered(name)) {
+            result = registry.get(name);
+        } else {
+            return null;
+        }
+        return result;
     }
-    return result;
-  }
- 
-  public boolean isRegistered(final String name) {
-    return registry.containsKey(name);
-  }
- 
-  public synchronized void register(final Object object) {
-//    final Reference reference = getReference(
-//        object.getName());
-//    if (!reference.hasObject() 
-//        || reference.getObject() != object) {
-//      reference.setObject(object);
-//    }
-  }
- 
-  public synchronized void unregister(
-      final Object object) { //should be Reference<T>
-      
-//    if (isRegistered(object.getName())) {
-//      final Reference reference = getReference(
-//          object.getName());
-//      if (reference.hasObject()) {
-//        reference.setObject(null);
-//      }
-//    }
-  }
- 
+
+    public boolean isRegistered(final String name) {
+        return registry.containsKey(name);
+    }
+
+    public synchronized void register(final Object object) {
+
+        String key = object.getClass().getName();
+
+        if (!registry.containsKey(key) || registry.get(key) != object) {
+            registry.put(key, object);
+        }
+    }
+
+    public synchronized void unregister(final String key) {
+        registry.remove(key);
+    }
 }
+
