c4d2eaed68e49d7394cef16a13e6f44616bb8736
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-01-03 16:07:33 +0000

    allow 1.5.1
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOServer/trunk@18761 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/persistence/DbAccess.java
+++ src/main/java/fi/dwo/server/persistence/DbAccess.java
@@ -785,7 +785,7 @@ public class DbAccess extends DbConnect implements DbAccessIF, ScormAccessIF, Db
                     (
                     hashMap.get("DBVersion Major").matches("1") 
                         && hashMap.get("DBVersion Minor").matches("5") 
-                        && hashMap.get("DBVersion Revision").matches("0")
+                        && hashMap.get("DBVersion Revision").matches("1")
                     )
                     
                     ) {
