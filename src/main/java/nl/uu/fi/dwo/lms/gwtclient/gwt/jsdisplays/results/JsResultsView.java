abb219cbbdc6e59e62dcee1053394700ee1e7180
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-01-04 13:53:51 +0000

    #LMS-156 fixed broken result viewing.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOGwtClient/trunk@18764 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/jsdisplays/results/JsResultsView.java
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/jsdisplays/results/JsResultsView.java
@@ -20,17 +20,20 @@ public class JsResultsView implements ResultsPresenter.Display {
     public void plot(ResultsPresenter.ResultPlot data, boolean zoomedClass, boolean zoomedCourse) {
         JSONArray rows = new JSONArray();
         JSONArray line = new JSONArray();
+        //set headers
         for (int x = 0; x < data.gethIndex().length; x++) {
                 line.set(x, new JSONString(data.gethIndex()[x].label));
             }
-            rows.set(0, line);
+        rows.set(0, line);
+        //set rows
         for (int y = 0; y < data.getvIndex().length; y++) {
+        //set row header appended by row scores
             line = new JSONArray();
                 line.set(0, new JSONString(data.getMarks().get(y).get(0).label));
-            for (int x = 0; x < data.getMarks().get(y).size(); x++) {
-                line.set(x, new JSONString(data.getMarks().get(y).get(x).label));
+            for (int x = 1; x < data.getMarks().get(y).size(); x++) {
+                line.set(x, new JSONString(""+data.getMarks().get(y).get(x).score));
             }
-            rows.set(y, line);
+            rows.set(y+1, line);
         }
         JsResultsDisplay.plot(rows.getJavaScriptObject(), zoomedClass, zoomedCourse);     
     }
--- war/js-plainui/jsResultsDisplay.js
+++ war/js-plainui/jsResultsDisplay.js
@@ -1,32 +1,37 @@
 var schoolclasses;
 function ResultsDisplay() {
-/**
- * Clears the results view.
- * 
- * @return {undefined}
- */
+    /**
+     * Clears the results view.
+     * 
+     * @return {undefined}
+     */
     this.clear = function () {
         console.log("cleared results view");
     };
 
-/**
- * Plots the results in the viewer. data contains a double array of strings with
- * the first row containing the column headers and the first column containing
- * the row headers.
- * 
- * @param {array of array} data
- * @param {boolean} zoomedClass
- * @param {boolean} zoomedCourse
- * @return {undefined}
- */
+    /**
+     * Plots the results in the viewer. data contains a double array of strings with
+     * the first row containing the column headers and the first column containing
+     * the row headers.
+     * 
+     * @param {array of array} data
+     * @param {boolean} zoomedClass
+     * @param {boolean} zoomedCourse
+     * @return {undefined}
+     */
     this.plot = function (data, zoomedClass, zoomedCourse) {
         schoolclasses = data;
         console.log("Updating results view");
-        console.log("ZoomedClass = "+zoomedClass+", "+zoomedCourse);
-        console.log(data);        
+        console.log("ZoomedClass = " + zoomedClass + ", " + zoomedCourse);
+        console.log(data);
         console.log("plotted results");
+        for (var y in data) {
+            for (var x in data[y]) {
+                console.log("x: " + x + " y: " + y + ", value: " + data[y][x]);
+            }
+        }
     };
-    
+
     /** Shows an empty result table.
      * 
      * @return {undefined}
