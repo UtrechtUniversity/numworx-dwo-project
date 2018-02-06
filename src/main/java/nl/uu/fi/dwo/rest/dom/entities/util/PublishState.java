954365e363db010d5f9bb258c5acbd0eb706c621
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-02-06 11:32:19 +0000

    #LMS-163 Added Archived state to PublishState enum.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@19054 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/dom/entities/util/PublishState.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/entities/util/PublishState.java
@@ -1,15 +1,16 @@
 package nl.uu.fi.dwo.rest.dom.entities.util;
 
 /**
- * Publish state for Sco launchData. Edit means being changed by a teacher, review
- * is currently not being edited. Published is available for students and therefor
- * no longer editable for a teacher. While this feature is unused the default value is
+ * Generic publish states for data. Edit means being changed by an editor, review
+ * is currently not being edited. Published is available for consumers and therefor
+ * no longer editable. While this feature is unused the default value is
  * enum ordinal 2, 'published'.
  * 
  * @author Gert van der Plas
  */
 public enum PublishState {
-    edit, //Someone is editing the launchdata, userid should be added
-    review, // Teachers may review and test the launchdata
-    published //Students may use the launchdata and it is fixed.
+    edit, //Someone is editing the data, userid should be added
+    review, // Data may be reviewed and tested by editors. State may change to edit.
+    published,//Users may use the data. 
+    archived //Data is available for viewing by editors for example but not visible for consumers.
 }
