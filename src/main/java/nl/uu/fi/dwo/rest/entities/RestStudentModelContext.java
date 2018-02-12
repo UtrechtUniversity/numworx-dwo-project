529f43c613ff1deab5ce5cc075fe4e5fe69a7ff1
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-02-12 11:54:40 +0000

    #LMS-163 WIP Dom and Rest classes for studentmodel
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@19131 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/rest/entities/RestStudentModelContext.java
@@ -0,0 +1,63 @@
+package nl.uu.fi.dwo.rest.entities;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import nl.uu.fi.dwo.rest.dom.entities.DomContext;
+import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfile;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContext;
+
+/**
+ * Profile language version of the StudentModel.
+ * 
+ * @author plas0006
+ */
+@XmlRootElement
+
+public class RestStudentModelContext {
+    
+    private DomContext restContext;
+    private DomDwoProfile domDwoProfile;
+    private DomStudentModelContext domStudentModelContext;
+
+    /**
+     * @return the restContext
+     */
+    public DomContext getRestContext() {
+        return restContext;
+    }
+
+    /**
+     * @param restContext the restContext to set
+     */
+    public void setRestContext(DomContext restContext) {
+        this.restContext = restContext;
+    }
+
+    /**
+     * @return the dpmStudentModelContext
+     */
+    public DomStudentModelContext getDomStudentModelContext() {
+        return domStudentModelContext;
+    }
+
+    /**
+     * @param domStudentModelContext the domStudentModelContext to set
+     */
+    public void setDomStudentModelContext(DomStudentModelContext domStudentModelContext) {
+        this.domStudentModelContext = domStudentModelContext;
+    }
+
+    /**
+     * @return the domDwoProfile
+     */
+    public DomDwoProfile getDomDwoProfile() {
+        return domDwoProfile;
+    }
+
+    /**
+     * @param domDwoProfile the domDwoProfile to set
+     */
+    public void setDomDwoProfile(DomDwoProfile domDwoProfile) {
+        this.domDwoProfile = domDwoProfile;
+    }
+
+}
