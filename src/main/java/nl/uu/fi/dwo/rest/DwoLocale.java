ec9a9b2fec591385a24f12720267b02c5b67a786
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-02-21 11:00:43 +0000

    #LMS-181 Locale now knows a default language.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@19241 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/DwoLocale.java
+++ src/main/java/nl/uu/fi/dwo/rest/DwoLocale.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.uu.fi.dwo.rest;
 
 
@@ -12,9 +7,10 @@ package nl.uu.fi.dwo.rest;
  */
 public class DwoLocale {
     private String locale;//IETF BCP 47 language tag required.
+    private static final String defaultLocale="nl";
  
    public DwoLocale(){
-        locale = "nl";
+        locale = defaultLocale;
     }
      
     public DwoLocale(String lcl){
@@ -32,5 +28,9 @@ public class DwoLocale {
      */
     public void setLocale(String locale) {
         this.locale = locale;
+    }
+
+    public String getDefaultLocale() {
+        return defaultLocale;
     }
 }
