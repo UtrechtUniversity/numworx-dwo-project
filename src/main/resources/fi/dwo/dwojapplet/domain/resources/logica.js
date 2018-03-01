62c9e92b9dab35aaa14bde66b3d024fc65786b0c
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-03-01 14:17:32 +0000

    HOTFIX: escape '{' en '}' PLAYER_BASE is apart, niet 13 maar 14
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOJClient/tags/DWOJClient-2.1.28@19391 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/resources/fi/dwo/dwojapplet/domain/resources/logica.js
+++ src/main/resources/fi/dwo/dwojapplet/domain/resources/logica.js
@@ -2,20 +2,20 @@
  * cdplogica script en css loader
  */
 
-var cdplogica = "{13}"
+var cdplogica = "{14}"
 var casServer = "https://app.dwo.nl/ideas/IdeasServlet"
 	
-function script(name) {
-	var elem = document.createElement('script');
+function script(name) '{'
+	var elem = document.createElement("script");
 	elem.src = cdplogica + name;
 	elem.async = false;
 	document.head.appendChild(elem);
-}
+'}'
 
-function css(name) {
-	var elem = document.createElement('link');
-	elem.type='text/css';
-	elem.rel = 'stylesheet';
+function css(name) '{'
+	var elem = document.createElement("link");
+	elem.type="text/css";
+	elem.rel = "stylesheet";
 	elem.href = cdplogica + name;
 	document.head.appendChild(elem);
-}
+'}'
