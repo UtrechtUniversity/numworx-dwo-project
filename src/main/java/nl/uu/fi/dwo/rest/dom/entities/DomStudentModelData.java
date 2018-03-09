da655aff34ca9b8066e9206e0a41f4f0919c4fad
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-03-09 12:13:35 +0000

    #LMS-181 WIP StudentmodelData. Building action setModelData.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@19449 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/dom/entities/DomStudentModelData.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/entities/DomStudentModelData.java
@@ -10,6 +10,50 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class DomStudentModelData extends DomStudentModelDataId implements Cloneable {
     private static final Logger LOG = Logger.getLogger(DomStudentModelData.class.getName());
-    
+    private DomStudentModelContextId modelId;
+    private DomScoContextId scoContextId; 
+    private DomStudentModelStructureScore domStudentModelStructureScore;
+
+    /**
+     * @return the scoContextId
+     */
+    public DomScoContextId getScoContextId() {
+        return scoContextId;
+    }
+
+    /**
+     * @param scoContextId the scoContextId to set
+     */
+    public void setScoContextId(DomScoContextId scoContextId) {
+        this.scoContextId = scoContextId;
+    }
+
+    /**
+     * @return the domStudentModelStructureScore
+     */
+    public DomStudentModelStructureScore getDomStudentModelStructureScore() {
+        return domStudentModelStructureScore;
+    }
+
+    /**
+     * @param domStudentModelStructureScore the domStudentModelStructureScore to set
+     */
+    public void setDomStudentModelStructureScore(DomStudentModelStructureScore domStudentModelStructureScore) {
+        this.domStudentModelStructureScore = domStudentModelStructureScore;
+    }
+
+    /**
+     * @return the modelId
+     */
+    public DomStudentModelContextId getModelId() {
+        return modelId;
+    }
+
+    /**
+     * @param modelId the modelId to set
+     */
+    public void setModelId(DomStudentModelContextId modelId) {
+        this.modelId = modelId;
+    }
     
 }
