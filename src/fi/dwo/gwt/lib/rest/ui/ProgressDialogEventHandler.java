179908936838eb2811dbef7d78bbe4ec2b837809
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-03-27 13:22:12 +0000

    #LMS-208 Added progress dialog for NumTeacher design.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-gwt-lib/trunk@19618 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/dwo/gwt/lib/rest/ui/ProgressDialogEventHandler.java
@@ -0,0 +1,12 @@
+package fi.dwo.gwt.lib.rest.ui;
+
+import com.google.gwt.event.shared.EventHandler;
+
+/**
+ *
+ * @author Gert van der Plas
+ */
+public interface ProgressDialogEventHandler extends EventHandler {
+    void onDialogEvent(ProgressDialogEvent dialogEvent);
+}
+
