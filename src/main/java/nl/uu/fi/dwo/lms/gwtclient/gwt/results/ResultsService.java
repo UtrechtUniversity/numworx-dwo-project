98d292fd8a1fd4a7618a94d06ddf2ea69398034f
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-05-24 08:09:32 +0000

    restore lost files
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOGwtClient/trunk@20238 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/results/ResultsService.java
@@ -0,0 +1,47 @@
+package nl.uu.fi.dwo.lms.gwtclient.gwt.results;
+
+import fi.dwo.gwt.lib.rest.CallManagers.SecuredTeacherResultsManager;
+
+import java.util.logging.Logger;
+
+import nl.uu.fi.dwo.lms.gwtclient.gwt.DwoGlobalVars;
+import nl.uu.fi.dwo.rest.dom.entities.DomClearStudentDataForScoAndClass;
+import nl.uu.fi.dwo.rest.dom.entities.DomContext;
+import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfile;
+import nl.uu.fi.dwo.rest.dom.entities.DomResultsPerTeacher;
+import nl.uu.fi.dwo.rest.entities.RestClearStudentDataForScoAndClass;
+import org.osgi.util.promise.Promise;
+import org.osgi.util.promise.Success;
+
+/**
+ * Persistent model service for Teacher results. Retrieves DomResultsPerTeacher data.
+ * In the future it may cache this data and merge updates into it. it may also request
+ * Updates if required. In example, fetch new data if older than xx seconds or
+ * Check if changes of results exist within the schoolgroup.
+ * 
+ * @author Gert van der Plas
+ */
+class ResultsService {
+
+    private static final Logger LOG = Logger.getLogger(ResultsService.class.getName());
+
+    private SecuredTeacherResultsManager manager = new SecuredTeacherResultsManager();
+    private final DwoGlobalVars dwoGlobalVars;
+    
+    public ResultsService(DwoGlobalVars aDwoGlobalVars){
+        dwoGlobalVars=aDwoGlobalVars;
+    }
+    
+    public Promise<DomResultsPerTeacher> getResultsPerTeacher() {
+        DomContext context = new DomContext();
+        context.setDomHasRole(dwoGlobalVars.getActiveSchoolRoleAndClass().getHasRole());
+        return dwoGlobalVars.getProfile().then(new Success<DomDwoProfile, DomResultsPerTeacher>() {
+
+			@Override
+			public Promise<DomResultsPerTeacher> call(
+					Promise<DomDwoProfile> resolved) throws Exception {
+				return manager.getTeachersResults(context, resolved.getValue());
+			}});
+    }
+
+}
