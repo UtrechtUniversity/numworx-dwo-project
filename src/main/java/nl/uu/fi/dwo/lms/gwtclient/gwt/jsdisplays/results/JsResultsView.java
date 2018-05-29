3ec860d64c400b189b78afd3ac03e923dac43f68
plas0006 <plas0006@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-05-29 12:08:15 +0000

    #LMS-219 Results fixed.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOGwtClient/trunk@20308 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/jsdisplays/results/JsResultsView.java
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/jsdisplays/results/JsResultsView.java
@@ -1,14 +1,19 @@
 package nl.uu.fi.dwo.lms.gwtclient.gwt.jsdisplays.results;
 
+import com.google.gwt.json.client.JSONNumber;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.results.ResultsPresenter;
 import nl.uu.fi.dwo.rest.dom.DomResultTree;
-import nl.uu.fi.dwo.rest.dom.DomTree;
-import nl.uu.fi.dwo.rest.dom.entities.DomCourseOfClass;
+import nl.uu.fi.dwo.rest.dom.entities.DomResultCourseInClass;
+import nl.uu.fi.dwo.rest.dom.entities.DomResultSchoolClass;
+import nl.uu.fi.dwo.rest.dom.entities.DomResultScoContext;
 import nl.uu.fi.dwo.rest.dom.entities.DomResultScore;
+import nl.uu.fi.dwo.rest.dom.entities.DomResultStudent;
+import nl.uu.fi.dwo.rest.dom.entities.DomResultStudentScoContext;
+import nl.uu.fi.dwo.rest.dom.entities.DomResultTeacher;
 
 /**
  * Mapper to allow java interface implementation.
@@ -25,31 +30,50 @@ public class JsResultsView implements ResultsPresenter.Display {
     }
 
     @Override
-        public void setEmptyTableMessage() {
+    public void setEmptyTableMessage() {
         JsResultsDisplay.setEmptyTableMessage();
     }
 
     @Override
-        public void setLoadingTableMessage() {
+    public void setLoadingTableMessage() {
         JsResultsDisplay.setLoadingTableMessage();
     }
 
     private JSONObject buildSubTree(DomResultScore node) {
         JSONObject json = new JSONObject();
         //set course data in node.
-        String name = node.getClass().getSimpleName();        
-        json.put(name, new JSONString(node.getLabel()));
-        LOG.log(Level.INFO, "name: "+name+" label: "+node.getLabel());
+        String classType = node.getClass().getSimpleName();
+        json.put("classType", new JSONString(classType));
+        json.put("label", new JSONString(node.getLabel()));
+        if (node instanceof DomResultStudentScoContext) {
+            String userIdString = ((DomResultStudentScoContext) node).getStudentSco().getUserID().getIdString();
+            json.put("user-id", new JSONString(userIdString));
+        }
+//        json.put("node-id", new JSONNumber(node.getNodeId()));
         //Add children.
         if (node.getChildren() != null && !node.getChildren().isEmpty()) {
             //there are children
             JSONObject children = new JSONObject();
             for (Object o : node.getChildren().values()) {
                 //child node
-                if(o instanceof DomResultScore){
+                String id;
+                if (o instanceof DomResultSchoolClass) {
+                    id = ((DomResultSchoolClass) o).getSchoolClass().getId().getIdString();
+                } else if (o instanceof DomResultCourseInClass) {
+                    id = ((DomResultCourseInClass) o).getCourse().getId().getIdString();
+                } else if (o instanceof DomResultScoContext) {
+                    id = ((DomResultScoContext) o).getScoContext().getId().getIdString();
+                } else if (o instanceof DomResultStudentScoContext) {
+                    id = ((DomResultStudentScoContext) o).getStudentSco().getId().getIdString();
+                } else {
+//                if(o instanceof DomResultStudent){
+//                 id = ((DomResultStudent) o).getStudent().getId().getIdString();
+//                }else{
+                    id = "";
+                }
+                if (o instanceof DomResultScore) {
                     DomResultScore childScore = (DomResultScore) o;
-                    name = childScore.getClass().getSimpleName();   
-                    children.put(childScore.getLabel(), buildSubTree(childScore));
+                    children.put(id, buildSubTree(childScore));
                 }
             }
             json.put("children", children);
@@ -65,15 +89,15 @@ public class JsResultsView implements ResultsPresenter.Display {
 //        LOG.log(Level.INFO, "tree: " + object.toString());
 //        JsModulesOfSchoolclassDisplay.setTree(object);
 //    }
-    
+
     @Override
     public void setResultTree(DomResultTree data) {
-        LOG.log(Level.INFO,"tree data has "+data.getStudentTree().getChildren().values().size()+" student classes.");
-        LOG.log(Level.INFO,"tree data has "+data.getResultTree().getChildren().values().size()+"  result classes.");
-        LOG.log(Level.INFO,"Building tree in json.");
+        LOG.log(Level.INFO, "tree data has " + data.getStudentTree().getChildren().values().size() + " student classes.");
+        LOG.log(Level.INFO, "tree data has " + data.getResultTree().getChildren().values().size() + "  result classes.");
+        LOG.log(Level.INFO, "Building tree in json.");
         JSONObject object = buildSubTree(data.getResultTree());
 //        JSONValue json = DomResultTreeCodec.CODEC.encode(data);
-        LOG.log(Level.INFO,"tree json string is:\n "+object.toString());
+        LOG.log(Level.INFO, "tree json string is:\n " + object.toString());
         JsResultsDisplay.setResultTree(object);
     }
 }
