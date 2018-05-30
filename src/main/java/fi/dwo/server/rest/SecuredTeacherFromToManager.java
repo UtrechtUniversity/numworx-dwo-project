38bf214f6fbd2ad9ced212fb156ca4e405403350
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-05-30 13:58:31 +0000

    SecureFromToManager initial version
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOServer/trunk@20314 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/server/rest/SecuredTeacherFromToManager.java
@@ -0,0 +1,70 @@
+package fi.dwo.server.rest;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.security.PermitAll;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+
+import fi.dwo.commons.persistence.entities.PersistentSchool;
+import fi.dwo.server.PersistentDataManagers.access.AnonDomainAuthorizer;
+import fi.dwo.server.PersistentDataManagers.access.SchoolAdminTeacherDomainAuthorizer.SchoolAdminTeacherState_HR_R_S_SG_U;
+import fi.dwo.server.PersistentDataManagers.core.FromToManager;
+import fi.dwo.server.PersistentDataManagers.core.SchoolManager;
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolFrom;
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolFromTo;
+import nl.uu.fi.dwo.rest.entities.RestContext;
+import nl.uu.fi.dwo.rest.entities.RestSchoolFromTo;
+import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
+
+@PermitAll
+@Path("/secure/teacher/fromto")
+public class SecuredTeacherFromToManager {
+
+    @PUT
+    @Path("get")
+    @Produces({"application/json"})
+    public DomSchoolFromTo get(@Context SecurityContext sc, RestContext rest) throws Dwo2Exception {
+      SchoolAdminTeacherState_HR_R_S_SG_U state = AnonDomainAuthorizer.build().submitUser(sc.getUserPrincipal().getName())
+          .setHasRole(rest.getRestContext().getDomHasRole()).buildSchoolAdminTeacher();
+      DomSchoolFromTo result = new DomSchoolFromTo();
+      return result;
+    }
+
+    @PUT
+    @Path("set")
+    @Produces({"application/json"})
+    public Boolean  set(@Context SecurityContext sc, RestSchoolFromTo rest) throws Dwo2Exception {
+      SchoolAdminTeacherState_HR_R_S_SG_U state = AnonDomainAuthorizer.build().submitUser(sc.getUserPrincipal().getName())
+          .setHasRole(rest.getRestContext().getDomHasRole()).buildSchoolAdminTeacher();
+      
+      PersistentSchool school = null;
+      FromToManager.findEntities(school);
+      
+      Boolean result = Boolean.FALSE;
+      return result;
+    }
+
+    @PUT
+    @Path("getExports")
+    @Produces({"application/json"})
+    public List<DomSchoolFrom> getExports(@Context SecurityContext sc, RestContext rest) throws Dwo2Exception {
+      SchoolAdminTeacherState_HR_R_S_SG_U state = AnonDomainAuthorizer.build().submitUser(sc.getUserPrincipal().getName())
+          .setHasRole(rest.getRestContext().getDomHasRole()).buildSchoolAdminTeacher();
+    
+      List<PersistentSchool> list = SchoolManager.findEntities();
+      return list.stream()
+          .filter(school -> Boolean.TRUE.equals(school.getExport()))
+          .map(PersistentSchool::buildDomSchoolFrom)
+          .collect(Collectors.toList());
+    }
+ 
+    
+}
