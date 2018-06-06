d5c1d80111dadc1c7f393543e89a187adccedb21
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-06-06 08:00:56 +0000

    formatting
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/EBServer/trunk@20396 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/db/DBInstaller.java
+++ src/main/java/fi/dwo/server/db/DBInstaller.java
@@ -10,20 +10,20 @@ import javax.servlet.ServletContextListener;
 
 public class DBInstaller implements ServletContextListener {
 
-	Logger LOG = Logger.getLogger(getClass().getName());
-	
-	public void contextDestroyed(ServletContextEvent e) {
-		LOG.log(Level.INFO, "destroyed");
-	}
+  Logger LOG = Logger.getLogger(getClass().getName());
 
-	public void contextInitialized(ServletContextEvent e) {
-		Driver driver;
-		try {
-			driver = new com.mysql.jdbc.Driver();
-			LOG.log(Level.INFO, "initialized " + driver);	
-		} catch (SQLException e1) {
-			LOG.log(Level.SEVERE, "contextInitialized exception", e1);
-		}
-	}
+  public void contextDestroyed(ServletContextEvent e) {
+    LOG.log(Level.INFO, "destroyed");
+  }
+
+  public void contextInitialized(ServletContextEvent e) {
+    Driver driver;
+    try {
+      driver = new com.mysql.jdbc.Driver();
+      LOG.log(Level.INFO, "initialized " + driver);
+    } catch (SQLException e1) {
+      LOG.log(Level.SEVERE, "contextInitialized exception", e1);
+    }
+  }
 
 }
