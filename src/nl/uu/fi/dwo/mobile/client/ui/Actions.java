7f7ce1edd1ce0daa5a4f442adea6d68126a0e878
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-07-16 07:49:01 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/trunk@20913 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/nl/uu/fi/dwo/mobile/client/ui/Actions.java
@@ -0,0 +1,26 @@
+package nl.uu.fi.dwo.mobile.client.ui;
+
+import com.google.gwt.user.client.Window.Location;
+
+public enum Actions {
+  showMainNav, hideMainNav;
+  private String command;
+
+  Actions() {
+    this.command = name();
+  }
+
+  private static native void sendParent(String cmd) /*-{
+		var domain = $wnd.location.protocol + "//" + $wnd.location.host;
+		var iframe = $wnd.parent;
+		iframe.postMessage(cmd, domain);
+  }-*/;
+
+  public void execute() {
+    sendParent(command);
+  }
+
+  public static boolean isAvailable() {
+    return "none".equals(Location.getParameter("header"));
+  }
+}
