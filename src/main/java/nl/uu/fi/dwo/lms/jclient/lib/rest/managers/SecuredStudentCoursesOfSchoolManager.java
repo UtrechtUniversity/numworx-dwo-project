668f69ed0284225281dc7f6fe764e8a153ce5c31
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-07-25 04:51:19 +0000

    exit getcoursesforclass
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-jclient-lib/trunk@20981 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/lms/jclient/lib/rest/managers/SecuredStudentCoursesOfSchoolManager.java
@@ -0,0 +1,32 @@
+package nl.uu.fi.dwo.lms.jclient.lib.rest.managers;
+
+import nl.uu.fi.dwo.lms.jclient.lib.rest.transport.StoredRestManager;
+import nl.uu.fi.dwo.rest.dom.entities.DomContext;
+import nl.uu.fi.dwo.rest.dom.entities.DomCoursesOfSchoolClass;
+import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfile;
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClass;
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClassAndProfile;
+import nl.uu.fi.dwo.rest.entities.RestSchoolClassAndProfile;
+import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
+import nl.uu.fi.dwo.rest.util.PathId;
+
+public class SecuredStudentCoursesOfSchoolManager {
+
+  public static 
+  DomCoursesOfSchoolClass getCoursesClass(DomSchoolClass schoolClass, DomDwoProfile profile) throws Dwo2Exception {
+    
+    DomCoursesOfSchoolClass result;
+    RestSchoolClassAndProfile rest = new RestSchoolClassAndProfile();
+    DomSchoolClassAndProfile  dom  = new DomSchoolClassAndProfile();
+    DomContext context = StoredRestManager.getInstance().getAuthenticator().getContext();
+    rest.setRestContext(context);
+    rest.setDomSchoolClassAndProfile(dom);
+    dom.setDomDwoProfile(profile);
+    dom.setDomSchoolClass(schoolClass);
+   
+    result = StoredRestManager.getInstance().put("rest/sec:"+PathId.getId(context)+"/student/coursesofschoolclass/get", DomCoursesOfSchoolClass.class, rest);
+    
+    return result;
+    
+  }
+}
