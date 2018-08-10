d7b77243e4ac1c76d6ab8405e7fe1cc7826fa3d6
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-08-10 08:54:26 +0000

    check fields in client, before sending to server
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOGwtClient/trunk@21187 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/persons/AddStudentPresenter.java
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/persons/AddStudentPresenter.java
@@ -13,6 +13,8 @@ import java.util.logging.Level;
 import jsinterop.annotations.JsMethod;
 
 import nl.uu.fi.dwo.lms.gwtclient.gwt.DwoGlobalVars;
+import nl.uu.fi.dwo.lms.gwtclient.gwt.LoggingFailure;
+
 import static nl.uu.fi.dwo.lms.gwtclient.gwt.persons.AddPersonPresenter.LOG;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.ui.AlertDialogWithOKEvent;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.ui.MessageDialogWithOKEvent;
@@ -20,8 +22,12 @@ import nl.uu.fi.dwo.rest.dom.entities.DomNewSingleSchoolStudent;
 import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClass;
 import nl.uu.fi.dwo.rest.dom.entities.DomSingleSchoolStudent;
 import nl.uu.fi.dwo.rest.dom.entities.RoleType;
+import nl.uu.fi.dwo.rest.dom.entities.SimpleValidUserFieldsChecker;
 import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
+import nl.uu.fi.dwo.rest.exceptions.Dwo2ExceptionCode;
 import nl.uu.fi.dwo.rest.locale.DwoLocalesForGWT;
+import nl.uu.fi.dwo.rest.util.Dwo2ExceptionTranslator;
+
 import org.osgi.util.promise.Failure;
 import org.osgi.util.promise.Promise;
 import org.osgi.util.promise.Success;
@@ -33,7 +39,7 @@ import org.osgi.util.promise.Success;
  */
 public class AddStudentPresenter extends AddPersonPresenter {
 
-    private SecuredTeacherSchoolClassManager manager = new SecuredTeacherSchoolClassManager();
+    private final SecuredTeacherSchoolClassManager manager = new SecuredTeacherSchoolClassManager();
     private Map<String, TaggedDomSchoolClass> taggedSchoolClasses;
 
 
@@ -46,9 +52,11 @@ public class AddStudentPresenter extends AddPersonPresenter {
         updateSchoolClasses();
     }
 
+    final private LoggingFailure FAILURE;
     public AddStudentPresenter(EventBus anEventBus, DwoGlobalVars aDwoGlobalVars) {
         eventBus = anEventBus;
         dwoGlobalVars = aDwoGlobalVars;
+        FAILURE = new LoggingFailure(LOG, anEventBus);
     }
 
 
@@ -74,26 +82,41 @@ public class AddStudentPresenter extends AddPersonPresenter {
                 return null;
             }
 
-        },
-                new Failure() {
-            @Override
-            public void fail(Promise<?> resolved) throws Exception {
-                Throwable fail = resolved.getFailure();
-                if (fail instanceof Dwo2Exception) {
-                    LOG.log(Level.SEVERE, fail.getMessage());
-                        eventBus.fireEvent(new AlertDialogWithOKEvent((Dwo2Exception) fail));
-                } else {
-                    LOG.log(Level.SEVERE, fail.getMessage());
-                        eventBus.fireEvent(new AlertDialogWithOKEvent(fail.getMessage()));
-                    //throw directly
-                }
-            }
-        });
+        },  FAILURE);
     }
 
     @JsMethod
     public void submitSingleSchoolStudent(String schoolClassId, String username, String givenName, String insertion, String familyName, String eMail, String password) { 
-        DomSingleSchoolStudent student = new DomSingleSchoolStudent();
+// Verify formfields
+      if (SimpleValidUserFieldsChecker.isNonEmptyNorNull(password, familyName, givenName, eMail, username)) {
+        LOG.log(Level.INFO, "valid required fields.");
+        if (SimpleValidUserFieldsChecker.isNonEmptyNorNull(insertion)) {
+            insertion = insertion.trim();
+        } else {
+            insertion = null;
+        }
+    } else {
+        eventBus.fireEvent(new AlertDialogWithOKEvent(Dwo2ExceptionTranslator.getLocalizedCodeExplanation(DwoGlobalVars.getDwoLocale(), Dwo2ExceptionCode.Rest_Registration_Required_Fields)));
+        return;
+    }
+
+     if (!SimpleValidUserFieldsChecker.isValidUserName(username)) {
+       eventBus.fireEvent(new AlertDialogWithOKEvent(Dwo2ExceptionTranslator.getLocalizedCodeExplanation(DwoGlobalVars.getDwoLocale(), Dwo2ExceptionCode.Rest_Registration_UserName_Invalid)));
+       return;
+     }
+     if (!SimpleValidUserFieldsChecker.isValidEmail(eMail)) {
+          eventBus.fireEvent(new AlertDialogWithOKEvent(Dwo2ExceptionTranslator.getLocalizedCodeExplanation(DwoGlobalVars.getDwoLocale(), Dwo2ExceptionCode.Rest_Registration_Email_Address_Invalid)));
+          return;
+      } else {
+        eMail = eMail.trim();
+      }
+      if (!SimpleValidUserFieldsChecker.isValidPassword(password)) {
+        //invalid password format
+        eventBus.fireEvent(new AlertDialogWithOKEvent(Dwo2ExceptionTranslator.getLocalizedCodeExplanation(DwoGlobalVars.getDwoLocale(), Dwo2ExceptionCode.GUI_AnIncorrectPasswordWasGiven)));
+        return;
+    }      
+      
+      DomSingleSchoolStudent student = new DomSingleSchoolStudent();
         student.setEmail(eMail);
         student.setFamilyName(familyName);
         student.setGivenName(givenName);
@@ -121,21 +144,7 @@ public class AddStudentPresenter extends AddPersonPresenter {
                 return null;
             }
 
-        },
-                new Failure() {
-            @Override
-            public void fail(Promise<?> resolved) throws Exception {
-                Throwable fail = resolved.getFailure();
-                if (fail instanceof Dwo2Exception) {
-                    LOG.log(Level.SEVERE, fail.getMessage());
-                        eventBus.fireEvent(new AlertDialogWithOKEvent((Dwo2Exception) fail));
-                } else {
-                    LOG.log(Level.SEVERE, fail.getMessage());
-                        eventBus.fireEvent(new AlertDialogWithOKEvent(fail.getMessage()));
-                    //throw directly
-                }
-            }
-        });
+        }, FAILURE);
     }
 
 }
