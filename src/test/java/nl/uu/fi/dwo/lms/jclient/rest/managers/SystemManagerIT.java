3605f42723ec1ad5f6c44ccf0c1c586a7931854f
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-01-08 13:51:00 +0000

    Integration tests, nog geen setup
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-jclient-lib/trunk@22272 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/test/java/nl/uu/fi/dwo/lms/jclient/rest/managers/SystemManagerIT.java
@@ -0,0 +1,80 @@
+package nl.uu.fi.dwo.lms.jclient.rest.managers;
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import nl.uu.fi.dwo.lms.jclient.lib.rest.managers.SystemManager;
+import nl.uu.fi.dwo.lms.jclient.lib.rest.transport.RestAuthenticator;
+import nl.uu.fi.dwo.lms.jclient.lib.rest.transport.StoredRestManager;
+import nl.uu.fi.dwo.rest.dom.entities.DomSamlUser;
+import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
+
+public class SystemManagerIT {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	
+	SystemManager manager;
+	
+	@Before
+	public void setUp() throws Exception {
+		RestAuthenticator authenticator = new RestAuthenticator();
+		authenticator.setServerUrlPath(new URL("http://localhost:8080/dwo/rest"));
+		StoredRestManager rest = new StoredRestManager(authenticator);
+		manager = new SystemManager(rest);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSystemManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSchoolDomSchoolId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSchoolString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSchoolClasses() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRequestSamlToken() throws Dwo2Exception {
+		String u = "wimvvv@kennisnet.org";
+		String o = "ENTREE";
+		DomSamlUser user = new DomSamlUser();
+		user.setSamlOrgId(o);
+		user.setSamlUserId(u);
+		DomSamlUser result = manager.requestSamlToken(user);
+		assertNotNull("request token", result.getAuthToken());
+	}
+
+	@Test
+	public void testGetSuggestion() {
+		fail("Not yet implemented");
+	}
+
+}
