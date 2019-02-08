beda4dbe782ed82bbf94e2bb43e449471852c193
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-02-08 07:39:11 +0000

    update voor Onderbouw toetsenbord.
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/KeyboardGWT/trunk@22422 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/keyboard/client/DesktopKeyboardOnderbouw.java
+++ src/nl/uu/fi/dwo/keyboard/client/DesktopKeyboardOnderbouw.java
@@ -9,30 +9,38 @@ public class DesktopKeyboardOnderbouw extends DesktopKeyboard {
 
 	public DesktopKeyboardOnderbouw(boolean small) {
 	    super(small);
-		disableKey(c5);
-		disableKey(c7);
+		atEnd(c5); FKey c32 = c5;
+		atEnd(c7); FKey c33 = c7;
 		disableKey(c8);
 		disableKey(c9);
 		disableKey(c10);
-		disableKey(c11);
+		//disableKey(c11);
 		disableKey(c12);
 		disableKey(c19);
 		disableKey(c20);
 		c25.setHTML("<span class='onderbouw onderbouw-C16'></span>");
 		c26.setHTML("<span class='onderbouw onderbouw-C17'></span>");
-		disableKey(c27);
-		disableKey(c28);
-		disableKey(c29);
-		disableKey(c30);
-		disableKey(c31);
+		c27.setHTML("<span class='onderbouw' style='font-size:16px'>ℝ</span>");
+		c28.setHTML("<span class='onderbouw' style='font-size:18px'>∪</span>");
+		c29.setHTML("<span class='onderbouw' style='font-size:18px'>÷</span>"); c29.addStyleName("bg-blue");
+		c30.setHTML("<span class='onderbouw' style='font-size:18px'>×</span>"); c30.addStyleName("bg-blue");
+		c31.setHTML("<span class='onderbouw' style='font-size:18px'>-</span>"); c31.addStyleName("bg-blue");
+		c32.setHTML("<span class='onderbouw' style='font-size:18px'>+</span>"); c32.addStyleName("bg-blue");
+		c33.setHTML("<span class='onderbouw' style='font-size:18px'>=</span>"); c33.addStyleName("bg-blue");
+		//disableKey(c30);
+		//disableKey(c31);
 	}
 
 	@Override
 	protected void disableKey(FKey key) {
 		super.disableKey(key);
-		HasWidgets parent = (HasWidgets) key.getParent(); key.removeFromParent();
-		parent.add(key);
+		atEnd(key);
 	}
+
+  private void atEnd(FKey key) {
+    HasWidgets parent = (HasWidgets) key.getParent(); key.removeFromParent();
+		parent.add(key);
+  }
 	@Override
 	void onC26(ClickEvent e) {
 		getEditor().insert("°");
@@ -43,20 +51,19 @@ public class DesktopKeyboardOnderbouw extends DesktopKeyboard {
 	}
 
 // disabled keys produce actions....	
-	@Override void onC5(ClickEvent e) {}
-	@Override void onC7(ClickEvent e) {}
 	@Override void onC8(ClickEvent e) {}
 	@Override void onC9(ClickEvent e) {}
 	@Override void onC10(ClickEvent e) {}
-	@Override void onC11(ClickEvent e) {}
 	@Override void onC12(ClickEvent e) {}
 	@Override void onC19(ClickEvent e) {}
 	@Override void onC20(ClickEvent e) {}
-	@Override void onC27(ClickEvent e) {}
-	@Override void onC28(ClickEvent e) {}
-	@Override void onC29(ClickEvent e) {}
-	@Override void onC30(ClickEvent e) {}
-	@Override void onC31(ClickEvent e) {}
+	@Override void onC27(ClickEvent e) { getEditor().insert('ℝ'); }
+	@Override void onC28(ClickEvent e) { getEditor().insert('∪'); }
+	@Override void onC29(ClickEvent e) { getEditor().insert('/'); }
+	@Override void onC30(ClickEvent e) { getEditor().insert('*'); }
+	@Override void onC31(ClickEvent e) { getEditor().insert('-'); }
+    @Override void onC5(ClickEvent e) { getEditor().insert('+'); }
+    @Override void onC7(ClickEvent e) { getEditor().insert('='); }
 
 	@Override
 	public void functionKey(int code) {
