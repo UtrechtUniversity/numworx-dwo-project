a003636d5b1fa61073a369744af765163a79ac27
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-02-21 14:46:37 +0000

    i18n van ndelog
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/KeyboardGWT/trunk@22519 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/keyboard/client/DWOkeyboardBundle.java
+++ src/nl/uu/fi/dwo/keyboard/client/DWOkeyboardBundle.java
@@ -1,11 +1,12 @@
 package nl.uu.fi.dwo.keyboard.client;
 
+import com.google.gwt.i18n.shared.Localizable;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.resources.client.DataResource;
 import com.google.gwt.resources.client.DataResource.MimeType;
 
-public interface DWOkeyboardBundle extends ClientBundle {
+public interface DWOkeyboardBundle extends ClientBundle, Localizable {
   
     // SVG resources
   
