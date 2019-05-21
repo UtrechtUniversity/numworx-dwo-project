591a10e1bad56ac3702df3feb7315cf3763ec7ae
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-05-21 12:05:19 +0000

    Toch terug naar 5.1.34 DEZE WERKT probleem met ophalen van profiel 77 bij 5.1.40 geen idee waarom?
    Daarom fix in eclipselink converter convert latin1 naar utf8
    
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-commons-lib/trunk@23123 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/commons/persistence/JpaEclipseConverterDomStudentModelStructure.java
+++ src/main/java/fi/dwo/commons/persistence/JpaEclipseConverterDomStudentModelStructure.java
@@ -9,15 +9,21 @@ import com.owlike.genson.GensonBuilder;
 import fi.dwo.commons.util.UEscape;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelStructure;
 import nl.uu.fi.dwo.rest.dom.entities.util.GensonMapConverter;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 import org.eclipse.persistence.mappings.DatabaseMapping;
 import org.eclipse.persistence.mappings.converters.Converter;
 import org.eclipse.persistence.sessions.Session;
 
 /**
- *
+ * Deze class houd rekening met de bug in Mysql jdbc driver 5.1.34
  * @author Gert van der Plas
  */
 public class JpaEclipseConverterDomStudentModelStructure implements Converter {
+    private static final Charset ISO_8859_1 = StandardCharsets.ISO_8859_1;
+    private static final Charset UTF8 = StandardCharsets.UTF_8;
     private Genson g = new GensonBuilder().withConverters(new GensonMapConverter()).create();
     
     @Override
@@ -30,6 +36,8 @@ public class JpaEclipseConverterDomStudentModelStructure implements Converter {
     @Override
     public Object convertDataValueToObjectValue(Object dataValue, Session session) {
         DomStudentModelStructure data;
+            String latin1 = (String) dataValue;
+            dataValue = new String( latin1.getBytes(ISO_8859_1), UTF8);
             data = g.deserialize((String) dataValue, DomStudentModelStructure.class);
             return data;
     }
