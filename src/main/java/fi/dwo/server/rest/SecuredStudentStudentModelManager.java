448912ac6016049d78cca3315ac199d0c983a9e1
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-05-21 11:28:36 +0000

    getLRS
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOServer/trunk@23119 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/rest/SecuredStudentStudentModelManager.java
+++ src/main/java/fi/dwo/server/rest/SecuredStudentStudentModelManager.java
@@ -1,5 +1,7 @@
 package fi.dwo.server.rest;
 
+import fi.dwo.commons.persistence.entities.PersistentSchool;
+import fi.dwo.commons.persistence.entities.PersistentUser;
 import fi.dwo.server.PersistentDataManagers.access.AnonDomainAuthorizer;
 import fi.dwo.server.PersistentDataManagers.access.StudentDomainAuthorizer;
 import java.util.ArrayList;
@@ -11,9 +13,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
+
+import org.omg.CORBA.UnknownUserException;
+
+import nl.uu.fi.dwo.rest.dom.entities.DomLRS;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContext;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelData;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelDataScore;
+import nl.uu.fi.dwo.rest.dom.entities.util.AboType;
+import nl.uu.fi.dwo.rest.dom.xapi.Account;
+import nl.uu.fi.dwo.rest.dom.xapi.Agent;
 import nl.uu.fi.dwo.rest.entities.RestContext;
 import nl.uu.fi.dwo.rest.entities.RestStudentModelContextId;
 import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
@@ -90,4 +100,31 @@ public class SecuredStudentStudentModelManager {
 //            throw new Dwo2RestException(e);
 //        }
 //    }    
+
+    @PUT
+    @Produces("application/json")
+    @Path("/getLRS") 
+    public DomLRS getLRS(@Context SecurityContext sc, @Context UriInfo info, RestContext rest) throws Dwo2Exception {
+      StudentDomainAuthorizer.StudentState_HR_R_S_SG_U state = AnonDomainAuthorizer.build().submitUser(sc.getUserPrincipal().getName())
+          .setHasRole(rest.getRestContext().getDomHasRole())
+          .buildStudent();
+      PersistentUser user = state.getContext().getUserCtx().user;
+      Agent agent = new Agent();
+      agent.name = user.getUsername();
+      agent.account = new Account();
+      agent.account.homePage = info.getBaseUri().toASCIIString();
+      agent.account.name = "pid:" + user.buildPersistenceId().getIdString();    
+      DomLRS lrs = new DomLRS();
+      lrs.setAgent(agent);
+      String endpoint = System.getProperty("XAPI_ENDPOINT", "/data/xAPI/");
+      lrs.setEndpoint(endpoint);
+      PersistentSchool school = state.getContext().getUserCtx().school;
+      if (school.getAboType() != AboType.premium) return null;
+      // school afhankelijk?
+      String auth = System.getProperty("XAPI_AUTH", "MzdjZTEwMzE2NzQxN2NhODlmNDNkODA1ZDJhNGY3YjU1MzM3MzE3YjpjY2QzODMwYjc1NWJkY2E3ZDJlYzQ5NmQ0ZTkyZWQwYzJlNDljYjRh");
+      lrs.setAuth("Basic " + auth);
+
+      return lrs;
+    }
+
 }
