b9138a47a140ef5f4d0da529183c438df642906b
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-05-27 09:52:47 +0000

    HOTFIX voor LMS-337
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/tags/DWOplayer-2.3.9@23184 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/mobile/client/ui/activities/ExamActivity.java
+++ src/nl/uu/fi/dwo/mobile/client/ui/activities/ExamActivity.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.Window.Location;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.gwt.user.client.ui.Label;
 
@@ -11,14 +12,16 @@ import nl.uu.fi.dwo.mobile.client.ui.Actions;
 
 public class ExamActivity extends AbstractActivity {
 
-  public ExamActivity() {  }
+  @Inject public ExamActivity() {  }
   
   
   @Override
   public void start(AcceptsOneWidget panel, EventBus eventBus) {
     panel.setWidget(new Label());
     Actions.EXAM.execute();
-    gotoExam("exam/");
+    String base = Location.getParameter("base");
+    if (base == null) base = "";
+    gotoExam(base + "exam/");
   }
 
   private static native void gotoExam(String ref) /*-{
