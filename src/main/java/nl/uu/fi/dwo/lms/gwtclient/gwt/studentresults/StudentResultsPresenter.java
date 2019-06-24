69a6ec31b508259186d202b243bc423c355a8473
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-06-24 07:32:07 +0000

    rich description
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOGwtClient/trunk@23335 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/studentresults/StudentResultsPresenter.java
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/studentresults/StudentResultsPresenter.java
@@ -1,6 +1,8 @@
 package nl.uu.fi.dwo.lms.gwtclient.gwt.studentresults;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
@@ -13,6 +15,8 @@ import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.i18n.client.LocaleInfo;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.user.client.Window.Location;
+import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Tree;
@@ -25,6 +29,7 @@ import nl.uu.fi.dwo.lms.gwtclient.gwt.DwoGlobalVars;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.LoggingFailure;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.jsdisplays.results.JsStudentResultsView;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.results.AbstractResultsPresenter;
+import nl.uu.fi.dwo.lms.gwtclient.gwt.results.StudentScoResultPresenter;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.ui.BasicDisplay;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelCategory;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelCategoryScore;
@@ -110,7 +115,8 @@ public class StudentResultsPresenter extends AbstractResultsPresenter implements
 			DomStudentModelContext model = (DomStudentModelContext) userObject;
 			DomStudentModelStructure structure = model.getModelStructure();
 			String text = structure.getInfo().getDescription().get(lang);
-			Widget description = createDescription(text);
+			String json = structure.getInfo().getDescription().get(lang +"@JSON");
+			Widget description = createDescription(text, json);
 			widget.get().description.setWidget(description);
 			text = structure.getInfo().getTitle().get(lang);
 			widget.get().title.setText(text);
@@ -139,7 +145,8 @@ public class StudentResultsPresenter extends AbstractResultsPresenter implements
 			DomStudentModelStructure structure = model.getModelStructure();
 			DomStudentModelCategory o = structure.getCategories().get(((Integer) userObject).intValue());
 			String text = o.getInfo().getDescription().get(lang);
-			Widget description = createDescription(text);
+			String json = o.getInfo().getDescription().get(lang + "@JSON");
+			Widget description = createDescription(text, json);
 			widget.get().description.setWidget(description);
             text = o.getInfo().getTitle().get(lang);
             widget.get().title.setText(text);
@@ -172,7 +179,8 @@ public class StudentResultsPresenter extends AbstractResultsPresenter implements
 			DomStudentModelCategory o = structure.getCategories().get(cat);
 			DomStudentModelObj oo = o.getObjectives().get(obj);
 			String text = oo.getInfo().getDescription().get(lang);
-	        widget.get().description.setWidget(createDescription(text));
+			String json = oo.getInfo().getDescription().get(lang + "@JSON");
+	        widget.get().description.setWidget(createDescription(text,json));
 	        text = oo.getInfo().getTitle().get(lang);
             widget.get().title.setText(text);
 			service.getScore(model).then( p -> { 
@@ -193,8 +201,35 @@ int percentage(DomStudentModelScore score) {
   		else perc = (int)Math.round(score.getScore()*100/score.getCount());
   return perc;
 }
+  private static final String WISKOPDR_SIG = "H4sIAAAAAA";
+  private String launch_data;
+    
+  
+	private Widget createDescription(String text, String json) {
+	    if (text != null && text.startsWith(WISKOPDR_SIG))
+	    {
+	      Frame wiskopdr;
+	      String random = String.valueOf(System.currentTimeMillis());
+	      LOG.info("Frame = "+random);
+	      String locale = LocaleInfo.getCurrentLocale().getLocaleName();
+	      if ("default".equals(locale) ) locale =  "nl";
+	      String profile = Location.getParameter("profile");
+	      if(profile == null || profile.isEmpty()) profile = "77";
 
-	private Widget createDescription(String text) {
+	      String url = "/dwo/apps/player.html?footer=none&locale="
+	          + locale
+	          + "&profile="
+	          + profile
+	          + "&t=" + random + "#cmi.launch_data:0";
+	      LOG.info("openUrl " + url);
+	      wiskopdr = new Frame(url);
+	      wiskopdr.setStylePrimaryName("score-frame");
+	      launch_data = json;
+	      setAPI(this);
+	      return wiskopdr;
+	    }
+	  
+	  
 		Widget description;
 		SafeHtmlBuilder builder = new SafeHtmlBuilder();
 		if (text == null) text = "";
@@ -202,4 +237,72 @@ int percentage(DomStudentModelScore score) {
 		description = new HTML(builder.toSafeHtml());
 		return description;
 	}
+
+	  private native static void setAPI(StudentResultsPresenter view) /*-{
+      var api = {
+          "LMSGetValue" : function(key) {
+              return view.@nl.uu.fi.dwo.lms.gwtclient.gwt.studentresults.StudentResultsPresenter::getValue(Ljava/lang/String;)(key)
+          },
+          "LMSInitialize" : function(dummy) {
+              return "true"
+          },
+          "LMSGetLastError" : function() {
+              return "0"
+          },
+          "LMSGetDiagnostic" : function(dummy) {
+              return ""
+          },
+          "LMSGetErrorString" : function(code) {
+              return ""
+          },
+          "LMSCommit" : function(dummy) {
+              return "true"
+          },
+          "LMSFinish" : function(dummy) {
+              return "true"
+          },
+          "LMSSetValue" : function(key, value) {
+              return "true"
+          },
+          "Initialize" : function(dummy) {
+              return "true"
+          },
+          "GetValue" : function(key) {
+              return view.@nl.uu.fi.dwo.lms.gwtclient.gwt.studentresults.StudentResultsPresenter::getValue(Ljava/lang/String;)(key)
+          },
+          "SetValue" : function(key, value) {
+              return "true"
+          },
+          "GetLastError" : function() {
+              return "0"
+          },
+          "GetDiagnostic" : function(dummy) {
+              return ""
+          },
+          "GetErrorString" : function(code) {
+              return ""
+          },
+          "Commit" : function(dummy) {
+              return "true"
+          },
+          "Terminate" : function(dummy) {
+              return "true"
+          },
+      // TODO more to follow...           
+      };
+      $wnd.API = api;
+      $wnd.API_1484_11 = api;
+}-*/;
+
+private String getValue(String key) {
+  LOG.info("GetValue " + key);
+  String value = null;
+  if ("cmi.launch_data".equals(key)) value = launch_data;
+  else if ("cmi.mode".equals(key)) value="browse";
+  if(value == null) value = "";
+  String shortValue = value.length() > 10 ? value.substring(0, 10) + "..." : value;
+  LOG.info("result GetValue: " + shortValue);
+  return value;
+}
+
 }
