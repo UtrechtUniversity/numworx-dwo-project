b53c87e09eed0e14a037f56330c082e45c46f28c
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2019-07-05 12:55:21 +0000

    create classes and student from other builder instances
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/edexml/trunk@638 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/edexml/ServerBuilder.java
+++ src/main/java/nl/numworx/edexml/ServerBuilder.java
@@ -3,20 +3,28 @@ package nl.numworx.edexml;
 import java.net.URL;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import nl.uu.fi.dwo.lms.jclient.lib.rest.managers.SecureSchoolAdminSchoolClassManager;
 import nl.uu.fi.dwo.lms.jclient.lib.rest.managers.SecureUserAccountLoginsManager;
 import nl.uu.fi.dwo.lms.jclient.lib.rest.managers.SecureUserAccountManager;
 import nl.uu.fi.dwo.lms.jclient.lib.rest.transport.StoredRestManager;
 import nl.uu.fi.dwo.rest.dom.entities.DomContext;
+import nl.uu.fi.dwo.rest.dom.entities.DomNewSingleSchoolStudent;
 import nl.uu.fi.dwo.rest.dom.entities.DomRole;
 import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClass;
 import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClassFull;
 import nl.uu.fi.dwo.rest.dom.entities.DomSchoolsRolesAndClassesV2;
+import nl.uu.fi.dwo.rest.dom.entities.DomSingleSchoolStudent;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudent;
+import nl.uu.fi.dwo.rest.dom.entities.DomSubmitStudentToSchoolClass;
 import nl.uu.fi.dwo.rest.dom.entities.DomTeacher;
 import nl.uu.fi.dwo.rest.dom.entities.DomUser;
 import nl.uu.fi.dwo.rest.dom.entities.DomUserFull;
@@ -26,7 +34,7 @@ import nl.uu.fi.dwo.rest.exceptions.Dwo2ExceptionCode;
 
 public class ServerBuilder implements Builder {
 
-	
+    private static final Logger LOG = Logger.getLogger(ServerBuilder.class.getName());
 	SecureSchoolAdminSchoolClassManager schoolClassManager;
 	
 	Map<String, DomUserFull> students = new TreeMap<>();
@@ -91,8 +99,7 @@ public class ServerBuilder implements Builder {
 				classes.put(id, klas);
 			}
 		} catch (Dwo2Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOG.log(Level.WARNING, "parseGroepen", e);
 		}
 		return classes;
 	}
@@ -117,8 +124,7 @@ public class ServerBuilder implements Builder {
 				t.forEach(i -> addMember(i.getId().getIdString(), item.getId().getIdString()));
 			}
 		} catch (Dwo2Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOG.log(Level.WARNING, "memberships", e);
 		}
 		return memberships;
 	}
@@ -129,10 +135,99 @@ public class ServerBuilder implements Builder {
 			List<DomTeacher> list = schoolClassManager.getTeachersInSchool();
 			putUsers(list, teachers);
 		} catch (Dwo2Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOG.log(Level.WARNING, "parseLeerkrachten", e);
 		}
 		return teachers;
 	}
 
+	// Add schoolclass (if not exists)
+	public void addSchoolClasses(Map<String, DomSchoolClassFull> set) {
+	  Set<String> names = parseGroepen().values().stream().map(DomSchoolClassFull::getSchoolClassName).collect(Collectors.toSet());
+	  for(Map.Entry<String, DomSchoolClassFull> item:set.entrySet()) {
+	    String key = item.getKey();
+	    DomSchoolClassFull schoolClass = item.getValue();
+	    if (names.contains(schoolClass.getSchoolClassName())) {
+	      // existing class
+	    } else {
+	      try {
+          SecureSchoolAdminSchoolClassManager.submitSchoolClass(schoolClass);
+        } catch (Dwo2Exception e) {
+          LOG.log(Level.WARNING, "addschoolclasses", e);
+        }
+	    }
+	  }
+	  Map<String,DomSchoolClassFull> byName = byName(parseGroepen());
+	  for (Map.Entry<String, DomSchoolClassFull> item: set.entrySet()) {
+	    DomSchoolClassFull value = item.getValue();
+	    item.setValue(byName.getOrDefault(value.getSchoolClassName(), value));
+	  }
+	  
+	} 
+	private Map<String, DomSchoolClassFull> byName(Map<String, DomSchoolClassFull> groepen) {
+    Map<String, DomSchoolClassFull> result = new TreeMap<>();
+    groepen.values().forEach(i -> result.put(i.getSchoolClassName(), i));
+    return result;
+  }
+
+
+  // Single school students
+  public void addStudents (Map<String, DomUserFull> users, Map<String,Collection<String>>members, Map<String, DomSchoolClassFull> classes) {  
+	for (Map.Entry<String, DomUserFull> item: users.entrySet()) {
+	  
+	  String key = item.getKey();
+	  DomSingleSchoolStudent domSingleSchoolStudent = new DomSingleSchoolStudent(item.getValue());
+	  if (domSingleSchoolStudent.getUserName() == null || domSingleSchoolStudent.getUserName().isEmpty()) {
+	    domSingleSchoolStudent.setUserName(key); // FIXME why
+	    item.getValue().setUserName(key);
+	  }
+	  if (domSingleSchoolStudent.getEmail() == null || domSingleSchoolStudent.getEmail().isEmpty())
+	    domSingleSchoolStudent.setEmail("noreply@numworx.nl");
+	  
+	  
+	  Collection<String> collection = members.get(key);
+      Iterator<String> iterator = collection.iterator();
+      if (!iterator.hasNext()) continue; // or iterator is backup class
+      try {
+      DomSchoolClass domSchoolClass = classes.get(iterator.next());
+	  DomNewSingleSchoolStudent submit = new DomNewSingleSchoolStudent();
+	  submit.setDomSchoolClass(domSchoolClass);
+	  submit.setDomSingleSchoolStudent(domSingleSchoolStudent);
+      try {
+        SecureSchoolAdminSchoolClassManager.submitSingleSchoolStudent(submit);
+      }
+      catch (Dwo2Exception e) {
+        Dwo2ExceptionCode code = e.getDwo2Code();
+        code = Dwo2ExceptionCode.Rest_Registration_UserName_exists;
+        if (code == Dwo2ExceptionCode.Rest_Registration_UserName_exists) {
+          iterator = collection.iterator();
+          List<DomStudent> allStudents = SecureSchoolAdminSchoolClassManager.getStudentsInSchool();
+          allStudents.forEach(i -> { 
+            if (i.getUserName().equals(item.getValue().getUserName()))
+                item.getValue().setId(i.getId());
+          });
+         } else {
+           LOG.log(Level.WARNING, "submit student", e);
+          continue;
+         }
+      }
+      // find persistenceid of student by name.
+      List<DomStudent> list = SecureSchoolAdminSchoolClassManager.getStudentsInSchoolClass(domSchoolClass);
+      list.forEach(i -> { 
+          if (i.getUserName().equals(item.getValue().getUserName()))
+              item.getValue().setId(i.getId());
+        });
+      
+      while (iterator.hasNext()) {
+        DomSubmitStudentToSchoolClass s = new DomSubmitStudentToSchoolClass();
+        DomSchoolClass schoolClassTo = classes.get(iterator.next());
+        s.setSchoolClassTo(schoolClassTo);
+        s.setStudent(new DomStudent(item.getValue()));
+        SecureSchoolAdminSchoolClassManager.submitStudentToSchoolClass(s);
+      }
+      } catch (Dwo2Exception e) {
+        LOG.log(Level.WARNING, "submit",e);
+        continue;
+      }      
+	}
+  }
 }
