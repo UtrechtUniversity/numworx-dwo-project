f890f653447819d345bcd9d6113d2673bce24b4f
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2019-09-10 12:47:11 +0000

    OSIRIS dumps inlezen
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/edexml/trunk@667 32ccb885-960d-4b53-a61f-10870b223014

---
+++ src/main/java/nl/numworx/edexml/OsirisBuilder.java
@@ -0,0 +1,170 @@
+package nl.numworx.edexml;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Pattern;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.xml.sax.InputSource;
+
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClassFull;
+import nl.uu.fi.dwo.rest.dom.entities.DomUserFull;
+
+public class OsirisBuilder implements Builder {
+
+	private static final CSVFormat EXCEL = CSVFormat.EXCEL.withHeader().withDelimiter(';');
+	private Map<String, DomUserFull> leerlingen;
+	private Map<String, DomSchoolClassFull> groepen;
+	private Map<String, Collection<String>> memberships;
+	private Map<String, DomUserFull> leerkrachten;
+
+	public OsirisBuilder() {
+		groepen = new TreeMap<>();
+		memberships = new HashMap<>();
+		leerlingen = new TreeMap<>();
+		leerkrachten = new TreeMap<>();
+	}
+	
+	@Override
+	public Map<String, DomUserFull> parseLeerlingen() {
+		return leerlingen;
+	}
+
+	@Override
+	public Map<String, DomSchoolClassFull> parseGroepen() {
+		return groepen;
+	}
+
+	@Override
+	public Map<String, Collection<String>> memberships() {
+		return memberships;
+	}
+
+	@Override
+	public Map<String, DomUserFull> parseLeerkrachten() {
+		return leerkrachten;
+	}
+
+	public void setLeerlingenSource(InputSource is) throws IOException {
+		InputStream in = is.getByteStream();
+		Reader reader = new InputStreamReader(in, "UTF-8");
+		CSVParser parser = CSVParser.parse(reader, EXCEL);
+
+		for( CSVRecord record: parser) {
+			String leerlingnummer = record.get(10);
+			String roepnaam = "student";
+			String achternaam = leerlingnummer;
+			String tussenvoegsel = "";
+			
+			
+			String email = leerlingnummer + "@students.uu.nl";
+			
+			DomUserFull user = new DomUserFull();
+			user.setEmail(email);
+			user.setFamilyName(achternaam);
+			user.setGivenName(roepnaam);
+			user.setInsertion(tussenvoegsel);
+			user.setUserName(leerlingnummer); //????
+			
+			leerlingen.put(leerlingnummer, user);
+			
+			String groepNaam = groepNaam(record.get(1), record.get(2), record.get(3), record.get(4));
+			groepen.putIfAbsent(groepNaam, null);
+			addMember(leerlingnummer, groepNaam);
+		}
+	}
+
+	public void setGroepenSource(InputSource is) throws IOException {
+		InputStream in = is.getByteStream();
+		Reader reader = new InputStreamReader(in, "UTF-8");
+		CSVParser parser = CSVParser.parse(reader, EXCEL);
+
+		for( CSVRecord record: parser) {
+			String collegejaar = record.get(0);
+			String cursus = record.get(1);
+			String blok = record.get(2);
+			String korteNaam = record.get(3);
+			
+			String groepNaam = groepNaam(collegejaar, cursus, blok, korteNaam);
+			
+			DomSchoolClassFull schoolklas = new DomSchoolClassFull();
+			schoolklas.setSchoolClassName(groepNaam);
+			schoolklas.setRegistrationKey(cursus);
+			schoolklas.setIconizer(Boolean.TRUE); // TODO wat is de default?
+			
+			groepen.put(groepNaam, schoolklas);
+		}
+		
+	}
+
+	private String groepNaam(String collegejaar, String cursus, String blok, String korteNaam) {
+		return collegejaar + " - " + cursus + " - " + blok + " - " + korteNaam;
+	}
+
+	private void addMember(String student, String klas) {
+		Collection<String> member = memberships.get(student);
+		if (member == null) {
+			member = new HashSet<>();
+			memberships.put(student, member);
+		}
+		member.add(klas);
+	}
+
+	private  static String createRegexFromGlob(String glob)
+	{
+	    String out = "^";
+	    for(int i = 0; i < glob.length(); ++i)
+	    {
+	        final char c = glob.charAt(i);
+	        switch(c)
+	        {
+	        case '*': out += ".*"; break;
+	        case '?': out += '.'; break;
+	        case '.': out += "\\."; break;
+	        case '\\': out += "\\\\"; break;
+	        default: out += c;
+	        }
+	    }
+	    out += '$';
+	    return out;
+	}
+	public void setLeerkrachtenSource(InputSource is) throws IOException {
+		InputStream in = is.getByteStream();
+		Reader reader = new InputStreamReader(in, "UTF-8");
+		CSVParser parser = CSVParser.parse(reader, EXCEL);
+
+		for( CSVRecord record: parser) {
+			String solisid = record.get(2);
+			String roepnaam = "docent";
+			String achternaam = solisid;
+			String tussenvoegsel = "";
+			String email = solisid + "@soliscom.uu.nl";
+			DomUserFull user = new DomUserFull();
+			user.setEmail(email);
+			user.setFamilyName(achternaam);
+			user.setGivenName(roepnaam);
+			user.setInsertion(tussenvoegsel);
+			user.setUserName(solisid); //????
+			
+			leerkrachten.put(solisid, user);
+			
+			String groepnaam = groepNaam(record.get(1), record.get(0), "*" , "*"); // FIXME stars
+			if (! groepen.isEmpty()) {
+				Pattern pattern = Pattern.compile(createRegexFromGlob(groepnaam));
+				final String id = solisid;
+				groepen.keySet().stream().filter(pattern.asPredicate()).forEach(n -> addMember(id, n));
+			} else 
+				addMember(solisid, groepnaam);
+		}
+	}
+}
---
+++ src/test/java/nl/numworx/edexml/ServerUUTest.java
@@ -0,0 +1,97 @@
+package nl.numworx.edexml;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Map;
+
+import org.xml.sax.InputSource;
+
+import junit.framework.TestCase;
+import nl.numworx.edexml.ServerUUTest.Translator;
+import nl.uu.fi.dwo.rest.DwoLocale;
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClass;
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClassFull;
+import nl.uu.fi.dwo.rest.dom.entities.DomUserFull;
+import nl.uu.fi.dwo.rest.exceptions.Dwo2ExceptionCode;
+import nl.uu.fi.dwo.rest.util.DWO2ExceptionTranslatorInterface;
+import nl.uu.fi.dwo.rest.util.Dwo2ExceptionTranslator;
+
+public class ServerUUTest extends TestCase {
+	
+	static class Translator implements DWO2ExceptionTranslatorInterface {
+
+		@Override
+		public String encodeJSON(Dwo2ExceptionCode code, String message) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String decodeMessageInJSON(String json) {
+			return json;
+		}
+
+		@Override
+		public Dwo2ExceptionCode decodeCodeInJSON(String json) {
+		  System.err.println(json);
+			return Dwo2ExceptionCode.Rest_CanNotReachServer;
+		}
+
+		@Override
+		public String getLocalizedCodeExplanation(DwoLocale locale, Dwo2ExceptionCode code) {
+			return code.toString();
+		}
+
+	}
+
+	static { 
+		Dwo2ExceptionTranslator.setTranslator(new Translator());
+	}
+	
+
+	private ServerBuilder builder;
+
+	protected void setUp() throws Exception {
+		builder = new ServerBuilder();
+		builder.setSource("project_wim", "d7f69547d875d5984c7c0d185f62a81b", new URL("http://localhost:8080/dwo/"));
+		builder.setRealm("@uu.dwo.nl");
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testUU() throws Exception {
+	  OsirisBuilder input = new OsirisBuilder();
+	  {
+			InputStream in  = getClass().getResourceAsStream("/NUMWORX-STUDENT.csv");
+			assertNotNull(in);
+			InputSource is = new InputSource();
+			is.setByteStream(in);
+			input.setLeerlingenSource(is);
+	  }{
+			InputStream in  = getClass().getResourceAsStream("/NUMWORX_CURS.csv");
+			assertNotNull(in);
+			InputSource is = new InputSource();
+			is.setByteStream(in);
+			input.setGroepenSource(is);
+	  }{
+			InputStream in = getClass().getResourceAsStream("/NUMWORX-DOCENT.csv");
+			assertNotNull(in);
+			InputSource is = new InputSource();
+			is.setByteStream(in);
+			input.setLeerkrachtenSource(is);
+	  }
+      Map<String, DomUserFull> leerlingen = input.parseLeerlingen();
+      Map<String, DomSchoolClassFull> groepen = input.parseGroepen();
+      Map<String, DomUserFull> leerkrachten = input.parseLeerkrachten();
+      Map<String, Collection<String>> members = input.memberships();
+      
+      builder.addSchoolClasses(groepen);
+      builder.addStudents(leerlingen, members, groepen);
+      builder.addTeachers(leerkrachten, members, groepen);
+	
+	}
+	
+}
