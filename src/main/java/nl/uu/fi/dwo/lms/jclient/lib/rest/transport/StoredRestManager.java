67c65552199f320915afe66c2fc0fa94114cccf5
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2020-01-29 13:37:41 +0000

    met een runner voor een supplier
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-jclient-lib/trunk@24346 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/lms/jclient/lib/rest/transport/StoredRestManager.java
+++ src/main/java/nl/uu/fi/dwo/lms/jclient/lib/rest/transport/StoredRestManager.java
@@ -1,9 +1,13 @@
 package nl.uu.fi.dwo.lms.jclient.lib.rest.transport;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import nl.uu.fi.dwo.rest.RestListClassTypes;
+import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
 
 /**
  * StoredRestManager ensures there is only one copy or each rest-fetched persistent object. It
@@ -33,5 +37,39 @@ public class StoredRestManager extends RestManager {
   public static StoredRestManager getInstance() {
     return storedInstance;
   }
+
+  @FunctionalInterface interface DwoSupplier<T> {
+	  T accept() throws Dwo2Exception;
+  }
+  
+  private <T> T run( DwoSupplier<T> result) throws Dwo2Exception {
+	  try {
+		return result.accept();
+	} catch (Dwo2Exception e) {
+		// inspect E, if Authenticated error, try re-authenticate and again
+		throw e;
+	}
+  }
+  
+  
+@Override
+public <T> T get(String path, Class<T> c) throws Dwo2Exception {
+	return run (() -> super.get(path, c));
+}
+
+@Override
+public <T> List<T> getList(String path, RestListClassTypes type) throws Dwo2Exception {
+	return run (() -> super.getList(path, type));
+}
+
+@Override
+public <T> T put(String path, Class<T> c, Object o) throws Dwo2Exception {
+	return run(() -> super.put(path, c, o));
+}
+
+@Override
+public <T> List<T> getPutList(String path, RestListClassTypes type, Object o) throws Dwo2Exception {
+	return run(() -> super.getPutList(path, type, o));
+}
 
 }
