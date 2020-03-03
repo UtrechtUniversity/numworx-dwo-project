1225f2374fc5072fa33d8d0b64e7390fc7bfe3cc
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2020-03-03 10:42:51 +0000

    Versie met alle knopjes min of meer
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOJClient/branches/leerdoel@24521 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/dwojapplet/gui/domainmodel/LeerdomeinEditPanel2.java
+++ src/main/java/fi/dwo/dwojapplet/gui/domainmodel/LeerdomeinEditPanel2.java
@@ -3,8 +3,10 @@ package fi.dwo.dwojapplet.gui.domainmodel;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,6 +17,7 @@ import java.util.TreeMap;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
@@ -29,7 +32,9 @@ import javax.swing.border.TitledBorder;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.plaf.basic.BasicMenuBarUI;
+import javax.swing.plaf.basic.BasicMenuUI;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 
@@ -37,6 +42,7 @@ import fi.beans.numworxlf.Constants;
 import fi.beans.numworxlf.JButton;
 import fi.beans.numworxlf.JScrollPane;
 import fi.beans.numworxlf.JTextField;
+import fi.beans.numworxlf.NumworxTextFieldUI;
 import fi.dwo.commons.system.TextMapper;
 import fi.dwo.dwojapplet.gui.wiskopdr.WiskOpdr;
 import fi.dwo.dwojapplet.gui.wiskopdr.WiskOpdrEditPanel;
@@ -58,17 +64,22 @@ public class LeerdomeinEditPanel2 extends JPanel implements ActionListener, Tree
   JTree tree;
   DefaultTreeModel model;
   DynamicUtilTreeNode root;
-  private JPanel settings;
+  private JComponent settings;
   JFormattedTextField slip, init, learn;
+
+  private Box settingsRO;
+  private JPanel settingsRW;
 
   @Override
   public void actionPerformed(ActionEvent e) {
     // TODO Auto-generated method stub
 
   }
+  private static final Font font = new Font("SansSerif", Font.PLAIN, 12);
 
   public LeerdomeinEditPanel2() {
     super(new BorderLayout());
+
   south = Box.createHorizontalBox();
   south.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
   south.setOpaque(true);
@@ -95,7 +106,7 @@ public class LeerdomeinEditPanel2 extends JPanel implements ActionListener, Tree
   bewerken = new JButton(TextMapper.getText(TextMapper.GUIH_EDIT));
   title = new JLabel(getTitle());
   title.setForeground(Constants.COLOR20);
-  title.setFont(title.getFont().deriveFont(24f));
+  title.setFont(font.deriveFont(24f));
   north.add(bewerken);
   north.add(Box.createHorizontalGlue());
   north.add(title);
@@ -115,14 +126,23 @@ public class LeerdomeinEditPanel2 extends JPanel implements ActionListener, Tree
   
   JSplitPane split = new JSplitPane();
   split.setDividerSize(20);
+  split.setResizeWeight(0.8);
   split.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
   split.setBackground(Constants.COLOR20);
   setBackground(Constants.COLOR20);
   JMenu Bestand = new JMenu(TextMapper.getText("file"));
   JMenu Bewerken = new JMenu(TextMapper.getText("edit"));
   bar.setBackground(Constants.COLOR21);
+  bar.setBorder(BorderFactory.createEmptyBorder());
+  Dimension pref = bar.getPreferredSize();
+  pref.height = 26; // same as textfield subtitle.
+  bar.setPreferredSize(pref);
   Bestand.setBackground(Constants.COLOR21);
+  Bestand.setForeground(Constants.COLOR15);
+  Bestand.setUI(new BasicMenuUI() {public void paint(Graphics g) {}});
   Bewerken.setBackground(Constants.COLOR21);
+  Bewerken.setForeground(Constants.COLOR15);
+  Bewerken.setUI(new BasicMenuUI() {public void paint(Graphics g) {}});
   bar.setOpaque(true);
   bar.setUI(new BasicMenuBarUI());
   bar.add(Bestand);
@@ -147,15 +167,31 @@ public class LeerdomeinEditPanel2 extends JPanel implements ActionListener, Tree
   root = new DynamicUtilTreeNode(v,v);
   model = new DefaultTreeModel(root);   
   tree = new JTree(model);
-  JPanel leftBox = new JPanel(new BorderLayout());
-  leftBox.setBorder(BorderFactory.createLineBorder(Constants.COLOR10));
+  DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();
+  tree.setCellRenderer(renderer);
+
+  
+
+  
+  leftBox = new JPanel(new BorderLayout());
+  leftBox.setBorder(BorderFactory.createLineBorder(Constants.COLOR13));
   leftBox.add(bar, BorderLayout.NORTH);
-  leftBox.add(new JScrollPane(tree), BorderLayout.CENTER);
-  //leftBox.add(comp)
+  JScrollPane scrollpane = new JScrollPane(tree);
+  scrollpane.setViewportBorder(BorderFactory.createEmptyBorder());
+  scrollpane.setBorder(BorderFactory.createEmptyBorder());
+  leftBox.add(scrollpane, BorderLayout.CENTER);
+  leftSouth = Box.createHorizontalBox();
+  JButton filter = new JButton("Filter leerdoelen");
+  leftSouth.add(Box.createHorizontalGlue());
+  leftSouth.add(filter);
+  leftSouth.add(Box.createHorizontalGlue());
+  leftSouth.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+  leftBox.add(leftSouth, BorderLayout.SOUTH);
   split.setLeftComponent(leftBox);
   
   JPanel rightBox = new JPanel(new BorderLayout());
   subtitle = new JTextField();
+  subtitle.setFont(font.deriveFont(14f));
   container = new JPanel(new GridLayout(1,1));
   container.setPreferredSize(new Dimension(500 , 325));
   rightBox.add(subtitle, BorderLayout.NORTH);
@@ -166,9 +202,78 @@ public class LeerdomeinEditPanel2 extends JPanel implements ActionListener, Tree
   learn = new JFormattedTextField(NumberFormat.getInstance());
   
   
-  settings = new JPanel();
-  settings.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Constants.COLOR15), "Instellingen", TitledBorder.CENTER, TitledBorder.CENTER, null, Constants.COLOR15));    
-  rightBox.add(settings, BorderLayout.SOUTH);
+  settingsRO = Box.createHorizontalBox();
+  settingsRO.setOpaque(true);
+  settingsRO.setBackground(Constants.COLOR20);
+  JButton voorkennisRO = new JButton("Voorkennis"); voorkennisRO.setFont(font);
+  settingsRO.add(voorkennisRO);
+  settingsRO.add(Box.createHorizontalGlue());
+  JButton genrRO = new JButton("Getal&Ruimte"); genrRO.setFont(font);
+  settingsRO.add(genrRO);
+  settingsRO.add(Box.createHorizontalStrut(10));
+  JButton mwRO = new JButton("Moderne Wiskunde"); mwRO.setFont(font);
+  settingsRO.add(mwRO);
+  settingsRO.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+  
+  
+  settings = settingsRW = new JPanel(null);
+  settings.setLayout(new BoxLayout(settings, BoxLayout.PAGE_AXIS));
+  settings.setBorder(BorderFactory.createTitledBorder(BorderFactory.createMatteBorder(1,0,0,0,Constants.COLOR15), "Instellingen", TitledBorder.CENTER, TitledBorder.CENTER, null, Constants.COLOR15));    
+
+  Box bkt = Box.createHorizontalBox();
+  JButton voorkennis = new JButton("Voorkennis");voorkennis.setFont(font);
+  bkt.add(voorkennis);
+  bkt.add(Box.createHorizontalGlue());
+  settings.add(bkt);
+  settings.add(Box.createVerticalStrut(10));
+  
+  bkt = Box.createHorizontalBox();
+  JLabel l;
+  JLabel parametersLabel = new JLabel("Knowledge tracing parameters:");
+    parametersLabel.setForeground(Constants.COLOR15);
+    bkt.add(parametersLabel);
+    bkt.add(Box.createHorizontalGlue());
+  bkt.add(l = new JLabel("Init "));
+    l.setForeground(Constants.COLOR15);
+    init = new JFormattedTextField(NumberFormat.getInstance());
+    init.setUI(NumworxTextFieldUI.createUI(init));
+    init.setPreferredSize(new Dimension(50,20));
+    init.setMaximumSize(new Dimension(50,30));
+    bkt.add(init);
+  bkt.add(l=new JLabel(" Learn "));
+  l.setForeground(Constants.COLOR15);
+    learn = new JFormattedTextField(NumberFormat.getInstance());
+    learn.setUI(NumworxTextFieldUI.createUI(learn));
+    learn.setPreferredSize(new Dimension(50,20));
+    learn.setMaximumSize(new Dimension(50,30));
+    bkt.add(learn);
+  bkt.add(l=new JLabel(" Slip "));
+  l.setForeground(Constants.COLOR15);
+    slip = new JFormattedTextField(NumberFormat.getInstance());
+    slip.setUI(NumworxTextFieldUI.createUI(slip));
+    slip.setColumns(6);
+    slip.setPreferredSize(new Dimension(50,20));
+    slip.setMaximumSize(new Dimension(50,30));
+   bkt.add(slip);
+  settings.add(bkt);
+  settings.add(Box.createVerticalStrut(10));
+  
+  bkt = Box.createHorizontalBox();
+  parametersLabel = new JLabel("Koppeling lesmateriaal:");
+  parametersLabel.setForeground(Constants.COLOR15);
+  bkt.add(parametersLabel);
+  bkt.add(Box.createHorizontalGlue());
+  JButton genr = new JButton("Getal&Ruimte"); genr.setFont(font);
+  bkt.add(genr);
+  bkt.add(Box.createHorizontalStrut(10));
+  JButton mw = new JButton("Moderne Wiskunde"); mw.setFont(font);
+  bkt.add(mw);
+  
+  settings.add(bkt);
+  
+  
+  rightBox.add(settings = settingsRO, BorderLayout.SOUTH);
+  rightBox.setBorder(BorderFactory.createLineBorder(Constants.COLOR13));
   split.setRightComponent(rightBox);
 
   JTextField leerdoelTitelEditor = subtitle;
@@ -194,21 +299,42 @@ public class LeerdomeinEditPanel2 extends JPanel implements ActionListener, Tree
   private boolean editable;
   private WiskOpdrEditPanel wiskOpdrEditPanel;
   private JPanel container;
+
+  private Box leftSouth;
+
+  private JPanel leftBox;
   public void setEditable(boolean b) {
     editable = b;
+    Container parent = settings.getParent();
+    parent.remove(settings);
+    boolean visible = settings.isVisible();
+    if (b) {
+      settings = settingsRW;
+    } else {
+      settings = settingsRO;
+    }
+    parent.add(settings, BorderLayout.SOUTH);
+    settings.setVisible(visible);
+    leftSouth.setVisible(!b);
     fillSelection();
     if (b) {
+      leftBox.setBorder(BorderFactory.createLineBorder(Constants.COLOR13));
       bewerken.setText(TextMapper.getText(TextMapper.GUIH_STOP_EDIT));
       title.setText("Editor " + getTitle());
       bar.show();
       south.show();
       subtitle.setEditable(true);
+      ((DefaultTreeCellRenderer) tree.getCellRenderer()).setBackgroundNonSelectionColor(Color.WHITE);
+      tree.setBackground(Color.WHITE);
     } else {
+      leftBox.setBorder(BorderFactory.createLineBorder(Constants.COLOR20));
       bewerken.setText(TextMapper.getText(TextMapper.GUIH_EDIT));
       title.setText(getTitle());
       bar.hide();
       south.hide();
       subtitle.setEditable(false);
+      ((DefaultTreeCellRenderer) tree.getCellRenderer()).setBackgroundNonSelectionColor(Constants.COLOR20);
+      tree.setBackground(Constants.COLOR20);
     }
   }
 
