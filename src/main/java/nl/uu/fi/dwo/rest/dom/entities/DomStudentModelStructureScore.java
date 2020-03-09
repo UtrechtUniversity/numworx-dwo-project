68758208d6ec65c6f28398f82da9903f148a83b0
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2020-03-09 14:23:56 +0000

    opruimen methode
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@24554 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/dom/entities/DomStudentModelStructureScore.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/entities/DomStudentModelStructureScore.java
@@ -1,6 +1,5 @@
 package nl.uu.fi.dwo.rest.dom.entities;
 
-import java.beans.Transient;
 import java.util.List;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -25,25 +24,5 @@ public class DomStudentModelStructureScore extends DomStudentModelScore<DomStude
     public void setCategories(List<DomStudentModelCategoryScore> categories) {
         this.setChildren(categories);
     }
-    
-    /**
-     * Assumes leafs in the tree have correct value and recalculates ancestral node values. Adding 
-     * properties.
-     */
-    @Transient
-    public void recalculateAncestors(){
-        setCount(0L);
-        setScore(0L);
-        for (DomStudentModelCategoryScore cat :this.getCategories()){
-            long count =0L;
-            double score = 0.0;
-            for (DomStudentModelObjectiveScore obj :cat.getObjectives()){
-                count += obj.getCount();
-                score += obj.getScore();
-            }
-            cat.setCount(count); setCount(count + getCount());
-            cat.setScore(score); setScore(score + getScore());
-        }
-    }
-    
+        
 }
