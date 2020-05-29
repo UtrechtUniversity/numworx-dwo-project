6f773f30c67dcbc74e6ff94b3669c09d7cee2f3b
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2020-05-29 12:11:59 +0000

    gwtclientwrap met post en antiCRSFtoken
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/EBServer/trunk@24953 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/server/db/GwtClientWrap.java
@@ -0,0 +1,103 @@
+package fi.dwo.server.db;
+
+import java.io.CharArrayWriter;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.security.SecureRandom;
+import java.util.logging.Logger;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import org.tuckey.web.filters.urlrewrite.gzip.FilterServletOutputStream;
+
+public class GwtClientWrap implements Filter {
+
+	
+	static class MyOutput extends FilterServletOutputStream {
+
+		public MyOutput(OutputStream out) {
+			super(out);
+		}
+		
+	}
+	
+	static class MyWrapper extends HttpServletResponseWrapper {
+
+//		MyOutput out;
+		Writer writer;
+		
+		public MyWrapper(HttpServletResponse response) {
+			super(response);
+		}
+
+		@Override
+		public synchronized ServletOutputStream getOutputStream() throws IOException {
+			throw new IllegalStateException(); // force use of getWriter
+//			if (out == null) {
+//				out = new MyOutput(super.getOutputStream());
+//			}
+//			return out;
+		}
+
+		@Override
+		public synchronized PrintWriter getWriter() throws IOException {
+			if (writer == null) 
+				writer = new CharArrayWriter(10240*15);
+			return new PrintWriter(writer);
+		}
+	}
+	
+	
+	
+	final Logger LOG = Logger.getLogger(getClass().getName());
+	final SecureRandom random = new SecureRandom();
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		// post niet toegestaan
+		HttpServletRequest http = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		String method = http.getMethod();
+		if ("POST".equals(method)) {
+			resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			return;
+		}
+		MyWrapper wrapper;
+		wrapper = new MyWrapper(resp);
+		
+		chain.doFilter(request, wrapper);
+		if (wrapper.writer != null) {
+			String content = wrapper.writer.toString();
+			wrapper = null;
+			content = content.replace("<form ", "<form method='post' ");
+			content = content.replace("</form>", "<input type='hidden' value='" + random.nextLong()
+					+ "' name='antiCSRFtoken' ></form>");
+			resp.getWriter().write(content);
+		}
+
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+}
