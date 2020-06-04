316fea12dc3c1115ad47b985901ee6fb2aa640b6
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2020-06-04 22:14:16 +0000

    nekot API
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOGwtClient/trunk@24988 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/BootPanel.java
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/BootPanel.java
@@ -1,9 +1,13 @@
 package nl.uu.fi.dwo.lms.gwtclient.gwt;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
+
+import fi.dwo.gwt.lib.rest.GwtRestVars;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,7 +18,7 @@ import nl.uu.fi.dwo.lms.gwtclient.gwt.dagger.DaggerBootComponent;
  *
  * @author G.A.J. van der Plas
  */
-public class BootPanel  implements EntryPoint, Window.ClosingHandler{
+public class BootPanel  implements EntryPoint, Window.ClosingHandler, CloseHandler<Window>{
     private BootPanelController controller;
     
     private static final Logger LOG = Logger.getLogger(BootPanel.class.getName());
@@ -48,6 +52,7 @@ public class BootPanel  implements EntryPoint, Window.ClosingHandler{
         controller = DaggerBootComponent.create().controller();
         controller.go(RootLayoutPanel.get());
         Window.addWindowClosingHandler(this);
+        Window.addCloseHandler(this);
 
     }
 
@@ -59,6 +64,22 @@ public class BootPanel  implements EntryPoint, Window.ClosingHandler{
         }else{
             event.setMessage(null);
         }
+    }
+
+    private static native void sendBeacon(String url, String query) /*-{
+      var data = new URLSearchParams(query);
+      navigator.sendBeacon(url, data);
+    }-*/;
+    
+    @Override
+    public void onClose(CloseEvent<Window> event) {
+      GwtRestVars vars = GwtRestVars.getInstance();
+      String query = vars.getLogoutQuery();
+      if (query != null) {
+        String url = vars.getLogoutURL();
+        sendBeacon(url, query);
+      }
+      
     }
 
 }
