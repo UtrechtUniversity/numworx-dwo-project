2702259c7848b8cc609fc0f62fd371d9f5c6bff8
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2020-09-01 07:01:20 +0000

    leerdomein studenten resultaten: totalCount
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@25334 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/dom/entities/DomStudentModelScore.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/entities/DomStudentModelScore.java
@@ -18,7 +18,7 @@ public class DomStudentModelScore<T extends DomStudentModelScore> {
     
     private double redScore, greenScore;
     private long redCount, greenCount;
-
+    private long totalCount;
     
     /**
      * @return the score
@@ -33,6 +33,7 @@ public class DomStudentModelScore<T extends DomStudentModelScore> {
     public void setScore(double score) {
         if (score < 0.5) { redScore = score;greenScore = 0; redCount = 1; greenCount = 0; }
         else { greenScore = score; redScore = 0; greenCount = 1; redCount = 0; }
+        totalCount = 1;
     }
 
 
@@ -42,15 +43,27 @@ public class DomStudentModelScore<T extends DomStudentModelScore> {
     public long getCount() {
         return redCount + greenCount;
     }
+    
+    public long getTotalCount() {
+    	return totalCount;
+    }
             
     public void setScore(double greenScore, long greenCount, double redScore, long redCount) {
       this.greenScore = greenScore;
       this.greenCount = greenCount;
       this.redScore = redScore;
-      this.redCount = redCount;     
+      this.redCount = redCount;
+      totalCount = redCount + greenCount;
     }
-    
-    
+
+    public void setScore(double greenScore, long greenCount, double redScore, long redCount, long totalCount) {
+        this.greenScore = greenScore;
+        this.greenCount = greenCount;
+        this.redScore = redScore;
+        this.redCount = redCount;
+        this.totalCount = totalCount;
+      }
+
     /**
      * @return the children
      */
@@ -87,26 +100,28 @@ public class DomStudentModelScore<T extends DomStudentModelScore> {
           int c = children.size();
           if (c > 0) {
             double redScore = 0, greenScore = 0;
-            long redCount = 0, greenCount = 0;
+            long redCount = 0, greenCount = 0, totalCount = 0;
             for(DomStudentModelScore child: children) {
               child.recalculateAncestors();
               long rc = child.getRedCount();
               long gc = child.getGreenCount();
+              long tc = child.getTotalCount();
               double rs = child.getRedScore();
               double gs = child.getGreenScore();
-              if (rc == 0L) rs = 0.5;
-              if (gc == 0L) gs = 0.5;
               redScore += rs;
               greenScore += gs;
               redCount += rc;
               greenCount += gc;
+              totalCount += tc;
             }
-            if (greenCount == 0) { greenScore = 0.0; } else { greenCount = c; }
-            if (redCount == 0)   { redScore = 0.0; } else { redCount = c; }
-            setScore(greenScore, greenCount, redScore, redCount);
+            if (greenCount == 0) { greenScore = 0.0; }
+            if (redCount == 0)   { redScore = 0.0; }
+            setScore(greenScore, greenCount, redScore, redCount, totalCount);
           } else {
             setScore(0.0,0L,0.0,0L);
           }
+        } else {
+        	this.totalCount = 1;
         }
       
     }
