c2ff9e1c408b368e48cd35cb9efd38d8621b96b3
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2020-09-04 13:50:10 +0000

    met genson annotatie
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@25356 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/dom/xapi/Extensions.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/xapi/Extensions.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Extensions {
 
-//  @com.owlike.genson.annotation.JsonProperty("http://www.dwo.nl/extensions/objectives")
+  @com.owlike.genson.annotation.JsonProperty("http://www.dwo.nl/extensions/objectives")
   @com.fasterxml.jackson.annotation.JsonProperty("http://www.dwo.nl/extensions/objectives")
   public List<String> objectives;
 
