16c1c862f01f8091d2ef6d5140916b301420807a
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2020-11-06 08:45:04 +0000

    opmaat voor database 1.5.4
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-commons-lib/trunk@25668 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/commons/persistence/entities/PersistentCourseData.java
@@ -0,0 +1,143 @@
+package fi.dwo.commons.persistence.entities;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import nl.uu.fi.dwo.rest.dom.entities.util.DelState;
+
+@Entity
+@Table(name = "tblcoursedata", schema = "")
+public class PersistentCourseData {
+  @Id
+  @Basic(optional = false)
+  @Column(name = "courseID", nullable = false)
+  private Long courseID;
+
+  @Basic(optional = false)
+  @Column(name = "lastChangeTimeStamp", nullable = true)
+  private Long lastChangeTimeStamp;
+
+//  For future use in case of optimistic locking.    
+  @Version
+  @Column(name = "optlock", columnDefinition = "integer DEFAULT 0", nullable = false)
+  private Long optlock;
+
+  @Basic(optional = false)
+  @NotNull
+  @Lob
+  @Size(min = 0, max = 16777215)
+  @Column(name = "description", nullable = false, length = 16777215)
+  private String description;
+
+  @Lob
+  @Column(name = "imageData")
+  private byte[] imageData;
+
+  @Lob
+  @Column(name = "descriptionbytes")
+  private byte[] descriptionbytes;
+
+  @NotNull
+  @Column(name="del",nullable = false)
+  private DelState delState = DelState.not;
+
+  @PrePersist
+  @PreUpdate
+  void changeTimestamp() {
+      lastChangeTimeStamp = System.currentTimeMillis();
+  }
+
+  /**
+   * @return the courseID
+   */
+  public Long getCourseID() {
+    return courseID;
+  }
+
+  /**
+   * @param courseID the courseID to set
+   */
+  public void setCourseID(Long courseID) {
+    this.courseID = courseID;
+  }
+
+  /**
+   * @return the lastChangeTimeStamp
+   */
+  public Long getLastChangeTimeStamp() {
+    return lastChangeTimeStamp;
+  }
+
+  /**
+   * @param lastChangeTimeStamp the lastChangeTimeStamp to set
+   */
+  public void setLastChangeTimeStamp(Long lastChangeTimeStamp) {
+    this.lastChangeTimeStamp = lastChangeTimeStamp;
+  }
+
+  /**
+   * @return the optlock
+   */
+  public Long getOptlock() {
+    return optlock;
+  }
+
+  /**
+   * @param optlock the optlock to set
+   */
+  public void setOptlock(Long optlock) {
+    this.optlock = optlock;
+  }
+
+  /**
+   * @return the description
+   */
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+   * @param description the description to set
+   */
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  /**
+   * @return the imageData
+   */
+  public byte[] getImageData() {
+    return imageData;
+  }
+
+  /**
+   * @param imageData the imageData to set
+   */
+  public void setImageData(byte[] imageData) {
+    this.imageData = imageData;
+  }
+
+  /**
+   * @return the descriptionbytes
+   */
+  public byte[] getDescriptionbytes() {
+    return descriptionbytes;
+  }
+
+  /**
+   * @param descriptionbytes the descriptionbytes to set
+   */
+  public void setDescriptionbytes(byte[] descriptionbytes) {
+    this.descriptionbytes = descriptionbytes;
+  }
+
+}
