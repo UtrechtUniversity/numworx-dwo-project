7f25229ef555914020940cb18d6eb9dd45318f39
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-01-12 13:34:01 +0000

    Void statement test
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-jclient-lib/trunk@25918 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/test/java/nl/uu/fi/dwo/lms/jclient/rest/managers/XapiManagerIT.java
+++ src/test/java/nl/uu/fi/dwo/lms/jclient/rest/managers/XapiManagerIT.java
@@ -2,10 +2,9 @@ package nl.uu.fi.dwo.lms.jclient.rest.managers;
 
 import static org.junit.Assert.*;
 
-import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
 
 import javax.swing.text.DateFormatter;
@@ -92,4 +91,37 @@ public class XapiManagerIT {
     assertEquals(state.content, r.getValue().content);
   }
 
+  @Test
+  public void testVoiding() throws Exception  {
+	    Date now = new Date();
+	    Statement out,in;
+	    out = new Statement();
+	    out.actor = null;
+	    out.verb = new Verb();
+	    out.verb.id = VERB;
+	    out.object = new Activity();
+	    out.object.id = "http://localhost:8080/activities/test";
+	    
+	    String id = manager.saveStatement(out).getValue();
+  
+	    out = new Statement();
+	    out.verb = new Verb();
+	    out.verb.id = "http://adlnet.gov/expapi/verbs/voided";
+	    out.verb.display = Collections.singletonMap("en-US", "voided");
+	    out.object = new Activity();
+	    out.object.id = id;
+	    out.object.objectType = Activity.STATEMENT_REF;
+	    Promise<?> p = manager.saveStatement(out);
+	    Promise<StatementsResult> q = p.then( s -> {  
+	        StatementsQuery query = new StatementsQuery();
+	        query.limit = 1;
+	        query.since = format.valueToString(now);
+	        Promise<StatementsResult> r = manager.queryStatements(query);
+	        return r;
+	    });
+	    System.out.println(q.getValue().statements);		
+	  
+  }
+  
+  
 }
