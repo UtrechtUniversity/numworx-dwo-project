b2e7354115149c8adae683d654d6fb7d39d4969f
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-01-12 13:33:21 +0000

    ObjectType niet final, wel default Activity
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@25917 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/rest/dom/xapi/Activity.java
+++ src/main/java/nl/uu/fi/dwo/rest/dom/xapi/Activity.java
@@ -1,7 +1,10 @@
 package nl.uu.fi.dwo.rest.dom.xapi;
 
 public class Activity {
+  public final static String ACTIVITY = "Activity";
+  public final static String STATEMENT_REF = "StatementRef";
+
   public ActivityDefinition definition;
   public String id;
-  public final String objectType = "Activity"; 
+  public String objectType = ACTIVITY; 
 }
