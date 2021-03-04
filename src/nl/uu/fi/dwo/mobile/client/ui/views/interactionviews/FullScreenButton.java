18684b80b8461b8e5aa536ed00ed8f27bd45a90a
boon0104 <boon0104@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-03-04 13:16:53 +0000

    Button voor fullscreen en weer terug: new FullScreenButton("open") en new FullScreenButton("close")
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/trunk@26176 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/nl/uu/fi/dwo/mobile/client/ui/views/interactionviews/FullScreenButton.java
@@ -0,0 +1,121 @@
+package nl.uu.fi.dwo.mobile.client.ui.views.interactionviews;
+
+import java.awt.BasicStroke;
+
+import org.vectomatic.dom.svg.OMSVGCircleElement;
+import org.vectomatic.dom.svg.OMSVGLength;
+import org.vectomatic.dom.svg.OMSVGLineElement;
+import org.vectomatic.dom.svg.OMSVGPathElement;
+import org.vectomatic.dom.svg.OMSVGPathSegList;
+import org.vectomatic.dom.svg.OMSVGRectElement;
+import org.vectomatic.dom.svg.OMSVGTextElement;
+import org.vectomatic.dom.svg.ui.SVGResource;
+import org.vectomatic.dom.svg.utils.SVGConstants;
+
+import com.google.gwt.canvas.dom.client.CssColor;
+
+import fi.wiskopdr.WiskOpdr;
+import nl.uu.fi.dwo.mobile.DWOplayer;
+import nl.uu.fi.dwo.mobile.client.ui.SVGButton;
+
+public class FullScreenButton extends SVGButton{
+
+	private String code = "";
+	
+	public FullScreenButton(String code) {
+		super(code);
+		this.code = code;
+	}
+	
+	public FullScreenButton(SVGResource resource) {
+		super(resource);
+	}
+	
+	protected void setBorderActive(boolean b) {
+		super.setBorderActive(b);
+	}
+
+	public void draw() {
+		float w = width;
+		float h = height;
+		float e = w / 24;
+		float d = 10;
+		float m = 4;
+		float x = w/2;
+		float y = w/2;
+		float r = 4; 
+		float p = 5;
+		OMSVGRectElement rect = doc.createSVGRectElement(e, e, width - 2 * e, height - 2 * e, 1 * e, 1 * e);
+		rect.getStyle().setSVGProperty(SVGConstants.CSS_FILL_PROPERTY, bgColor.toString());
+		rect.getStyle().setSVGProperty(SVGConstants.CSS_STROKE_PROPERTY, borderColor.toString());
+		rect.getStyle().setSVGProperty(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, "" + e);
+		
+
+		if ("open".equals(code)) {
+			OMSVGPathElement open = doc.createSVGPathElement();
+			OMSVGPathSegList segsPijlDown = open.getPathSegList();
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x+r, y+r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+d, y+d));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x+d-p, y+d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+d, y+d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+d, y+d-p));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x-r, y-r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-d, y-d));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x-d+p, y-d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-d, y-d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-d, y-d+p));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x+r, y-r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+d, y-d));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x+d-p, y-d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+d, y-d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+d, y-d+p));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x-r, y+r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-d, y+d));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x-d+p, y+d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-d, y+d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-d, y+d-p));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x,y));
+			
+			
+			segsPijlDown.appendItem(open.createSVGPathSegClosePath());
+			open.getStyle().setSVGProperty(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, "" + 2.0*e);
+			open.getStyle().setSVGProperty(SVGConstants.CSS_STROKE_PROPERTY, foregroundColor.toString());
+			open.getStyle().setSVGProperty(SVGConstants.CSS_FILL_PROPERTY, "transparent");
+			svg.appendChild(open);
+		}
+		else if ("close".equals(code)) {
+			OMSVGPathElement open = doc.createSVGPathElement();
+			OMSVGPathSegList segsPijlDown = open.getPathSegList();
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x+d, y+d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+r, y+r));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x+r+p, y+r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+r, y+r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+r, y+r+p));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x-d, y-d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-r, y-r));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x-r-p, y-r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-r, y-r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-r, y-r-p));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x+d, y-d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+r, y-r));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x+r+p, y-r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+r, y-r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x+r, y-r-p));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x-d, y+d));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-r, y+r));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x-r-p, y+r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-r, y+r));
+			segsPijlDown.appendItem(open.createSVGPathSegLinetoAbs(x-r, y+r+p));
+			segsPijlDown.appendItem(open.createSVGPathSegMovetoAbs(x,y));
+			
+			
+			segsPijlDown.appendItem(open.createSVGPathSegClosePath());
+			open.getStyle().setSVGProperty(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, "" + 2.0*e);
+			open.getStyle().setSVGProperty(SVGConstants.CSS_STROKE_PROPERTY, foregroundColor.toString());
+			open.getStyle().setSVGProperty(SVGConstants.CSS_FILL_PROPERTY, "transparent");
+			svg.appendChild(open);
+			
+		}
+	}
+		
+}
