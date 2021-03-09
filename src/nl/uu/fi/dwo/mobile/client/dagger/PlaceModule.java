49d5e8b85caf30c81614a0904daf5e663658cb17
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-03-09 09:31:20 +0000

    defaultPlace is nu aanpasbaar
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/trunk@26186 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/mobile/client/dagger/PlaceModule.java
+++ src/nl/uu/fi/dwo/mobile/client/dagger/PlaceModule.java
@@ -3,14 +3,27 @@ package nl.uu.fi.dwo.mobile.client.dagger;
 import javax.inject.Named;
 
 import com.google.gwt.place.shared.Place;
+import com.google.gwt.place.shared.PlaceHistoryMapper;
 
 import dagger.Module;
 import dagger.Provides;
 import nl.uu.fi.dwo.mobile.client.ui.places.LoginPlace;
 
 @Module abstract class PlaceModule {
+	
+	static native private String getDefaultPlace() /*-{
+		return $wnd.defaultPlace
+	}-*/;
+	
 
-  @Provides @Named("defaultPlace") static Place defaultPlace() {
+  @Provides @Named("defaultPlace") static Place defaultPlace(PlaceHistoryMapper mapper) {
+	  try { 
+		  String token = getDefaultPlace();
+		  if (!token.isEmpty()) {
+			  Place p = mapper.getPlace(token);
+			  if (p != null) return p;
+		  }
+	  } catch(Exception oops) { }
     return new LoginPlace();
   }
 
