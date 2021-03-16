759f78469ef09b65da2a0f13fe55f3d83de5ba11
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-03-16 09:37:44 +0000

    https://numworx.atlassian.net/browse/LMS-498
    met een eenvoudig choice panel
    
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOJClient/trunk@26210 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/dwojapplet/gui/domainmodel/SettingsSchoolClassPanel.java
+++ src/main/java/fi/dwo/dwojapplet/gui/domainmodel/SettingsSchoolClassPanel.java
@@ -1,41 +1,57 @@
 package fi.dwo.dwojapplet.gui.domainmodel;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Collections;
+import java.util.EventObject;
 import java.util.List;
 
+import javax.swing.AbstractCellEditor;
+import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
+import javax.swing.event.CellEditorListener;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 
 import fi.beans.numworxlf.JButton;
+import fi.beans.numworxlf.JOptionPane;
 import fi.beans.numworxlf.JScrollPane;
+import fi.dwo.dwojapplet.gui.ConfirmDialog;
 import fi.dwo.dwojapplet.gui.TableUtil;
 import nl.uu.fi.dwo.lms.jclient.lib.rest.managers.SecureTeacherStudentModelManager;
 import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClass;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContext;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContextId;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelStructure;
 import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
 import nl.uu.fi.dwo.rest.persistence.PersistenceId;
 
 public class SettingsSchoolClassPanel extends JPanel {
   
+  private final String FILTER_LEERDOELEN = "Filter leerdoelen";
+  private final JButton filter = new JButton(FILTER_LEERDOELEN);
+
   class ClassTableModel extends AbstractTableModel {
     
     List<DomSchoolClass> classes;
     boolean[] check;
-    JButton filter;
     DomStudentModelContext[] list;
-    private DomStudentModelContextId id;
+    private DomStudentModelContext id;
+    private List<String>[] objectives;
     
     
-    public ClassTableModel(List<DomSchoolClass> classes, DomStudentModelContextId id) {
-      filter = new JButton("Filter leerdoelen");
+    public ClassTableModel(List<DomSchoolClass> classes, DomStudentModelContext id) {
+      
       check = new boolean[classes.size()];
+      objectives = new List[classes.size()];
       this.classes = classes;
       this.id = id;
       try {
@@ -82,7 +98,7 @@ public class SettingsSchoolClassPanel extends JPanel {
 
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
-       return columnIndex == 1 || columnIndex == 2;
+       return columnIndex == 1 || (columnIndex == 2 && check[rowIndex]);
     }
 
     @Override
@@ -91,6 +107,11 @@ public class SettingsSchoolClassPanel extends JPanel {
         case 1: check[rowIndex] = Boolean.TRUE.equals(aValue); 
             fireTableCellUpdated(rowIndex, columnIndex+1);
             fireTableCellUpdated(rowIndex, columnIndex);
+            if (!check[rowIndex]) {
+              objectives[rowIndex] = null;
+            } else if (objectives[rowIndex] == null) {
+              objectives[rowIndex] = Collections.emptyList();
+            }
             return;
         case 4: list[rowIndex] = (DomStudentModelContext) aValue; return;
         default: super.setValueAt(aValue, rowIndex, columnIndex);
@@ -113,9 +134,11 @@ public class SettingsSchoolClassPanel extends JPanel {
 
 
       public ButtonRenderer() {
-      super();
+        super("");
         setOpaque(true);
-    }
+        setSize(filter.getPreferredSize());
+        setPreferredSize(getSize());
+      }
 
       @Override
       public Component getTableCellRendererComponent(JTable table,
@@ -135,16 +158,110 @@ public class SettingsSchoolClassPanel extends JPanel {
     }
 
 }
+  class ButtonEditor extends AbstractCellEditor implements TableCellEditor, Action {
 
+    public ButtonEditor(ClassTableModel model) {
+      this.model = model;
+    }
+
+    int row;
+    ClassTableModel model;
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected,
+        int row, int column) {
+      JButton btn = (JButton) value;
+      this.row = row;
+      btn.setAction(this);      
+      return btn;
+    }
+
+
+    @Override
+    public Object getCellEditorValue() {
+      return filter;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      DomSchoolClass sc = model.classes.get(row);
+      
+      ConfirmDialog cd = new ConfirmDialog(SettingsSchoolClassPanel.this, "Leerdoelen voor " + sc.getSchoolClassName());
+      DomStudentModelStructure str = model.id.getModelStructure();
+      if (str.getCategories() == null) {
+        try {
+          model.id = SecureTeacherStudentModelManager.get(model.id);
+          str = model.id.getModelStructure();
+        } catch (Dwo2Exception e1) {
+        }
+      }
+      NodeVector vector = new NodeVector(str.getCategories(), str.getInfo(), getLocale().getLanguage());
+      StudentModelChoicePanel panel = new StudentModelChoicePanel(vector);
+      List<String> objectives = model.objectives[row];
+      if(objectives == null) objectives = Collections.emptyList();
+      panel.setObjectives(objectives);
+      cd.getContentPane().add(panel);
+      JButton ok = new JButton("OK");
+      ok.addActionListener(cd::ok);
+      cd.getContentPane().add(ok, BorderLayout.SOUTH);
+      cd.pack();
+      cd.center();
+      cd.setVisible(true);
+      if (cd.getOption() == JOptionPane.OK_OPTION) {
+        panel.makeChoices();
+        model.objectives[row] = panel.getObjectives();        
+      }
+    }
+
+
+    @Override
+    public Object getValue(String key) {
+      if (key == Action.NAME) return FILTER_LEERDOELEN;
+      return null;
+    }
+
+
+    @Override
+    public void putValue(String key, Object value) {
+    }
+
+
+    @Override
+    public void setEnabled(boolean b) {
+    }
+
+
+    @Override
+    public boolean isEnabled() {
+      return true;
+    }
+
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+    }
+
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+    } 
+    
+  }
+  
+  
+  
 
   public SettingsSchoolClassPanel(List<DomSchoolClass> classes, DomStudentModelContext context) {
     super();
     
-    JTable table = new JTable(new ClassTableModel(classes, context));
+    ClassTableModel model = new ClassTableModel(classes, context);
+    JTable table = new JTable(model);
     JScrollPane pane = new JScrollPane(table);
     TableUtil.setDefaults(table, true, null, null);
     TableCellRenderer renderer = new ButtonRenderer();
     table.setDefaultRenderer(JButton.class, renderer);
+    table.setDefaultEditor(JButton.class, new ButtonEditor(model));
     TableUtil.setBorder(pane);
     TableUtil.setJTableSizes(table);
     add(pane);    
