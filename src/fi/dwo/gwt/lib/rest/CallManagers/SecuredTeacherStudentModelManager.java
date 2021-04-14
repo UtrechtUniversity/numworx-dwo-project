884648cfb729dc11b91ec4556020eef5a09c300b
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-04-14 14:31:25 +0000

    https://numworx.atlassian.net/browse/LMS-501
    functies voor ophalen studentmodel als teacher
    
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-gwt-lib/trunk@26345 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/gwt/lib/rest/CallManagers/SecuredTeacherStudentModelManager.java
+++ src/fi/dwo/gwt/lib/rest/CallManagers/SecuredTeacherStudentModelManager.java
@@ -2,6 +2,8 @@ package fi.dwo.gwt.lib.rest.CallManagers;
 
 import static fi.dwo.gwt.lib.rest.GwtRestVars.F;
 
+import java.util.List;
+
 import org.osgi.util.promise.Promise;
 
 import com.google.gwt.core.shared.GWT;
@@ -9,6 +11,7 @@ import com.google.gwt.core.shared.GWT;
 import fi.dwo.gwt.lib.rest.client.RestCallers.SecuredTeacherStudentModelRestCaller;
 import nl.uu.fi.dwo.rest.dom.entities.DomContext;
 import nl.uu.fi.dwo.rest.dom.entities.DomLRS;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContext;
 import nl.uu.fi.dwo.rest.entities.RestContext;
 import nl.uu.fi.dwo.rest.util.PathId;
 
@@ -20,5 +23,11 @@ public class SecuredTeacherStudentModelManager {
     RestContext rest = new RestContext();
     rest.setRestContext(context);
     return F(service::getLRS,PathId.getId(context), rest);
+  }
+  
+  public Promise<List<DomStudentModelContext>> getReducedList(DomContext context) {
+	RestContext rest = new RestContext();
+	rest.setRestContext(context);
+	return F(service::getReducedList,PathId.getId(context), rest);
   }
 }
--- src/fi/dwo/gwt/lib/rest/client/RestCallers/SecuredTeacherStudentModelRestCaller.java
+++ src/fi/dwo/gwt/lib/rest/client/RestCallers/SecuredTeacherStudentModelRestCaller.java
@@ -1,5 +1,7 @@
 package fi.dwo.gwt.lib.rest.client.RestCallers;
 
+import java.util.List;
+
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -8,6 +10,7 @@ import org.fusesource.restygwt.client.MethodCallback;
 import org.fusesource.restygwt.client.RestService;
 
 import nl.uu.fi.dwo.rest.dom.entities.DomLRS;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContext;
 import nl.uu.fi.dwo.rest.entities.RestContext;
 
 public interface SecuredTeacherStudentModelRestCaller extends RestService {
@@ -15,4 +18,8 @@ public interface SecuredTeacherStudentModelRestCaller extends RestService {
   @Path("/sec:{id}/teacher/studentmodel/getLRS")
   @PUT
   void getLRS(@PathParam("id") String id, RestContext rest, MethodCallback<DomLRS> callback);
+  
+  @Path("/sec:{id}/teacher/studentmodel/getReducedList")
+  @PUT
+  void getReducedList(@PathParam("id") String id, RestContext rest, MethodCallback<List<DomStudentModelContext>> callback);
 }
