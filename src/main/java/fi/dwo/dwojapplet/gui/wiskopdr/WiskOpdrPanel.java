317c9b57ae548626af6bdc181bccc9f99c6df6dd
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-04-22 11:11:04 +0000

    https://numworx.atlassian.net/browse/LMS-506
    getWiskOpdrPanel met appletstub
    fallback naar zonder
    
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOJClient/trunk@26366 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/dwojapplet/gui/wiskopdr/WiskOpdrPanel.java
+++ src/main/java/fi/dwo/dwojapplet/gui/wiskopdr/WiskOpdrPanel.java
@@ -1,11 +1,14 @@
 package fi.dwo.dwojapplet.gui.wiskopdr;
 
+import java.applet.AppletContext;
+import java.applet.AppletStub;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.net.URL;
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,7 +17,17 @@ import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class WiskOpdrPanel extends JPanel implements InvocationHandler {
+import com.owlike.genson.GenericType;
+import com.owlike.genson.Genson;
+
+import fi.dwo.dwojapplet.domain.DWO;
+import fi.dwo.dwojapplet.domain.DwoHelper;
+import fi.dwo.dwojapplet.gui.GuiCreator;
+import nl.uu.fi.dwo.lms.jclient.lib.rest.transport.StoredRestManager;
+import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfile;
+import nl.uu.fi.dwo.rest.dom.entities.util.AboType;
+
+public class WiskOpdrPanel extends JPanel implements InvocationHandler, AppletStub {
 
 	private static final long serialVersionUID = 1L;
 	private final Logger LOG = Logger.getLogger(getClass().getName());
@@ -29,8 +42,13 @@ public class WiskOpdrPanel extends JPanel implements InvocationHandler {
 		
 		try {
 			Class<?> wiskopdr = WiskOpdrCache.getInstance();
-			Method m = wiskopdr.getMethod("getWiskOpdrPanel", String.class, Locale.class);
-			component = (JComponent) m.invoke(null, s, locale);
+			try {
+              Method m = wiskopdr.getMethod("getWiskOpdrPanel", String.class, Locale.class, AppletStub.class);
+              component = (JComponent) m.invoke(null, s, locale, this);
+            } catch (Exception e) {
+                Method m = wiskopdr.getMethod("getWiskOpdrPanel", String.class, Locale.class);
+              component = (JComponent) m.invoke(null, s, locale);
+            }
 			component.setBackground(getBackground());
 			add(component, BorderLayout.CENTER);
 			setSize(component.getSize());
@@ -92,5 +110,56 @@ public class WiskOpdrPanel extends JPanel implements InvocationHandler {
     } catch (Exception e) {
       LOG.log(Level.WARNING, "WiskOpdr.end()", e);
     }
+  }
+
+  @Override
+  public boolean isActive() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public URL getDocumentBase() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public URL getCodeBase() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public String getParameter(String name) {
+    if ("abo_type".equals(name)) {
+      try {
+        String abo_type = DwoHelper.getSchoolLogins().getActiveSchoolRoleAndClass().getSchool().getAboType().name();
+        return abo_type;
+      } catch (Exception e) {
+        return AboType.standard.name();
+      }
+    }
+    if ("dwoProfile".equals(name)) {
+      Genson g = StoredRestManager.getInstance().getGenson();
+      DomDwoProfile profile = DWO.getDwoProfile();
+      return g.serialize(profile, new GenericType<DomDwoProfile>() {});
+    }
+    if ("dwo_env".equals(name)) {
+      return GuiCreator.instance().getDWO().getParameter(name);
+    }
+    return null;
+  }
+
+  @Override
+  public AppletContext getAppletContext() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void appletResize(int width, int height) {
+    // TODO Auto-generated method stub
+    
   }
 }
