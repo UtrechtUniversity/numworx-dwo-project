6912ce7db7c3c7962745305d4ddfac4fb937c862
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-06-23 13:42:32 +0000

    DomMethod voor students
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOServer/trunk@26506 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/fi/dwo/server/rest/SecuredStudentMethodManager.java
@@ -0,0 +1,35 @@
+package fi.dwo.server.rest;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+
+import fi.dwo.commons.persistence.entities.PersistentMethod;
+import fi.dwo.commons.persistence.entities.PersistentSchool;
+import fi.dwo.server.PersistentDataManagers.access.AnonDomainAuthorizer;
+import fi.dwo.server.PersistentDataManagers.access.UserDomainAuthorizer.UserState_HR_R_S_SG_U;
+import fi.dwo.server.PersistentDataManagers.core.MethodManager;
+import nl.uu.fi.dwo.rest.dom.entities.DomMethod;
+import nl.uu.fi.dwo.rest.entities.RestMethod;
+import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
+
+@RolesAllowed({"STUDENT"})
+@Path("/secure/teacher/method")
+public class SecuredStudentMethodManager {
+
+	@PUT
+    @Produces({"application/json"})
+    @Path("/get")
+    public DomMethod get(@Context SecurityContext sc, RestMethod rest) throws Dwo2Exception {
+    	UserState_HR_R_S_SG_U hasRole = AnonDomainAuthorizer.build().submitUser(sc.getUserPrincipal().getName()).setHasRole(rest.getRestContext().getDomHasRole());
+    	PersistentSchool school = hasRole.getSchool();
+    	hasRole.buildStudent();
+    	PersistentMethod p = MethodManager.toValue(rest.getDomMethod(), school);
+    	p = MethodManager.findEntity(p.getId());
+    	return MethodManager.toDom(p);
+    }
+
+}
