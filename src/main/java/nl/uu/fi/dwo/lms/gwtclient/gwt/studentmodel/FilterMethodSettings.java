963bbb263fdc5a905e01d7583feb339fcdca90b4
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-06-24 07:47:08 +0000

    functionaliteit en layout filter settings via DomMethod
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOGwtClient/trunk@26514 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/studentmodel/FilterMethodSettings.java
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/studentmodel/FilterMethodSettings.java
@@ -1,9 +1,14 @@
 package nl.uu.fi.dwo.lms.gwtclient.gwt.studentmodel;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.editor.client.LeafValueEditor;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
@@ -17,6 +22,23 @@ import nl.uu.fi.dwo.rest.dom.entities.DomMethod;
 
 public class FilterMethodSettings extends Composite implements LeafValueEditor<Map<String, Map<String, Set<Integer>>>> {
 
+	static class P extends Label {
+		private P(String text) {
+			super(Document.get().createPElement());
+			setStylePrimaryName("filter");
+			setText(text);
+		}
+	}
+	static class L extends Label {
+		private L(String text) {
+			super(Document.get().createLabelElement());
+			setStylePrimaryName("filter");
+			getElement().getStyle().setFontSize(12, Unit.PX);
+			setText(text);
+		}
+	}
+	
+	
 	private Map<String, Map<String, Set<Integer>>> value;
 	private final String key;
 	private final String[] books;
@@ -58,17 +80,19 @@ public class FilterMethodSettings extends Composite implements LeafValueEditor<M
 		key = method.key();
 		books = method.books.toArray(new String[method.books.size()]);
 		FlowPanel flow = new FlowPanel();
-		Label title = new Label(method.getMethod()); flow.add(title);
+		Label title = new P(method.getMethod()); flow.add(title);
 		int columns = 0;
 		for (List<?> list : method.chapters) {
 			columns = Math.max(columns, list.size());
 		}		
 		int rows = method.books.size();
 		Grid  grid = new Grid(rows+1, columns+1); flow.add(grid);
+		grid.getElement().getStyle().setMarginBottom(18, Unit.PX);
 		gr = new CheckBox();
 		grid.setWidget(0, 0, gr);
 		for(int i = 1; i <= columns; i++) {
-			grid.setText(0, i, "hfst " + i);
+			grid.setWidget(0, i, new L("hfst " + i));
+			grid.getColumnFormatter().getElement(i).getStyle().setWidth(40, Unit.PX);
 		}
 		gri = new CheckBox[rows][];
 		
@@ -87,23 +111,64 @@ public class FilterMethodSettings extends Composite implements LeafValueEditor<M
 			grrow.addValueChangeHandler(new GRiHandler(gri[row-1]));
 		}
 		gr.addValueChangeHandler(new GRHandler());
-		Label alle = new Label("Alle leerdoelen");flow.add(alle); flow.add(alle);
+		Label alle = new P("Alle leerdoelen");flow.add(alle); flow.add(alle);
 		grid = new Grid(1,1); flow.add(grid);
+		grid.getElement().getStyle().setMarginBottom(18, Unit.PX);
 		rest = new CheckBox("Niet geclassificeerde leerdoelen");
 		grid.setWidget(0, 0, rest);
 
 		initWidget(flow);
 	}
 
+	private void setValue(Map<String, Set<Integer>> GenR, String hfst, CheckBox[] g) {
+		Set<Integer> h1 = GenR.get(hfst);
+		if (h1 == null) return;
+		if (h1.isEmpty()) {
+			setValue(g,Boolean.TRUE);
+			return;
+		}
+		h1.forEach(item -> g[item.intValue()].setValue(Boolean.TRUE));
+		g[0].setValue(h1.size() == g.length-1);
+	}
+
 	@Override
 	public void setValue(Map<String, Map<String, Set<Integer>>> value) {
 		this.value = value;
-		
+		rest.setValue(value.containsKey("")||value.containsKey(null));
+		Map<String, Set<Integer>> GenR = value.getOrDefault(key, Collections.emptyMap());
+		gr.setValue(Boolean.FALSE, true);
+		boolean sum = true;
+		for(int i = 0; i < books.length; i++ ) 
+		{
+			setValue(GenR, books[i], gri[i]);
+			sum = sum & gri[i][0].isChecked();
+		}
+		gr.setValue(sum);
+	}
+
+	private Set<Integer> getValue(CheckBox[] g) {
+		Set<Integer> set = new TreeSet<>();
+		for (int i = 1; i < g.length; i++) {
+			if (g[i].isChecked()) set.add(i);
+		}
+		return set;
 	}
 
 	@Override
 	public Map<String, Map<String, Set<Integer>>> getValue() {
-		return value;
+		Map<String, Map<String,Set<Integer>>> result = new HashMap<>();
+		Map<String, Set<Integer>> GenR = new HashMap<>();
+		Set<Integer> h;
+		for (int i = 0; i < books.length; i++ ) {
+			CheckBox[] gr1s = gri[i];
+			h = getValue(gr1s);if (!h.isEmpty()) {
+				if (h.size() == gr1s.length-1) h = Collections.emptySet();
+				GenR.put(books[i], h);
+			}		
+		}
+		if (!GenR.isEmpty()) result.put(key, GenR);
+		if (rest.isChecked()) result.put("", Collections.emptyMap());		
+		return result;
 	}
 
 }
