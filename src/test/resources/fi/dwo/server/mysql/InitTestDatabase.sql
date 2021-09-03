64ac438636254639d844d89ea10dbaa1b43d3a09
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-09-03 11:51:41 +0000

    firstname/middlename/lastname full utf8
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOServer/trunk@26681 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/test/resources/fi/dwo/server/mysql/InitTestDatabase.sql
+++ src/test/resources/fi/dwo/server/mysql/InitTestDatabase.sql
@@ -929,7 +929,7 @@ CREATE TABLE `tbluser` (
   KEY `USER_GROUP_FK` (`schoolGroupID`),
   KEY `STUDENT_CLASS_FK` (`classID`),
   CONSTRAINT `tbluser_ibfk_1` FOREIGN KEY (`schoolGroupID`) REFERENCES `tblschoolgroup` (`schoolGroupID`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
