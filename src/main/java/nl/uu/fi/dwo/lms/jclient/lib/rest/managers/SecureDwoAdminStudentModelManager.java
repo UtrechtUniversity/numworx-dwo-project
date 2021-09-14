48aa06c0e6a774e6fbdd37876bd0745c1bb2e3d0
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-09-14 11:23:42 +0000

    fetch dwoadmin studentmodels
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-jclient-lib/trunk@26709 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/lms/jclient/lib/rest/managers/SecureDwoAdminStudentModelManager.java
+++ src/main/java/nl/uu/fi/dwo/lms/jclient/lib/rest/managers/SecureDwoAdminStudentModelManager.java
@@ -1,17 +1,37 @@
 package nl.uu.fi.dwo.lms.jclient.lib.rest.managers;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import nl.uu.fi.dwo.lms.jclient.lib.rest.transport.StoredRestManager;
+import nl.uu.fi.dwo.rest.RestListClassTypes;
+import nl.uu.fi.dwo.rest.dom.entities.DomContext;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContext;
+import nl.uu.fi.dwo.rest.entities.RestContext;
 import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
+import nl.uu.fi.dwo.rest.util.PathId;
 
 public class SecureDwoAdminStudentModelManager implements SecureStudentModelManager {
+	private static final Logger LOG =
+		      Logger.getLogger(SecureDwoAdminStudentModelManager.class.getName());
+
+	private final StoredRestManager restManager = StoredRestManager.getInstance();
 
 	@Override
 	public List<DomStudentModelContext> getReducedList() throws Dwo2Exception {
-		// TODO Auto-generated method stub
-		return Collections.EMPTY_LIST;
+	    RestContext rest = new RestContext();
+	    rest.setRestContext(getContext());
+	    List<DomStudentModelContext> src =
+	        restManager.getPutList("rest/sec:" + PathId.getId(getContext()) + "/dwoadmin/studentmodel/getReducedList",
+	            RestListClassTypes.DomStudentModelContext, rest);
+	    LOG.log(Level.FINE, "Retrieved list of studentmodels of the dwoadmin with username {0}.",
+	        new Object[] {restManager.getAuthenticator().getUsername()});
+	    return src;
+	}
+
+	private DomContext getContext() {
+		    return restManager.getAuthenticator().getContext();
 	}
 
 }
