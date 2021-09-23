993b7ce1cdcde4f786b748a58ea874297c0c9116
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2021-09-23 11:53:34 +0000

    SchoolAdmin als gebruiker van leerdoelen bij activiteiten editen
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-jclient-lib/trunk@26755 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/lms/jclient/lib/rest/managers/SecureSchoolAdminStudentModelManager.java
@@ -0,0 +1,80 @@
+package nl.uu.fi.dwo.lms.jclient.lib.rest.managers;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import nl.uu.fi.dwo.lms.jclient.lib.rest.transport.RestAuthenticator;
+import nl.uu.fi.dwo.lms.jclient.lib.rest.transport.StoredRestManager;
+import nl.uu.fi.dwo.rest.RestListClassTypes;
+import nl.uu.fi.dwo.rest.dom.entities.DomContext;
+import nl.uu.fi.dwo.rest.dom.entities.DomSchoolMethod;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContext;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContextId;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContextPatch;
+import nl.uu.fi.dwo.rest.entities.RestContext;
+import nl.uu.fi.dwo.rest.entities.RestStudentModelContext;
+import nl.uu.fi.dwo.rest.entities.RestStudentModelContextId;
+import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
+import nl.uu.fi.dwo.rest.util.PathId;
+
+public class SecureSchoolAdminStudentModelManager implements SecureStudentModelManager {
+
+	private static final Logger LOG =
+		      Logger.getLogger(SecureSchoolAdminStudentModelManager.class.getName());
+
+	private DomContext getContext() {
+		    return StoredRestManager.getInstance().getAuthenticator().getContext();
+	}
+	
+	@Override
+	public List<DomStudentModelContext> getReducedList() throws Dwo2Exception {
+	    RestContext rest = new RestContext();
+	    rest.setRestContext(getContext());
+	    List<DomStudentModelContext> src =
+	        StoredRestManager.getInstance().getPutList("rest/sec:" + PathId.getId(getContext()) + "/schooladmin/studentmodel/getReducedList",
+	            RestListClassTypes.DomStudentModelContext, rest);
+	    LOG.log(Level.FINE, "Retrieved list of studentmodels of the teacher with username {0}.",
+	        new Object[] {RestAuthenticator.getInstance().getUsername()});
+	    return src;
+	}
+
+	@Override
+	public DomStudentModelContext updateModel(DomStudentModelContext submit) throws Dwo2Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DomStudentModelContext patchModel(DomStudentModelContextPatch submit) throws Dwo2Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DomStudentModelContext get(DomStudentModelContextId modelContext) throws Dwo2Exception {
+		 RestStudentModelContext rest = new RestStudentModelContext();
+		 rest.setRestContext(getContext());
+		 DomStudentModelContext context = new DomStudentModelContext();
+		 context.setId(modelContext.getId());
+		 rest.setDomStudentModelContext(context);
+		 DomStudentModelContext src =
+				        StoredRestManager.getInstance().put("rest/sec:" + PathId.getId(getContext()) + "/schooladmin/studentmodel/get",
+				            DomStudentModelContext.class, rest);
+		 LOG.log(Level.FINE, "Retrieved studentmodel of the teacher with username {0}.",
+			new Object[] {RestAuthenticator.getInstance().getUsername()});
+	     return src;
+	}
+	
+	@Override
+	public DomSchoolMethod getActiveMethod(DomStudentModelContextId id) throws Dwo2Exception {
+	  RestStudentModelContextId rest = new RestStudentModelContextId();
+	  rest.setRestContext(getContext());
+	  rest.setDomStudentModelContext(id);
+	  rest.setDomSchoolClass(null);
+	  DomSchoolMethod result = StoredRestManager.getInstance()
+			  .put("rest/sec:" + PathId.getId(getContext()) + "/schooladmin/studentmodel/getMethod", DomSchoolMethod.class, rest);
+	  return result;
+	}
+
+}
