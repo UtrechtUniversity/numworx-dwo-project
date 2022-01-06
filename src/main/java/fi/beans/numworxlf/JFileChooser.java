7908ab8b8425a848e3fff5b71efdc2ab168c012f
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2022-01-06 08:53:19 +0000

    https://numworx.atlassian.net/browse/DWOWISK-135
    implementatie JFileChooser met globale directory
    
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/NumworxLF/trunk@27201 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/beans/numworxlf/JFileChooser.java
+++ src/main/java/fi/beans/numworxlf/JFileChooser.java
@@ -3,9 +3,15 @@
  */
 package fi.beans.numworxlf;
 
+import java.awt.Component;
+import java.awt.HeadlessException;
 import java.io.File;
 
 import javax.swing.filechooser.FileSystemView;
+import javax.swing.plaf.FileChooserUI;
+
+
+
 
 /**
  * @author wim
@@ -14,10 +20,22 @@ import javax.swing.filechooser.FileSystemView;
 @SuppressWarnings("serial")
 public class JFileChooser extends javax.swing.JFileChooser {
 
+    private static final File USER_HOME = FileSystemView.getFileSystemView().getHomeDirectory();
+	private static File currentDirectory = USER_HOME;
+    public static File getGlobalDirectory() {
+      return currentDirectory;
+    }
+
+    public static void setGlobalDirectory(File dir) {
+      if (dir == null) return;
+      if (!dir.isDirectory()) dir = dir.getParentFile();
+      currentDirectory = dir;
+    }
   /**
    * 
    */
   public JFileChooser() {
+	  super();
   }
 
   /**
@@ -56,5 +74,28 @@ public class JFileChooser extends javax.swing.JFileChooser {
   public JFileChooser(String currentDirectoryPath, FileSystemView fsv) {
     super(currentDirectoryPath, fsv);
   }
+
+	@Override
+	public void setCurrentDirectory(File dir) {	
+		super.setCurrentDirectory(dir);
+		if (dir == null) dir = USER_HOME; // komt voor
+		setGlobalDirectory(dir);
+	}
+	
+	
+	@Override
+	public int showDialog(Component parent, String approveButtonText) throws HeadlessException {
+		super.setCurrentDirectory(getGlobalDirectory());
+		int result = super.showDialog(parent, approveButtonText);
+		if (result == APPROVE_OPTION)
+			setGlobalDirectory(getCurrentDirectory());
+		return result;
+	}
+	
+	@Override
+	public void updateUI() {
+		// TODO Implement Numworx Look and Feel
+		super.updateUI();
+	}
 
 }
