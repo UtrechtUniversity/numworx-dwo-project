17bed04758df26594b54a120ab8d8ef05464e0ef
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2022-03-14 15:48:08 +0000

    / is toegestaan in querystring
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/EBServer/trunk@27439 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/db/Util.java
+++ src/main/java/fi/dwo/server/db/Util.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Util {
 	public static boolean illegal(String q) {
 		if (q == null) return true;
-		if (! Pattern.matches("[%#:a-zA-Z0-9=&:.]*", q))
+		if (! Pattern.matches("[%#:a-zA-Z0-9/=&:.]*", q))
 			return true;
 		return false;
 	}
