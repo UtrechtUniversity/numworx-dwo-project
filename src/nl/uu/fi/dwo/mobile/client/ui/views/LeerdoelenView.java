a2154bb7d55c2037d86d11d2fafa296ef1164f78
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2022-05-02 08:22:44 +0000

    no OK
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/trunk@27615 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/mobile/client/ui/views/LeerdoelenView.java
+++ src/nl/uu/fi/dwo/mobile/client/ui/views/LeerdoelenView.java
@@ -1,11 +1,7 @@
 package nl.uu.fi.dwo.mobile.client.ui.views;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -18,14 +14,6 @@ public class LeerdoelenView extends Composite  {
 
 	public LeerdoelenView() {
 		initWidget(uiBinder.createAndBindUi(this));
-	}
-
-	@UiField
-	Button button;
-
-
-	@UiHandler("button")
-	void onClick(ClickEvent e) {
 	}
 
 }
