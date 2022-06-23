b87bf68e417fe7459d20cc24951d46873eb49c9f
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2022-06-23 05:39:55 +0000

    remove comments, fix imports
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/trunk@27750 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/nl/uu/fi/dwo/mobile/client/DWOplayerDefaults.java
+++ src/nl/uu/fi/dwo/mobile/client/DWOplayerDefaults.java
@@ -1,26 +1,11 @@
 package nl.uu.fi.dwo.mobile.client;
 
-import nl.uu.fi.dwo.mobile.client.ui.NavigationCss;
 import nl.uu.fi.dwo.mobile.client.ui.ActivityComponent;
-import nl.uu.fi.dwo.mobile.client.ui.NavigationBundle;
-import nl.uu.fi.dwo.mobile.client.ui.NavigationTextAndroid;
-import nl.uu.fi.dwo.mobile.client.ui.NavigationTextDefault;
-import nl.uu.fi.dwo.mobile.client.ui.NavigationTextIpad;
-import nl.uu.fi.dwo.mobile.client.ui.Prepare;
 import nl.uu.fi.dwo.mobile.client.ui.ScoreNavIF;
 import nl.uu.fi.dwo.mobile.client.ui.StatusBarIF;
 import nl.uu.fi.dwo.mobile.client.ui.views.ScoreNavFacade;
-import nl.uu.fi.dwo.mobile.utils.Logging;
-import nl.uu.fi.dwo.mobile.utils.NoLogging;
-
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.user.client.Window;
-import com.google.web.bindery.event.shared.EventBus;
-import com.google.web.bindery.event.shared.SimpleEventBus;
-import com.googlecode.mgwt.ui.client.MGWT;
-import com.googlecode.mgwt.ui.client.MGWTStyle;
-import com.googlecode.mgwt.ui.client.OsDetection;
-import com.googlecode.mgwt.ui.client.theme.base.HeaderCss;
 
 public class DWOplayerDefaults implements DWOplayerParameters {
 
@@ -44,50 +29,9 @@ public class DWOplayerDefaults implements DWOplayerParameters {
 		return "dwo";
 	}
 
-//	public HeaderCss headercss() {
-//		return MGWTStyle.getTheme().getMGWTClientBundle().getHeaderCss();
-//	}
 
-//	private NavigationBundle navigationBundle;
 	protected String launchData;//, courseDescription;
 	
-//	NavigationCss navigationcss() {		
-//		if(navigationBundle == null) {
-//		    OsDetection detection = MGWT.getOsDetection();
-//
-//		    if (detection.isAndroidPhone()) {
-//		    	navigationBundle = GWT.create(NavigationTextAndroid.class);
-//		    }
-//
-//		    if (detection.isAndroidTablet()) {
-//		    	navigationBundle = GWT.create(NavigationTextAndroid.class);
-//		    }
-//
-//		    if (detection.isIPhone()) {
-//		        if (detection.isRetina()) {
-//		        	navigationBundle = GWT.create(NavigationTextIpad.class);
-//		        } else {
-//		        	navigationBundle = GWT.create(NavigationTextIpad.class);
-//		        }
-//		      }
-//
-//		      if (detection.isIPad()) {
-//		        if (detection.isIPadRetina()) {
-//		        	navigationBundle = GWT.create(NavigationTextIpad.class);
-//		        } else {
-//		        	navigationBundle = GWT.create(NavigationTextIpad.class);
-//		        }
-//		      }
-//		    
-//		    
-//			if(navigationBundle == null)
-//				navigationBundle = GWT.create(NavigationTextDefault.class);
-//		}
-//		NavigationCss navigationcss = navigationBundle.navigationcss();
-//		navigationcss.ensureInjected();
-//		return navigationcss;
-//	}
-
 	@Override
 	public boolean isNavTitle() {
 		return false;
@@ -101,8 +45,6 @@ public class DWOplayerDefaults implements DWOplayerParameters {
 	@Override
 	public StatusBarIF getStatusBar(ActivityComponent a) {
 		return new nl.uu.fi.dwo.mobile.client.ui.dwokb.DWOKeyboard(a);
-
-		//return new nl.uu.fi.dwo.mobile.client.ui.dwokb.FormuleKeyboard();
 	}
 
 	@Override
@@ -133,10 +75,6 @@ public class DWOplayerDefaults implements DWOplayerParameters {
 //				+ host
 //				+ "/dwo/rest/public/course/getCourseDescription?courseId=";		
 	}
-
-//	protected String getDefaultHost() {
-//		return "ws-dev.fisme.science.uu.nl";
-//	}
 	
 	public String getHost() {
 		//if(GWT.isProdMode()) 
