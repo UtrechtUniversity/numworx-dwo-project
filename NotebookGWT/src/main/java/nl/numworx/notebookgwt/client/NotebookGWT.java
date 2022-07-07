775b3270a6a69947b670731b9148e1909a447845
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2022-07-07 12:18:40 +0000

    Initial import.
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/notebook/trunk@1085 32ccb885-960d-4b53-a61f-10870b223014

---
+++ NotebookGWT/src/main/java/nl/numworx/notebookgwt/client/NotebookGWT.java
@@ -0,0 +1,10 @@
+package nl.numworx.notebookgwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class NotebookGWT implements EntryPoint {
+  public void onModuleLoad() {
+
+				        				        
+  }
+}
