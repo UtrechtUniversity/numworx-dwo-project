b6ae38087dd0de37dff89aa9539e955911fdde76
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2022-09-07 14:19:22 +0200

    werkt met inloggen en 1 chatroom

--- ChatGWT-client/src/main/java/nl/uu/fi/dwo/lms/chatgwt/ChatGWT.java
+++ ChatGWT-client/src/main/java/nl/uu/fi/dwo/lms/chatgwt/ChatGWT.java
@@ -5,16 +5,29 @@ import java.util.logging.Logger;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.NodeList;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.BorderStyle;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.ResizeLayoutPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.SimpleLayoutPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.web.bindery.event.shared.HandlerRegistration;
@@ -29,8 +42,11 @@ import com.stanziq.strophe.client.Namespace;
 import nl.uu.fi.dwo.interaction.client.FormuleClipboardIF;
 import nl.uu.fi.dwo.interaction.client.FormuleEditorIF;
 import nl.uu.fi.dwo.interaction.client.FormuleFont;
+import nl.uu.fi.dwo.interaction.client.JSONUtilities;
 import nl.uu.fi.dwo.interaction.client.keyboard.EnterType;
+import nl.uu.fi.dwo.interaction.client.keyboard.FocusOnTouch;
 import nl.uu.fi.dwo.keyboard.client.AbstractKeyboard;
+import nl.uu.fi.dwo.keyboard.client.AbstractKeyboard.HasHeight;
 import nl.uu.fi.dwo.keyboard.client.Combined;
 import nl.uu.fi.dwo.keyboard.client.CombinedState;
 import nl.uu.fi.dwo.keyboard.client.DWOCombinedKeyboardFactory;
@@ -43,8 +59,9 @@ import nl.uu.fi.dwo.lms.chatgwt.util.MD5;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class ChatGWT implements EntryPoint, CombinedState {
+public class ChatGWT implements EntryPoint, CombinedState, HasHeight, FormuleClipboardIF {
 	
+	private static final ChangeEvent CHANGE_EVENT = new ChangeEvent() {};
 	private static String DOMAIN = "chat-dev.dwo.nl";
 	private static String ROOMS = "conference." + DOMAIN;
 	private static String BOSH = "wss://chat-dev.dwo.nl/xmpp-websocket";
@@ -59,6 +76,8 @@ public class ChatGWT implements EntryPoint, CombinedState {
 			LOG.info("received " + element.serialize());
 //			String to = element.getAttribute("to");
 			String from = element.getAttribute("from");
+			int at = from.indexOf('/');
+			if (at>0) from = from.substring(at+1);
 			String type = element.getAttribute("type");
 
 			NodeList<com.google.gwt.dom.client.Element> elems = element.getElementsByTagName("body");
@@ -66,9 +85,23 @@ public class ChatGWT implements EntryPoint, CombinedState {
 			if (("chat".equals(type)||"groupchat".equals(type)) && elems.getLength() > 0) {
 				Element body = (Element) elems.getItem(0);
 				Label afzender = new Label("From: " + from);
+				
 				panel.add(afzender);
-				Label message = new Label(body.getText());
+				StubWidget message = new StubWidget();
+				
+				HashMap<String, Object> data = new HashMap<>();
+				data.put("rekenTool", Boolean.FALSE);
+				data.put("tekst", body.getText());
+				data.put("balkZichtbaar", Boolean.FALSE);
+				data.put("boxMetRand", Boolean.FALSE);
+				data.put("editable", Boolean.FALSE);
+				message.init(800, 100, data);				
+				HashMap<String, Object> state = null;
+				//message.setState(state);
+
+				message.getElement().getStyle().setBorderStyle(BorderStyle.NONE);
 				panel.add(message);
+				scroll.scrollToBottom();
 			}
 			return true;
 		}
@@ -102,49 +135,62 @@ public class ChatGWT implements EntryPoint, CombinedState {
 	private TextBox username;
 	private TextBox password;
 	private VerticalPanel panel;
-	private TextBox input;
 	private ChatUser chatUser;
 	private Combined combined;
 	private ChangeHandler handler;
+	private SimplePanel container;
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		RootLayoutPanel root = RootLayoutPanel.get();
 		
-		
+// dummy login schreen
+		FlowPanel login = new FlowPanel();
 		username = new TextBox();
-		RootPanel.get().add(username);
+		login.add(username);
 		password = new PasswordTextBox();
 		password.setValue("");
-		RootPanel.get().add(password);
+		login.add(password);
 		
 		Button btn = new Button("LOGIN");
 		btn.addClickHandler(this::onClickLogin);
-		RootPanel.get().add(btn);
+		login.add(btn);
 		btn = new Button("LOGOUT");
 		btn.addClickHandler(this::onClickLogout);
-		RootPanel.get().add(btn);
+		login.add(btn);
+		
+		root.add(login);
+		root.setWidgetTopHeight(login, 0, Unit.PX, 40, Unit.PX);
+		
+		
+		main = new DockLayoutPanel(Unit.PX);
+		main.addStyleName("main");
+		FocusPanel wrap;
+		root.add(wrap = FocusOnTouch.wrap(main));
+		root.setWidgetTopBottom(wrap, 40, Unit.PX, 0, Unit.PX);
+		Style style = main.getElement().getStyle();
+		style.setHeight(100, Unit.PCT);
+		
 		
 		panel = new VerticalPanel();
-		RootPanel.get().add(panel);
+		scroll = new ScrollPanel(panel);
 		
-		input = new TextBox();
-		RootPanel.get().add(input);	
 		
-		StubEditor editor = new StubEditor();
-		
+		editor = new StubWidget();
+		editor.getElement().getStyle().setBorderStyle(BorderStyle.NONE);
+
 		HashMap<String, Object> data = new HashMap<>();
+		data.put("rekenTool", Boolean.FALSE);
+		editor.init(800, 100, data);
 		
-		editor.init(800, 200, data);
-		
-		RootPanel.get().add(editor);
 		
 		HashMap<String, Object> state = new HashMap<>();
 		editor.setState(state);
 		
 		btn = new Button("SEND");
 		btn.addClickHandler(this::onClickInput);
-		RootPanel.get().add(btn);
+		main.addSouth(btn, 40);
 		
 		
 		
@@ -153,15 +199,35 @@ public class ChatGWT implements EntryPoint, CombinedState {
 		factory.setPremium(true);
 		factory.setCombinedState(this);
 		
-		AbstractKeyboard keyboard = factory.getKeyboard();
-		RootPanel.get().add(keyboard);		
-		keyboard.setEnterType(EnterType.ENTER);
+		keyboard = factory.getKeyboard();
+		container = new SimpleLayoutPanel();
+		style = container.getElement().getStyle();
+		style.setBackgroundColor("#e5e7e9");
+		style.setProperty("maxWidth", 1024, Unit.PX);
+		container.setWidget(keyboard);
+
+		main.addSouth(container, 200);
+		main.getWidgetContainerElement(container).getStyle().setBackgroundColor("#e5e7e9");
+		keyboard.setSoortKeyboard(0);
+		keyboard.setWriteMathSet(0);
+		keyboard.setScrollPanel(this, 0);
 		keyboard.setEditor(editor);
+
+		main.addSouth(editor, 100);
+
+		FocusOnTouch.installKeyboard(keyboard, this);
+		FocusOnTouch.focus();
+		
+		setHeight(-keyboard.getKeyboardHeight());
+		main.add(scroll);
+		main.forceLayout();
 	}
 
 	private void onClickLogout(ClickEvent event) {
-		connection.disconnect("logout");
-		connection = null;
+		if (connection != null) {
+			connection.disconnect("logout");
+			connection = null;
+		}
 	}
 
 	private void onClickLogin(ClickEvent event) {
@@ -176,16 +242,28 @@ public class ChatGWT implements EntryPoint, CombinedState {
 		password = u + ":" + password;
 		password = Base64.btoa(password);
 
-		connection.connect(chatUser.jid, password, callback);		
+		connection.connect(chatUser.jid, password, callback);
+		FocusOnTouch.focus();
+		event.preventDefault();
 	}
 
-	private void onClickInput(ClickEvent event) {
-		String value = input.getValue();
+	private void onClickInput(ClickEvent event) {		
+		HashMap<String, Object> map = editor.getState();
+		LOG.info("editor state = " + map);
+		String value = JSONUtilities.wrapMap(map).getString("tekst");
+		if (value == null || value.isEmpty() || connection == null)
+		{
+			return;		
+		}
+		editor.clearAll();
+		editor.insert("");
 		LOG.info("send " + value);
 		
 		String[][] attributes = { { "to", room.jid }, { "type", "groupchat" } };
 		Builder reply = Builder.$msg(attributes).c("body",null).t(value);
 		connection.send(reply);
+		FocusOnTouch.focus();
+		keyboard.focus();
 	}
 
 		
@@ -197,7 +275,8 @@ public class ChatGWT implements EntryPoint, CombinedState {
 
 	@Override
 	public void setCombined(Combined state) {
-		combined = state;		
+		combined = state;
+		if (handler != null) handler.onChange(CHANGE_EVENT);
 	}
 
 	@Override
@@ -209,8 +288,30 @@ public class ChatGWT implements EntryPoint, CombinedState {
 	public int getWidth() {
 		return 0;
 	}
+
+	@Override
+	public void setHeight(int px) {
+		container.setPixelSize(-1, -px);
+		main.setWidgetSize(container, -px);
+	}
+
+
+	private String clipboard = "";
+	private StubWidget editor;
+	private DockLayoutPanel main;
+	private AbstractKeyboard keyboard;
+	private ScrollPanel scroll;
+	@Override
+	public String getClipboard() {
+		return clipboard;
+	}
+
+	@Override
+	public void setClipboard(String formule) {
+		clipboard = formule;
+	}
 	
-	
+
 	
 	
 }
