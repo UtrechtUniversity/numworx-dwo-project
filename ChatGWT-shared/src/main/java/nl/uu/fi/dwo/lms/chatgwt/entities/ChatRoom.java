e088b7a539e3708ce2fce349f4b87173ffc9bea8
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2022-09-08 15:23:56 +0200

    Merge branch 'develop' of git@git.science.uu.nl:dwoproject/DWO-LMS/chatgwt.git into develop

--- ChatGWT-shared/src/main/java/nl/uu/fi/dwo/lms/chatgwt/entities/ChatRoom.java
+++ ChatGWT-shared/src/main/java/nl/uu/fi/dwo/lms/chatgwt/entities/ChatRoom.java
@@ -1,14 +1,17 @@
 package nl.uu.fi.dwo.lms.chatgwt.entities;
 
+import java.util.List;
+
 public class ChatRoom {
 	public ChatRoom(String string) {
 		jid = string;
 		int at = string.indexOf('@');
-		displayName = jid.substring(0, at);
+		displayName = at >=0 ? jid.substring(0, at) : jid;
 	}
 	
 	public ChatRoom() {}
 	
 	public String jid;
 	public String displayName;
+	public List<ChatUser> chatUser;
 }
