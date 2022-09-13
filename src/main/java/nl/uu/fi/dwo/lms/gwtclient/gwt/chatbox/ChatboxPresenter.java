faeb7b5acde94070e65c0174c0e820d282179627
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2022-09-13 06:29:13 +0000

    me
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOGwtClient/trunk@27957 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/chatbox/ChatboxPresenter.java
+++ src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/chatbox/ChatboxPresenter.java
@@ -3,13 +3,17 @@ package nl.uu.fi.dwo.lms.gwtclient.gwt.chatbox;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.inject.Inject;
 
 import org.osgi.util.promise.Promise;
 import org.osgi.util.promise.Promises;
 
+import com.google.common.collect.Streams;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.web.bindery.event.shared.EventBus;
@@ -18,14 +22,17 @@ import fi.dwo.gwt.lib.rest.util.RestAuthenticator;
 import nl.uu.fi.dwo.lms.chatgwt.entities.ChatRoom;
 import nl.uu.fi.dwo.lms.chatgwt.entities.ChatUser;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.DwoGlobalVars;
+import nl.uu.fi.dwo.lms.gwtclient.gwt.login.LoginEvent;
+import nl.uu.fi.dwo.lms.gwtclient.gwt.login.LoginEventHandler;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.persons.PersonsService;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.ui.BasicDisplay;
 import nl.uu.fi.dwo.rest.dom.entities.DomSchoolClass;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudent;
+import nl.uu.fi.dwo.rest.dom.entities.DomTeacher;
 import nl.uu.fi.dwo.rest.dom.entities.DomUserFull;
 import nl.uu.fi.dwo.rest.dom.entities.RoleType;
 
-public class ChatboxPresenter implements ValueChangeHandler<String> {
+public class ChatboxPresenter implements ValueChangeHandler<String>, LoginEventHandler {
 
 	public interface Display extends BasicDisplay {
 
@@ -50,6 +57,7 @@ public class ChatboxPresenter implements ValueChangeHandler<String> {
 		
 		//RestAuthenticator.instance.addValueChangeHandler(this);
 		bus.addHandlerToSource(ValueChangeEvent.getType(), RestAuthenticator.instance, this); // resettable eventbus, helaas werkt niet want Authenticator gebruikt andere bus
+		bus.addHandler(LoginEvent.TYPE, this);
 		view.openUrl("about:blank");
 	}
 	
@@ -70,12 +78,10 @@ public class ChatboxPresenter implements ValueChangeHandler<String> {
 			view.openUrl("about:blank");
 			return;
 		}
-		user.nickName = u.getDisplayName();
-		user.jid = u.getUserName();
+        RoleType role = RoleType.valueOf(vars.getActiveSchoolRoleAndClass().getRole().getRoleName());    
+        user = new ChatUser(u, role);
 		String password = RestAuthenticator.instance.getAuthorization(); // access token of so
 		user.token = strip(password);
-        RoleType role = RoleType.valueOf(vars.getActiveSchoolRoleAndClass().getRole().getRoleName());    
-		user.role  = role;
 		if (role == RoleType.STUDENT) {
 			DomSchoolClass klas = vars.getActiveSchoolRoleAndClass().getSchoolClass();
 			Promise<ChatRoom> room = roomOfSchoolClass(klas);
@@ -108,22 +114,23 @@ public class ChatboxPresenter implements ValueChangeHandler<String> {
 		room.chatUser = Collections.emptyList();
 		if ( service.isPresent() ) {
 			Promise<List<DomStudent>> students = service.get().getStudentsInSchoolClass(klas);
-			return students.map(list -> {
-				room.chatUser = list.stream().map(this::newChatUser).collect(Collectors.toList());
+			Promise<List<DomTeacher>> teachers = service.get().getTeachersInSchoolClass(klas);
+			
+			Promise<Stream<ChatUser>> u1 = students.map(list -> list.stream().map(ChatUser::new));
+			Promise<Stream<ChatUser>> u2 = teachers.map(list -> list.stream().map(ChatUser::new));
+			
+			Promise<List<Stream<ChatUser>>> all = Promises.all(u1, u2);
+			return all.map(streams -> {
+				Stream<ChatUser> s = streams.stream().flatMap(Function.identity());
+				room.chatUser = s.collect(Collectors.toList());
 				return room;
+		
 			});
 		}
 		room.chatUser = Collections.singletonList(user);
 		return Promises.resolved(room);
 	}
-	
-	private ChatUser newChatUser(DomStudent s) {
-		ChatUser u = new ChatUser(s.getUserName());
-		u.nickName = s.getDisplayName();
-		u.role = RoleType.STUDENT;
-		return u; // no token, no chatRoom
-	}
-	
+
 	private Promise<List<ChatRoom>> roomOfSchoolClass(List<DomSchoolClass> list) {
 		List<Promise<ChatRoom>> promises = list.stream().map(this::roomOfSchoolClass).collect(Collectors.toList());
 		Promise<List<ChatRoom>> result = Promises.all(promises);
@@ -148,5 +155,12 @@ public class ChatboxPresenter implements ValueChangeHandler<String> {
 		if (password.toLowerCase().startsWith("basic ")) return password.substring(6);
 		if (password.toLowerCase().startsWith("bearer ")) return password.substring(7);
 		return "None";
+	}
+
+	@Override
+	public void onLoginEvent(LoginEvent loginEvent) {
+		GWT.log("catch " + loginEvent.getState());
+		view.openUrl("about:blank");
+		inited = false;
 	}
 }
