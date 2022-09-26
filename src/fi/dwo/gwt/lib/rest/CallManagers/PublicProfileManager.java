4e3128fd6c2d9c8347353ce51f39ac566ad98389
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2022-09-26 11:03:40 +0000

    https://numworx.atlassian.net/browse/LMS-552
    extra methode: getDescription 
    
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-gwt-lib/trunk@28011 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo/gwt/lib/rest/CallManagers/PublicProfileManager.java
+++ src/fi/dwo/gwt/lib/rest/CallManagers/PublicProfileManager.java
@@ -1,10 +1,12 @@
 package fi.dwo.gwt.lib.rest.CallManagers;
 
 import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfileFull;
+import nl.uu.fi.dwo.rest.entities.RestDwoProfile;
 
 import org.osgi.util.promise.Promise;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.json.client.JSONValue;
 
 import fi.dwo.gwt.lib.rest.client.RestCallers.PublicProfileRestCaller;
 import fi.dwo.gwt.lib.rest.util.PromiseCallback;
@@ -25,6 +27,12 @@ public class PublicProfileManager {
 
 	public Promise<DomDwoProfileFull> get(int id) {
 		return get(Integer.toString(id));
+	}
+	
+	public Promise<JSONValue> getDescription(RestDwoProfile rest) {
+		PromiseCallback<JSONValue> result = new PromiseCallback<>();
+		service.getDescription(rest, result);
+		return result.getPromise();
 	}
 
 }
--- src/fi/dwo/gwt/lib/rest/client/RestCallers/PublicProfileRestCaller.java
+++ src/fi/dwo/gwt/lib/rest/client/RestCallers/PublicProfileRestCaller.java
@@ -1,18 +1,26 @@
 package fi.dwo.gwt.lib.rest.client.RestCallers;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
 import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfileFull;
+import nl.uu.fi.dwo.rest.entities.RestDwoProfile;
 
 import org.fusesource.restygwt.client.MethodCallback;
 import org.fusesource.restygwt.client.RestService;
+
+import com.google.gwt.json.client.JSONValue;
 
 public interface PublicProfileRestCaller extends RestService {
 
 	@GET
     @Path("/public/profile/{id}")
     public void get(@PathParam("id") String id, MethodCallback<DomDwoProfileFull> callback);
+	
+	@PUT
+	@Path("/public/profile/description")
+	public void getDescription(RestDwoProfile rest, MethodCallback<JSONValue> callback);
 
 }
