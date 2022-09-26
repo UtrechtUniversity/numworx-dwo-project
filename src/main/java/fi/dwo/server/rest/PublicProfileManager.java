7bf009404cf4ac01f257394a9f693b332afcd7ff
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2022-09-26 11:01:08 +0000

    https://numworx.atlassian.net/browse/LMS-552
    extra methode: getDescription 
    omzetten WiskOpdr launchdata naar json
    
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWOServer/trunk@28010 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/main/java/fi/dwo/server/rest/PublicProfileManager.java
+++ src/main/java/fi/dwo/server/rest/PublicProfileManager.java
@@ -1,15 +1,31 @@
 package fi.dwo.server.rest;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Hashtable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.GZIPOutputStream;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfileFull;
+import nl.uu.fi.dwo.rest.entities.RestDwoProfile;
+import nl.uu.fi.dwo.rest.exceptions.Dwo2Exception;
+import fi.beans.dwomaccess.JSONEncoder;
+import fi.beans.private_base64code.StringCodeObject;
+import fi.dwo.commons.persistence.MySQLPersistenceId;
 import fi.dwo.commons.persistence.entities.PersistentDwoProfile;
+import fi.dwo.server.PersistentDataManagers.core.CourseDataManager;
 import fi.dwo.server.PersistentDataManagers.core.DwoProfileManager;
 
 @Path("/public/profile")
@@ -34,6 +50,36 @@ public class PublicProfileManager {
 			LOG.severe("Profile not found for " + id);
 		}
 		return null;
+	}
+	
+	@PUT
+	@Path("/description")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response getDescription(RestDwoProfile rest) throws Dwo2Exception, IOException {
+     try {
+		Long id = MySQLPersistenceId.getNativeId(rest.getDomDwoProfile());
+		PersistentDwoProfile profile = DwoProfileManager.findEntity(id);
+        if(profile == null) return Response.ok().entity("{}").build(); // Not fatal
+        Hashtable map = (Hashtable) StringCodeObject.decodeStringToObject(profile.getDwoProfileText(), null); // FIXM load wiskopdr.jar
+        StringWriter writer = new StringWriter();
+		JSONEncoder.encode(map, writer, null); // FIXME, load wiskopdr.jar
+        String string = writer.toString();
+
+//        if (data != null) {
+//        	ByteArrayOutputStream bos = new ByteArrayOutputStream(string.length());
+//        	GZIPOutputStream gos = new GZIPOutputStream(bos);
+//        	OutputStreamWriter w = new OutputStreamWriter(gos, StandardCharsets.UTF_8);
+//        	w.write(string);
+//        	w.close();
+//        	data.setDescriptionbytes(bos.toByteArray());
+//        	data = CourseDataManager.edit(data);
+//        }
+		return Response.ok().entity(string).build();
+     } catch(Exception oops) {
+    	 LOG.log(Level.WARNING, "get profile description", oops);
+    	 return Response.noContent().build();
+     }
+		
 	}
 	
 }
