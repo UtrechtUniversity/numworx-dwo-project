d13d44d6c0d9125b7d04362b112eded15455c2e2
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2022-10-26 11:50:39 +0000

    initial version
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DwoPlayer/GWT/trunk@28200 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/nl/uu/fi/dwo/mobile/client/ui/views/interactionviews/reviewvak/ReviewObjectMap.java
@@ -0,0 +1,114 @@
+package nl.uu.fi.dwo.mobile.client.ui.views.interactionviews.reviewvak;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import nl.uu.fi.dwo.interaction.client.json.ObjectList;
+import nl.uu.fi.dwo.interaction.client.json.ObjectMap;
+import nl.uu.fi.dwo.mobile.client.ui.views.CorrectieView;
+
+class ReviewObjectMap implements ObjectMap {
+
+	final static String STATES = ReviewActivity.INTERACTIE_PANEL_STATES;
+	
+	final private ObjectMap delegate;
+	private ObjectMap review;
+
+	public boolean containsKey(String key) {
+		return delegate.containsKey(key);
+	}
+
+	public Object get(String key) {
+		if (STATES.equals(key)) return review.get(key);
+		return delegate.get(key);
+	}
+
+	public int getInt(String key) {
+		return delegate.getInt(key);
+	}
+
+	public double getDouble(String key) {
+		return delegate.getDouble(key);
+	}
+
+	public boolean getBoolean(String key) {
+		return delegate.getBoolean(key);
+	}
+
+	public boolean getBoolean(String key, boolean value) {
+		return delegate.getBoolean(key, value);
+	}
+
+	public String getString(String key) {
+		return delegate.getString(key);
+	}
+
+	public Map<String, Object> getMap(String key) {
+		return delegate.getMap(key);
+	}
+
+	public List<Object> getList(String key) {
+		return delegate.getList(key);
+	}
+
+	public ObjectMap getObjectMap(String key) {
+		return delegate.getObjectMap(key);
+	}
+
+	public ObjectList getObjectList(String key) {
+		if (STATES.equals(key)) return review.getObjectList(key);
+		return delegate.getObjectList(key);
+	}
+
+	public List<String> getStringList(String key) {
+		return delegate.getStringList(key);
+	}
+
+	public List<Integer> getIntegerList(String key) {
+		return delegate.getIntegerList(key);
+	}
+
+	public List<Boolean> getBooleanList(String key) {
+		return delegate.getBooleanList(key);
+	}
+
+	public List<Double> getDoubleList(String key) {
+		return delegate.getDoubleList(key);
+	}
+
+	public List<Map<String, Object>> getMapList(String key) {
+		if (STATES.equals(key)) return review.getMapList(key);
+		return delegate.getMapList(key);
+	}
+
+	public double[] getDoubleArray(String key) {
+		return delegate.getDoubleArray(key);
+	}
+
+	public int[] getIntArray(String key) {
+		return delegate.getIntArray(key);
+	}
+
+	public String[] getStringArray(String key) {
+		return delegate.getStringArray(key);
+	}
+
+	public boolean[] getBooleanArray(String key) {
+		return delegate.getBooleanArray(key);
+	}
+
+	public boolean isEmpty() {
+		return delegate.isEmpty();
+	}
+
+	public Set<String> keySet() {
+		return delegate.keySet();
+	}
+
+	ReviewObjectMap(ObjectMap delegate) {
+		this.delegate = delegate;
+		this.review = delegate.getObjectMap(CorrectieView.REVIEW_INTERACTIE_DATA);
+	}
+
+}
