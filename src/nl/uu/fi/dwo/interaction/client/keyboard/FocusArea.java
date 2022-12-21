107b1d072bf161482d8385940ae3dbb75e58bca6
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2022-12-21 08:17:27 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/GWT/DWOinteraction/trunk@28419 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/nl/uu/fi/dwo/interaction/client/keyboard/FocusArea.java
@@ -0,0 +1,101 @@
+package nl.uu.fi.dwo.interaction.client.keyboard;
+
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.ui.TextArea;
+
+@SuppressWarnings("deprecation")
+class FocusArea extends TextArea {
+	private final FocusOnTouch handler;
+
+	static native String getClipboardData(Event event) /*-{
+		return event.clipboardData.getData('text/plain'); 
+	}-*/;
+
+	FocusArea(FocusOnTouch handler) {
+		this.handler = handler;
+		sinkEvents(Event.ONPASTE); 
+		getElement().getStyle().setZIndex(-100);
+		registerOnCutCopy(getElement());
+	}
+
+	@Override
+	public void onBrowserEvent(Event event) {
+		int type = event.getTypeInt();
+		if (type == Event.ONPASTE) {
+			
+			String data = getClipboardData(event);
+			handler.onPaste(data);
+			event.preventDefault();
+			return;
+		}
+		
+		super.onBrowserEvent(event);
+	}
+
+	   private native void registerOnCutCopy(Element element)
+	    /*-{
+	        var that = this;
+	        element.oncut = $entry(function(event)
+	        {
+	            that.@nl.uu.fi.dwo.interaction.client.keyboard.FocusArea::doCut(Lcom/google/gwt/user/client/Event;)(event);
+	            return false;
+	        });
+	        element.oncopy = $entry(function(event)
+	        {
+	            that.@nl.uu.fi.dwo.interaction.client.keyboard.FocusArea::doCopy(Lcom/google/gwt/user/client/Event;)(event);
+	            return false;
+	        });
+	    }-*/;
+
+	    @SuppressWarnings("unused")
+	    private void doCut(Event event)
+	    {
+	        // substitute your own 'cut' function here
+	        String clip = handler.doCut();
+	        // and attempt to shove that text into the clipboard
+	        setClipboardData(event, clip);
+	    }
+	    @SuppressWarnings("unused")
+	    private void doCopy(Event event)
+	    {
+	        // substitute your own 'cut' function here
+	        String clip = handler.doCopy();
+	        // and attempt to shove that text into the clipboard
+	        setClipboardData(event, clip);
+	    }
+
+	    public static native void setClipboardData(Event event, String text)
+	    /*-{
+	        // Apple says this should work:
+	        // http://devworld.apple.com/mac/library/documentation/AppleApplications/Conceptual/SafariJSProgTopics/Tasks/CopyAndPaste.html
+	        // But it's broken...here's the bug report:
+	        // https://bugs.webkit.org/show_bug.cgi?id=17645
+	        if (event && event.clipboardData) // WebKit (Chrome/Safari)
+	        {
+	            try
+	            {
+	                event.clipboardData.setData("text/plain", text);
+	                return;
+	            }
+	            catch (e)
+	            {
+	                // Hmm, that didn't work.
+	            }
+	        }
+
+	        // http://msdn.microsoft.com/en-us/library/ms535220(VS.85).aspx
+	        if ($wnd.clipboardData) // IE
+	        {
+	            try
+	            {
+	                $wnd.clipboardData.setData("Text", text);
+	                return;
+	            }
+	            catch (e)
+	            {
+	                // Hmm, that didn't work.
+	            }
+	        }
+	    }-*/;
+}
