45cdd386b1d1bb234462598cf3c8ed03b2036ae2
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2023-02-01 10:41:00 +0000

    met plafond algorithme
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWOProject/DWO-LMS/DWO-rest-lib/trunk@28515 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/main/java/nl/uu/fi/dwo/rest/util/StudentModelUtil.java
@@ -0,0 +1,138 @@
+package nl.uu.fi.dwo.rest.util;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelCategory;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelCategoryScore;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContextInfo;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelObj;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelObjectiveScore;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelStructure;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelStructureScore;
+
+public class StudentModelUtil {
+
+
+	Map<String, DomStudentModelObj> items;
+	Map<String, Collection<String>> foreknowledge;
+	Map<String, DomStudentModelObjectiveScore> scores;
+	DomStudentModelStructureScore structure;
+		
+	public StudentModelUtil() {
+		items = new TreeMap<>();
+		foreknowledge = new TreeMap<>();
+		scores = new TreeMap<>();
+	}
+	
+	public void setStudentModelStructure(DomStudentModelStructure structure) {
+		items.clear();
+		foreknowledge.clear();
+		for( DomStudentModelCategory cat: structure.getCategories()) {
+			for (DomStudentModelObj obj: cat.getObjectives()) {
+				addObjective(obj);
+			}			
+		}		
+		items.values().forEach(this::addForeKnowledge);
+// closure
+		boolean changed;
+		do {
+			changed = false;
+			for (Collection<String> set: foreknowledge.values()) {
+				Collection<String> add;
+				add = set.stream().flatMap(id -> foreknowledge.get(id).stream()).collect(Collectors.toSet());
+				if ( !add.isEmpty() && set.addAll(add) )
+					changed = true;
+			}
+		} while(changed);
+	}
+
+	private void addObjective(DomStudentModelObj obj) {
+		List<DomStudentModelObj> list = obj.getObjectives();
+		if (list != null) 
+			list.forEach(this::addObjective);
+		else {
+			String id = obj.getInfo().getId();
+			if (id != null) items.put(id, obj);
+		}
+	}
+
+	private void addForeKnowledge(DomStudentModelObj obj) {
+		DomStudentModelContextInfo info = obj.getInfo();
+		String id = info.getId();
+		Collection<String> items = info.getVoorkennis();		
+		Collection<String> set = foreknowledge.computeIfAbsent(id, key -> new TreeSet<String>());
+		if (items != null) set.addAll(items);
+	}
+	
+	public void setStudentModelScore(DomStudentModelStructureScore score) {
+		scores.clear();
+		structure = score;
+		for( DomStudentModelCategoryScore cat: score.getCategories()) {
+			for (DomStudentModelObjectiveScore s: cat.getObjectives()) {
+				addObjectiveScore(s);
+			}
+		}
+		
+	}
+
+	private void addObjectiveScore(DomStudentModelObjectiveScore s) {
+		List<DomStudentModelObjectiveScore> children = s.getChildren();
+		if (children != null)
+			children.forEach(this::addObjectiveScore);
+		else {
+			scores.put(s.getId(), s);
+		}
+	}
+	
+	public DomStudentModelStructureScore calculate() {
+		DomStudentModelStructureScore result = new DomStudentModelStructureScore();
+		result.setId(structure.getId());
+		result.setCategories(structure.getCategories().stream().map(this::copy).collect(Collectors.toList()));
+		result.recalculateAncestors();
+		return result;
+	}
+	
+	private DomStudentModelCategoryScore copy (DomStudentModelCategoryScore org) {
+		DomStudentModelCategoryScore result = new DomStudentModelCategoryScore();
+		result.setId(org.getId());
+		result.setObjectives(org.getObjectives().stream().map(this::calculate).collect(Collectors.toList()));		
+		return result;
+	}
+	private DomStudentModelObjectiveScore calculate(DomStudentModelObjectiveScore org) {
+		DomStudentModelObjectiveScore result = new DomStudentModelObjectiveScore();
+		String id = org.getId();
+		result.setId(id);
+		List<DomStudentModelObjectiveScore> list = org.getChildren();
+		if (list != null) {
+			result.setChildren(list.stream().map(this::calculate).collect(Collectors.toList()));
+		}
+		
+		long gc = org.getGreenCount();
+		double gs = org.getGreenScore();
+		long rc = org.getRedCount();
+		double rs = org.getRedScore();
+		long t = org.getTotalCount();
+		if (gc > 0L) {
+			Collection<String> objectives = foreknowledge.getOrDefault(id, Collections.emptyList());
+			int size = objectives.size();
+			long count;
+			if (size != 0) {
+				count = objectives.stream().filter(this::isCorrect).count();
+				gs = 0.5 + (gs-0.5) * count / size;
+			}
+		}
+		result.setScore(gs, gc, rs, rc, t);
+		return result;		
+	}
+	
+	boolean isCorrect(String id) {
+		return scores.get(id).getScore() > 0.75;
+	}
+}
---
+++ src/test/java/nl/uu/fi/dwo/rest/util/StudentModelUtilTest.java
@@ -0,0 +1,39 @@
+package nl.uu.fi.dwo.rest.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentModelUtilTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSetStudentModelStructure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStudentModelScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsCorrect() {
+		fail("Not yet implemented");
+	}
+
+}
