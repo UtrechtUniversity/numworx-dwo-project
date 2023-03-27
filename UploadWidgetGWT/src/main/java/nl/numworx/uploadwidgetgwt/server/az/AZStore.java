06cfa2ff3500b7eaba53ac84cbdf1dff40ba7b16
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2023-03-27 12:52:28 +0000

    Met AZStore
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/UploadWidget/trunk@1202 32ccb885-960d-4b53-a61f-10870b223014

---
+++ UploadWidgetGWT/src/main/java/nl/numworx/uploadwidgetgwt/server/az/AZStore.java
@@ -0,0 +1,11 @@
+package nl.numworx.uploadwidgetgwt.server.az;
+
+import nl.numworx.uploadwidgetgwt.server.Store;
+
+public class AZStore extends Store {
+
+	public AZStore() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
