f84064775d6ae43571202dbffc9cf410aa388812
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-04-18 14:37:17 +0200

    Origin met later ook een *

--- src/main/java/nl/numworx/oauth2client/server/Origin.java
+++ src/main/java/nl/numworx/oauth2client/server/Origin.java
@@ -10,14 +10,14 @@ public class Origin {
 	public static String of(HttpServletRequest request) {
 		String origin = request.getHeader("Origin");
 		for (String allow : ORIGINS) {
-			if (allow.equals(origin)) return allow;
+			if (allow.equals(origin)|| "*".equals(allow)) return allow;
 		}
 		return ORIGINS[0];
 	}
 
 	public static String of(String origin) {
 		for (String allow : ORIGINS) {
-			if (origin.startsWith(allow)) return allow;
+			if (origin.startsWith(allow)|| "*".equals(allow)) return allow;
 		}
 		return ORIGINS[0];
 	}
