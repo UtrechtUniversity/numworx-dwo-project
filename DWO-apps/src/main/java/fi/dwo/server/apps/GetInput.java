388ee52bb4aaaa69733d1d4e672c1f8f3921f443
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-10-27 08:52:43 +0200

    met een /get_input/ implementatie over de server

---
+++ DWO-apps/src/main/java/fi/dwo/server/apps/GetInput.java
@@ -0,0 +1,55 @@
+package fi.dwo.server.apps;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class GetInput extends HttpServlet {
+
+	private static final String GET_INPUT = "fi.dwo.server.apps.GetInput";
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String key = getKey(req);
+		HttpSession session = req.getSession();
+		session.removeAttribute(key);	
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String key = getKey(req);
+		HttpSession session = req.getSession(false);
+		if (session == null) {
+			resp.sendError(resp.SC_NOT_FOUND);
+			return;
+		}
+		Object result = session.getAttribute(key);
+		if (result == null) {
+			resp.sendError(resp.SC_NOT_FOUND);
+			return;			
+		}
+		resp.setContentType("text/plain");
+		resp.getWriter().print(result);
+	}
+
+	private String getKey(HttpServletRequest req) {
+		return GET_INPUT + Objects.toString(req.getPathInfo(), "/");
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String key = getKey(req);
+		HttpSession session = req.getSession();
+		BufferedReader reader = req.getReader();
+		Object data = reader.readLine();
+		session.setAttribute(key, data);
+	}
+
+
+}
