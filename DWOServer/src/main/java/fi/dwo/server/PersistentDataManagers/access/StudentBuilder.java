82c578b673c710c45094f32e6c6709421f9823ad
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-11-08 14:08:09 +0100

    test voor studentmodels per klas

--- DWOServer/src/main/java/fi/dwo/server/PersistentDataManagers/access/StudentBuilder.java
+++ DWOServer/src/main/java/fi/dwo/server/PersistentDataManagers/access/StudentBuilder.java
@@ -241,8 +241,19 @@ class StudentBuilder implements StudentDomainAuthorizer.StudentState_HR_R_S_SG_U
         for(PersistentStudentModelOfClass item: p4Class) {
         	Long id = item.getId().getModelID();
         	PersistentStudentModelContext context = StudentModelContextManager.findEntity(id);
-        	if (context == null) continue; // id is verwijdert, komt voor
-        	if (context.getDwoProfileID() != null && pid != context.getDwoProfileID().longValue()) continue; // filter by profileid
+        	if (context == null) continue; // id is verwijderd, komt voor
+
+        	
+        	if (context.getSchoolID().longValue()== 0L) // standaard model, dan toegang met profiel
+        	{
+        		if (!context.getProfiles().contains(dwoProfile) &&
+        			context.getDwoProfileID() != null && pid != context.getDwoProfileID().longValue()) continue; // filter by profileid
+        	} else { // school model, dan schoolid gelijk
+        		if (context.getSchoolID().longValue() != instance.getContext().getUserCtx().school.getSchoolID().longValue())
+        			continue;
+        	}
+   
+        	
         	DomStudentModelContext r = context.buildDomStudentModelContext();
         	DomStudentModelContext4Student rr = new DomStudentModelContext4Student(r.getId());
         	rr.setModelStructure(r.getModelStructure());
