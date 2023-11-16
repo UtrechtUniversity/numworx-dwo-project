127169922ad04a286f2031a1d5305243e1a988ba
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-11-16 14:23:18 +0100

    version management
    TODO uit BUILD halen

--- DWOServer/src/main/java/fi/dwo/server/BUILD.java
+++ DWOServer/src/main/java/fi/dwo/server/BUILD.java
@@ -1,8 +1,13 @@
 package fi.dwo.server;
 
-public interface BUILD {
-	String buildNumber = "${buildNumber}";
-	String version = "${project.version}";
-    String timeStamp = "${timestamp}";
+public class BUILD {
+	public static String buildNumber = "${buildNumber}";
+	public static String version;
+    public static String timeStamp = "${timestamp}";
+    public static String javaClient = "${DWOClientVersion}";
+    public static String htmlClient = "${DWOGwtClientVersion}";
 
+    static {
+    	version = "${project.version}";
+    }
 }
--- EBServer/src/main/java-templates/fi/dwo/server/BUILD.java
+++ EBServer/src/main/java-templates/fi/dwo/server/BUILD.java
@@ -1,7 +1,15 @@
 package fi.dwo.server;
 
-public interface BUILD {
-	String buildNumber = "${buildNumber}";
-	String version = "${project.version}";
-    String timeStamp = "${timestamp}";
+public class BUILD {
+	public static String buildNumber = "${buildNumber}";
+	public static String version;
+    public static String timeStamp = "${timestamp}";
+    public static String javaClient = "${DWOJClientVersion}";
+    public static String htmlClient = "${DWOGwtClientVersion}";
+
+    static {
+    	version = "${project.version}";
+    	javaClient = fi.dwo.dwojapplet.BUILD.version;
+    	//htmlClient = nl.uu.fi.dwo.lms.gwtclient.gwt.BUILD.version;
+    }
 }
