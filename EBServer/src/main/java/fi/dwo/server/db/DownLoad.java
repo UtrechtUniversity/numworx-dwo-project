7d6c7799ab34cc02258163bdcffd10f942115905
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-12-14 16:21:02 +0100

    meer UU features

--- EBServer/src/main/java/fi/dwo/server/db/DownLoad.java
+++ EBServer/src/main/java/fi/dwo/server/db/DownLoad.java
@@ -2,19 +2,44 @@ package fi.dwo.server.db;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @SuppressWarnings("serial")
 public class DownLoad extends HttpServlet {
 
+	private static class RedirectDispatcher implements RequestDispatcher {
+
+		private final String download;
+
+		public RedirectDispatcher(String download) {
+			this.download = download;
+		}
+
+		@Override
+		public void forward(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+			((HttpServletResponse) response).sendRedirect(download);
+
+		}
+
+		@Override
+		public void include(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		}
+
+	}
+
 	private String download = "https://www.numworx.nl/help/downloads/";
+	private RequestDispatcher dispatch = new RedirectDispatcher(download);
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.sendRedirect(download);
+		dispatch.forward(req, resp);
 	}
 
 	@Override
@@ -23,8 +48,12 @@ public class DownLoad extends HttpServlet {
 		param = getInitParameter("fi.dwo.server.db.download");
 		if (param != null)
 			param = getServletContext().getInitParameter(param);
-		if (param != null) {
+		if (param != null && !param.isEmpty() && !"${UU_DOWNLOAD}".equals(param)) {
 			download = param;
+			if (download.startsWith("/"))
+				dispatch = getServletContext().getRequestDispatcher(download);
+			else
+				dispatch = new RedirectDispatcher(param);
 		}
 	}
 
