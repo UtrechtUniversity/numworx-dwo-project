6ce3bcff4044d47a0c2221a77aceaac6989bb6af
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2024-02-23 15:55:18 +0100

    caching profile descriptions
    nog 10 minuten (erg kort)

--- DWO-gwt-lib/src/fi/dwo/gwt/lib/rest/CallManagers/PublicProfileManager.java
+++ DWO-gwt-lib/src/fi/dwo/gwt/lib/rest/CallManagers/PublicProfileManager.java
@@ -2,6 +2,7 @@ package fi.dwo.gwt.lib.rest.CallManagers;
 
 import nl.uu.fi.dwo.rest.dom.entities.DomDwoProfileFull;
 import nl.uu.fi.dwo.rest.entities.RestDwoProfile;
+import nl.uu.fi.dwo.rest.persistence.PersistenceClassType;
 
 import org.osgi.util.promise.Promise;
 
@@ -9,6 +10,7 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.json.client.JSONValue;
 
 import fi.dwo.gwt.lib.rest.client.RestCallers.PublicProfileRestCaller;
+import fi.dwo.gwt.lib.rest.util.PersistenceIdDecoderInterface;
 import fi.dwo.gwt.lib.rest.util.PromiseCallback;
 
 public class PublicProfileManager {
@@ -29,9 +31,16 @@ public class PublicProfileManager {
 		return get(Integer.toString(id));
 	}
 	
-	public Promise<JSONValue> getDescription(RestDwoProfile rest) {
+	public Promise<JSONValue> getDescription_old(RestDwoProfile rest) {
 		PromiseCallback<JSONValue> result = new PromiseCallback<>();
 		service.getDescription(rest, result);
+		return result.getPromise();
+	}
+
+	public Promise<JSONValue> getDescription(RestDwoProfile rest) {
+		PromiseCallback<JSONValue> result = new PromiseCallback<>();
+		Number id = (Number) PersistenceIdDecoderInterface.instance.idOf(rest.getDomDwoProfile().getId(), PersistenceClassType.PersistentDwoProfile);
+		service.getDescription(id, result);
 		return result.getPromise();
 	}
 
