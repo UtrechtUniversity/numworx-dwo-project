759a2312703f3c3d25febab396eaea7ae9b6648d
velth101 <w.p.g.vanvelthoven@uu.nl>
2024-09-24 14:57:59 +0200

    Group object met ObjectType

--- DWO-rest-lib/src/main/java/nl/uu/fi/dwo/rest/dom/xapi/Group.java
+++ DWO-rest-lib/src/main/java/nl/uu/fi/dwo/rest/dom/xapi/Group.java
@@ -5,6 +5,6 @@ import java.util.Set;
 public class Group {
   public Account account;
   public String name;
-  public final String objectType = "Group"; 
+  /*final*/ public String objectType = "Group"; 
   public Set<Agent> member;
 }
