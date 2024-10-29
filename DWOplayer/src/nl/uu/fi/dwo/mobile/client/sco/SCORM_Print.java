02d96e0713c3df9b9c2cebad9ffe510c45e5093d
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2024-10-29 09:52:49 +0100

    readonly scorm print

---
+++ DWOplayer/src/nl/uu/fi/dwo/mobile/client/sco/SCORM_Print.java
@@ -0,0 +1,57 @@
+package nl.uu.fi.dwo.mobile.client.sco;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.util.promise.Promise;
+import org.osgi.util.promise.Promises;
+
+public class SCORM_Print extends SCORM_2004_API {
+
+	static final Map<String, String> VALUES = new HashMap<>();
+	static {
+		VALUES.put(Memento.LESSON_MODE, "browse");
+		VALUES.put(Memento.COMPLETION_STATUS, Memento.COMPLETED);
+		VALUES.put(Memento.LOCATION, "0");
+	}
+	
+	public SCORM_Print() {
+	}
+
+	@Override
+	public Promise<String> Terminate() {
+		return Promises.resolved("");
+	}
+
+	@Override
+	public String GetValue(String name) {
+		String value = VALUES.get(name);
+		if (value != null) return value;
+		return super.GetValue(name);
+	}
+
+	@Override
+	public String SetValue(String name, String value) {
+		if (Memento.LOCATION.equals(name)) {
+			VALUES.put(name, value);
+		}
+		return "true"; // sink...
+	}
+
+	@Override
+	public Promise<String> Commit() {
+		return Promises.resolved("");
+	}
+
+	@Override
+	public Promise<String> getValuePromise(String name) {
+		return Promises.resolved("");
+	}
+
+	@Override
+	public Promise<Map<String, String>> getValuesPromise(Collection<String> names) {
+		return super.getValuesPromise(names);
+	}
+
+}
