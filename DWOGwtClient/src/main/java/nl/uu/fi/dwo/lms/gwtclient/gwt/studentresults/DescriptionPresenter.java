77cd995960bb3a481423725d04cd0af0edd8533e
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-01-08 09:55:09 +0100

    https://numworx.atlassian.net/browse/LMS-641
    varianten hier

--- DWOGwtClient/src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/studentresults/DescriptionPresenter.java
+++ DWOGwtClient/src/main/java/nl/uu/fi/dwo/lms/gwtclient/gwt/studentresults/DescriptionPresenter.java
@@ -1,6 +1,8 @@
 package nl.uu.fi.dwo.lms.gwtclient.gwt.studentresults;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
@@ -27,9 +29,13 @@ import com.google.web.bindery.event.shared.EventBus;
 
 import nl.uu.fi.dwo.lms.gwtclient.gwt.SwitchViewEvent;
 import nl.uu.fi.dwo.lms.gwtclient.gwt.SwitchViewEvent.SelectedView;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelCategory;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContext;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContextId;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelContextInfo;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelMethodInfo;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelObj;
+import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelStructure;
 import nl.uu.fi.dwo.rest.dom.entities.DomStudentModelVariant;
 
 public class DescriptionPresenter {
@@ -173,7 +179,17 @@ public class DescriptionPresenter {
 		private final DescriptionService service;
 		//DomStudentModelContextId current;
 
-		public Promise<Widget> get(DomStudentModelContextId current, DomStudentModelContextInfo info) {
+		public Promise<Widget> get(DomStudentModelContext current, DomStudentModelContextInfo info, String pid) {
+			if (pid != null) {
+				info = findInfo(current, info);
+				List<DomStudentModelMethodInfo> methods = info.getMethodInfo();
+				if (methods != null) {
+					for(DomStudentModelMethodInfo x:methods) {
+						if (pid.equals(x.key()))
+							return get(current, info, x);
+					}
+				}
+			}
 			return service.getDescription(current, info)
 			   .then(p -> {
 				String text = p.getValue();
@@ -187,6 +203,62 @@ public class DescriptionPresenter {
 			});
 			
 		}
+		
+		private DomStudentModelStructure structurecache;
+		private Map<String, DomStudentModelContextInfo> infocache = new HashMap<>();
+		
+		private DomStudentModelContextInfo findInfo(DomStudentModelContext current, DomStudentModelContextInfo info) {
+			if (info.getTitle() != null) return info;
+			String id = info.getId();
+			DomStudentModelContextInfo found = null;
+			DomStudentModelStructure structure = current.getModelStructure();
+			if (structure == structurecache)
+			{	found = infocache.get(id);
+				if (found != null) return found;
+			} else {
+				structurecache = structure;
+				infocache.clear();
+			}
+			if (id.equals(structure.getInfo().getId())) return structure.getInfo();
+			List<DomStudentModelCategory> children = structure.getCategories();
+			for(DomStudentModelCategory item : children) {
+				found = findInfo(item, id);
+				if (found != null) {
+					return found;
+				}
+			}
+			return info;
+		}
+
+		private DomStudentModelContextInfo findInfo(DomStudentModelCategory item, String id) {
+			DomStudentModelContextInfo info = item.getInfo();
+			infocache.put(info.getId(), info);
+			if (id.equals(info.getId())) return info;
+			List<DomStudentModelObj> children = item.getObjectives();
+			for(DomStudentModelObj obj : children) {
+				DomStudentModelContextInfo found = findInfo(obj, id);
+				if (found != null) {
+					return found;
+				}
+			}			
+			return null;
+		}
+
+		private DomStudentModelContextInfo findInfo(DomStudentModelObj item, String id) {
+			DomStudentModelContextInfo info = item.getInfo();
+			infocache.put(info.getId(), info);
+			if (id.equals(info.getId())) return info;
+			List<DomStudentModelObj> children = item.getObjectives();
+			if (children != null)
+			for(DomStudentModelObj obj : children) {
+				DomStudentModelContextInfo found = findInfo(obj, id);
+				if (found != null) {
+					return found;
+				}
+			}			
+			return null;
+		}
+
 		public Promise<Widget> get(DomStudentModelContextId current, DomStudentModelContextInfo info, DomStudentModelMethodInfo method) {
 			return service.getDescription(current, info)
 			   .then(p -> {
@@ -196,32 +268,35 @@ public class DescriptionPresenter {
 					json = text;
 					text = DescriptionPresenter.WISKOPDR_SIG;
 				}
-				if (json != null && info != null && info.getVariants() != null && method != null) {
-					String string = method.getVariant();
-					Optional<DomStudentModelVariant> opt = info.getVariants().stream().filter(t -> Objects.equals(t.getName(),string)).findAny();
-					if (opt.isPresent()) {
-						Map<String, Boolean> layers = opt.get().getLayers();
-						JSONValue v = JSONParser.parseStrict(json);
-						JSONObject instellingen = v.isObject().get("instellingen").isObject();
-						JSONBoolean hasLayers = instellingen.get("hasLayers").isBoolean();
-						if (hasLayers.booleanValue()) {
-							JSONArray names = instellingen.get("layerNames").isArray();
-							JSONArray values = instellingen.get("layerVisible").isArray();
-							for(int i = 0; i < names.size(); i++) {
-								String n = names.get(i).isString().stringValue();
-								Boolean visible = layers.getOrDefault(n, Boolean.FALSE);
-								values.set(i, JSONBoolean.getInstance(visible));
-							}
-							json = v.toString();
-						}
-					}
-				}
-				
-				
+				json = selectVariant(json, info, method);
 				Widget description = this.createDescription(text, json);
 				return Promises.resolved(description);
 			});
 			
+		}
+
+		protected String selectVariant(String json, DomStudentModelContextInfo info, DomStudentModelMethodInfo method) {
+			if (json != null && info != null && info.getVariants() != null && method != null) {
+				String string = method.getVariant();
+				Optional<DomStudentModelVariant> opt = info.getVariants().stream().filter(t -> Objects.equals(t.getName(),string)).findAny();
+				if (opt.isPresent()) {
+					Map<String, Boolean> layers = opt.get().getLayers();
+					JSONValue v = JSONParser.parseStrict(json);
+					JSONObject instellingen = v.isObject().get("instellingen").isObject();
+					JSONBoolean hasLayers = instellingen.get("hasLayers").isBoolean();
+					if (hasLayers.booleanValue()) {
+						JSONArray names = instellingen.get("layerNames").isArray();
+						JSONArray values = instellingen.get("layerVisible").isArray();
+						for(int i = 0; i < names.size(); i++) {
+							String n = names.get(i).isString().stringValue();
+							Boolean visible = layers.getOrDefault(n, Boolean.FALSE);
+							values.set(i, JSONBoolean.getInstance(visible));
+						}
+						json = v.toString();
+					}
+				}
+			}
+			return json;
 		}
 
 
