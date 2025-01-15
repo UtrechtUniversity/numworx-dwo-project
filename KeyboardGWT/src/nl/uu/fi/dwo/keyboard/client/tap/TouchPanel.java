be8fbc8f27d3424b30097485c30935b9010851a2
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-01-15 08:14:16 +0100

    W I P
    TouchPanel als child van FocusPanel

--- KeyboardGWT/src/nl/uu/fi/dwo/keyboard/client/tap/TouchPanel.java
+++ KeyboardGWT/src/nl/uu/fi/dwo/keyboard/client/tap/TouchPanel.java
@@ -1,21 +1,28 @@
 package nl.uu.fi.dwo.keyboard.client.tap;
 
 import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.FocusPanel;
+import com.google.web.bindery.event.shared.HandlerRegistrations;
 
-public class TouchPanel extends SimplePanel {
+public class TouchPanel extends FocusPanel {
 
 	public HandlerRegistration addLongTapHandler(LongTapHandler handler) {
-		// TODO Auto-generated method stub
-		return null;
+		return addHandler(handler, LongTapEvent.getType());
 	}
 
+	public HandlerRegistration addTapHandler(TapHandler handler) {
+		return addHandler(handler, TapEvent.getType());
+	}
+	
 	public HandlerRegistration addTouchHandler(TouchHandler handler) {
-		return null;
+		com.google.web.bindery.event.shared.HandlerRegistration x = 
+				HandlerRegistrations.compose(
+						addTouchStartHandler(handler),
+						addTouchMoveHandler(handler),
+						addTouchEndHandler(handler),
+						addTouchCancelHandler(handler));
+		return x::removeHandler;
+		
 	}
 
-	public HandlerRegistration addTapHandler(TapHandler tapHandler) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
