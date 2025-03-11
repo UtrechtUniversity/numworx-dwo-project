55f9ef3c1e80512248929f9e10641d303bc659b4
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-03-11 09:38:35 +0100

    Subnet voor IPv6 adressen

--- DWOServer/src/test/java/fi/servlet/dwomaccess/SubnetTest.java
+++ DWOServer/src/test/java/fi/servlet/dwomaccess/SubnetTest.java
@@ -36,7 +36,50 @@ public class SubnetTest extends TestCase {
     assertFalse(check);
     check = netMatch("0.0.0.0", "ERROR");
     assertFalse(check);
-  
-    
   }
+  
+  public void testNetMaskFail6() {
+	  boolean check;
+	  check = netMatch("131.211.0.0/16", "::1");
+	  assertFalse(check);
+	  check = netMatch("2001::/16", "131.211.42.27");
+	  assertFalse(check);
+	  check = netMatch("2001::/16", "2003::2345");
+	  assertFalse(check);
+  }
+  
+  public void testNetMask6() {
+	  boolean check; 
+	  check = netMatch("2001:1234:1234:1234::/64", "2001:1234:1234:1234:1234:1234:1234:1234");
+	  assertTrue(check);
+	  check = netMatch("2001:8000::/17", "2001::");
+	  assertFalse(check);
+	  check = netMatch("2001:8000::/17", "2001:AAAA::00001");
+	  assertTrue(check);
+	  check = netMatch("2001:8000::", "2001:8000::");
+	  assertTrue(check);
+	  check = netMatch("2001:8000::", "2001:8000::1");
+	  assertFalse(check);
+  }
+  
+  public void testNetMatchRange6() {
+	    boolean check = netMatchRange("131.211.22.0/23, 131.211.24.45", "::1");
+	    assertFalse(check);
+	    check = netMatchRange("131.211.22.0/23, 0::0/127, 131.211.24.45", "::1");
+	    assertTrue(check); 
+  }
+  
+  public void testnull() {
+	  boolean check = netMatch("::/0", "1.2.3.4");
+	  assertFalse(check);
+	  check = netMatch("::/0", "::1");
+	  assertTrue(check);
+	  check = netMatch("0.0.0.0/0", "1.2.3.4");
+	  assertTrue(check);
+	  check = netMatch("0.0.0.0/0", "::1");
+	  assertFalse(check);
+	    
+  }
+  
+  
 }
