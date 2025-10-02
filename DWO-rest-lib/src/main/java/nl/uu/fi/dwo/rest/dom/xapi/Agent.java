0a60089f035bcff5ed2fbc5d04faa28f1b04e984
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-10-02 16:05:00 +0200

    eerste poging
    haal completed records op van xapi server

--- DWO-rest-lib/src/main/java/nl/uu/fi/dwo/rest/dom/xapi/Agent.java
+++ DWO-rest-lib/src/main/java/nl/uu/fi/dwo/rest/dom/xapi/Agent.java
@@ -1,7 +1,8 @@
 package nl.uu.fi.dwo.rest.dom.xapi;
 
-public class Agent {
+public class Agent { 
+  public static final String AGENT = "Agent";
   public Account account;
   public String name;
-  public final String objectType = "Agent"; 
+  public /*final*/ String objectType = AGENT; 
 }
