652fb5b675c44527999af62d4149c410bf6e6f6e
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-11-17 08:53:47 +0100

    move java widgets to numworx-dwo-author

--- UploadWidget/UploadWidgetRM/updatetest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-D=s3://test-dwo-nl/jars
-cd target/
-S=uploadwidgetrm.jar
-aws --profile prod s3 cp --acl public-read  $S $D/$S
-scp $S $USER@gemini.science.uu.nl:/science/wwwprojects/FI-Sites/www-dev/dwo/jars/
-
