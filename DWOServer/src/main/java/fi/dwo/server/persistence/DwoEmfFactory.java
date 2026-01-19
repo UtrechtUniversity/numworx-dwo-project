125547d3ef21265085eb3b67f2c779ecc7ca8f84
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2026-01-19 11:43:04 +0100

    remove get_instance

--- DWOServer/src/main/java/fi/dwo/server/persistence/DwoEmfFactory.java
+++ DWOServer/src/main/java/fi/dwo/server/persistence/DwoEmfFactory.java
@@ -16,7 +16,7 @@ public class DwoEmfFactory {
 
     private static final Logger LOG = Logger.getLogger(DwoEmfFactory.class.getName());
 
-    private volatile static EntityManagerFactory _instance = Persistence.createEntityManagerFactory("DWO_MySQLDB");
+    private volatile static EntityManagerFactory _instance;
 //uses config below, but does not load persistent classes. Needs to generate xml or so.
 //<persistence-unit name="DWO_MySQLDB" transaction-type="NON_JTA_DATASOURCE">
 //<!-- include all and any required persistent entity in here exclude-unlisted-classes is being ignored!-->
@@ -29,17 +29,10 @@ public class DwoEmfFactory {
 //    <shared-cache-mode>NONE</shared-cache-mode>
 //  </persistence-unit>
 
-    /**
-	 * @return the _instance
-	 */
-	public static EntityManagerFactory get_instance() {
-		return _instance;
-	}
-
 	/**
 	 * @param _instance the _instance to set
 	 */
-	public static void set_instance(EntityManagerFactory _instance) {
+	public synchronized static void set_instance(EntityManagerFactory _instance) {
 		DwoEmfFactory._instance = _instance;
 	}
 
